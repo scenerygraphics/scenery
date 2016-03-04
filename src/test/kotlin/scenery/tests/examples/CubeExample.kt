0366584ad4cb7491af45d9a7022646bf936fb1a3
Ulrik Günther <ulrik@42degreesoffreedom.com>
2016-03-04 16:52:04 +0100

    Shortened CubeExample further by removing unnecessary portions of code

--- src/test/kotlin/scenery/tests/examples/CubeExample.kt
+++ src/test/kotlin/scenery/tests/examples/CubeExample.kt
@@ -2,11 +2,9 @@ package scenery.tests.examples
 
 import cleargl.*
 import com.jogamp.opengl.GLAutoDrawable
-import com.jogamp.opengl.GLException
 import org.junit.Test
 import scenery.*
 import scenery.rendermodules.opengl.DeferredLightingRenderer
-import java.io.IOException
 import kotlin.concurrent.thread
 
 /**
@@ -26,70 +24,49 @@ class CubeExample {
 
             override fun init(pDrawable: GLAutoDrawable) {
                 super.init(pDrawable)
-                try {
-                    deferredRenderer = DeferredLightingRenderer(pDrawable.gl.gL4, mClearGLWindow!!.width, mClearGLWindow!!.height)
+                deferredRenderer = DeferredLightingRenderer(pDrawable.gl.gL4, mClearGLWindow!!.width, mClearGLWindow!!.height)
 
-                    val cam: Camera = DetachedHeadCamera()
+                var box = Box(GLVector(1.0f, 1.0f, 1.0f))
+                var boxmaterial = PhongMaterial()
+                boxmaterial.ambient = GLVector(1.0f, 0.0f, 0.0f)
+                boxmaterial.diffuse = GLVector(0.0f, 1.0f, 0.0f)
+                boxmaterial.specular = GLVector(1.0f, 1.0f, 1.0f)
+                box.position = GLVector(0.0f, 0.0f, 1.1f)
 
-                    var box = Box(GLVector(1.0f, 1.0f, 1.0f))
-                    var boxmaterial = PhongMaterial()
-                    boxmaterial.ambient = GLVector(1.0f, 0.0f, 0.0f)
-                    boxmaterial.diffuse = GLVector(0.0f, 1.0f, 0.0f)
-                    boxmaterial.specular = GLVector(1.0f, 1.0f, 1.0f)
-                    box.position = GLVector(0.0f, 0.0f, 1.1f)
+                scene.addChild(box)
+                scene.initList.add(box)
 
-                    scene.addChild(box)
-                    scene.initList.add(box)
-
-                    var lights = (0..2).map {
-                        PointLight()
-                    }
-
-                    lights.mapIndexed { i, light ->
-                        light.position = GLVector(2.0f*i, 2.0f*i, 2.0f*i)
-                        light.emissionColor = GLVector(i*0.33f, 1.0f, 1.0f)
-                        scene.addChild(light)
-                    }
-
-                    cam.position = GLVector(0.0f, 0.0f, -5.0f)
-                    cam.view = GLMatrix().setCamera(cam.position, cam.position + cam.forward, cam.up)
-                    cam.projection = GLMatrix()
-                            .setPerspectiveProjectionMatrix(
-                                70.0f / 180.0f * Math.PI.toFloat(),
-                                pDrawable.surfaceWidth.toFloat() / pDrawable.surfaceHeight.toFloat(), 0.1f, 1000.0f)
-                            .invert()
-                    cam.active = true
-
-                    scene.addChild(cam)
-
-                    thread {
-                        while (true) {
-                            box.rotation.rotateByEuler(0.0f, 0.01f, 0.01f)
-                            box.needsUpdate = true
-
-                            Thread.sleep(20)
-                        }
-                    }
-                    deferredRenderer?.initializeScene(scene)
-                } catch (e: GLException) {
-                    e.printStackTrace()
-                } catch (e: IOException) {
-                    e.printStackTrace()
+                var lights = (0..2).map {
+                    PointLight()
                 }
 
-            }
+                lights.mapIndexed { i, light ->
+                    light.position = GLVector(2.0f * i, 2.0f * i, 2.0f * i)
+                    light.emissionColor = GLVector(i * 0.33f, 1.0f, 1.0f)
+                    scene.addChild(light)
+                }
 
-            override fun reshape(pDrawable: GLAutoDrawable,
-                                 pX: Int,
-                                 pY: Int,
-                                 pWidth: Int,
-                                 pHeight: Int) {
-                var pHeight = pHeight
-                super.reshape(pDrawable, pX, pY, pWidth, pHeight)
+                val cam: Camera = DetachedHeadCamera()
+                cam.position = GLVector(0.0f, 0.0f, -5.0f)
+                cam.view = GLMatrix().setCamera(cam.position, cam.position + cam.forward, cam.up)
+                cam.projection = GLMatrix()
+                        .setPerspectiveProjectionMatrix(
+                                70.0f / 180.0f * Math.PI.toFloat(),
+                                pDrawable.surfaceWidth.toFloat() / pDrawable.surfaceHeight.toFloat(), 0.1f, 1000.0f)
+                        .invert()
+                cam.active = true
 
-                if (pHeight == 0)
-                    pHeight = 1
-                val ratio = 1.0f * pWidth / pHeight
+                scene.addChild(cam)
+
+                thread {
+                    while (true) {
+                        box.rotation.rotateByEuler(0.0f, 0.01f, 0.01f)
+                        box.needsUpdate = true
+
+                        Thread.sleep(20)
+                    }
+                }
+                deferredRenderer?.initializeScene(scene)
             }
 
             override fun display(pDrawable: GLAutoDrawable) {
@@ -97,11 +74,7 @@ class CubeExample {
 
                 frameNum++
                 deferredRenderer?.render(scene)
-                clearGLWindow.windowTitle = "%.1f fps".format(pDrawable.animator?.lastFPS)
-            }
-
-            override fun dispose(pDrawable: GLAutoDrawable) {
-                super.dispose(pDrawable)
+                clearGLWindow.windowTitle = "scenery: CubeExample - %.1f fps".format(pDrawable.animator?.lastFPS)
             }
 
             override fun setClearGLWindow(pClearGLWindow: ClearGLWindow) {
