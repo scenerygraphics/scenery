f0237074459dd763d653f8332539a895dddeb155
Ulrik Günther <ulrik@42degreesoffreedom.com>
2016-06-20 13:37:32 +0200

    Introduced SceneryDefaultApplication
    * enables faster prototyping with much less boilerplate code
    * just implement `init()` routine for scene setup, and be able to use the deferred
      lighting renderer, REPL and Hub
    * _TexturedCubeExample_ has been updated to use SceneryDefaultApplication as base

---
+++ src/main/kotlin/scenery/SceneryDefaultApplication.kt
@@ -0,0 +1,98 @@
+package scenery
+
+import cleargl.ClearGLDefaultEventListener
+import cleargl.ClearGLDisplayable
+import cleargl.ClearGLWindow
+import com.jogamp.opengl.GLAutoDrawable
+import org.junit.Test
+import scenery.controls.ClearGLInputHandler
+import scenery.rendermodules.opengl.DeferredLightingRenderer
+import scenery.repl.REPL
+
+/**
+ * <Description>
+ *
+ * @author Ulrik Günther <hello@ulrik.is>
+ */
+
+open class SceneryDefaultApplication(var applicationName: String) {
+    protected val scene: Scene = Scene()
+    protected val repl: REPL = REPL()
+    protected var frameNum = 0
+    protected var deferredRenderer: DeferredLightingRenderer? = null
+    protected var hub: Hub = Hub()
+    protected var glWindow: ClearGLDisplayable? = null
+
+    open fun init(pDrawable: GLAutoDrawable) {
+
+    }
+
+    @Test open fun main() {
+        val lClearGLWindowEventListener = object : ClearGLDefaultEventListener() {
+
+            override fun init(pDrawable: GLAutoDrawable) {
+                this@SceneryDefaultApplication.init(pDrawable)
+            }
+
+            override fun display(pDrawable: GLAutoDrawable) {
+                super.display(pDrawable)
+
+                frameNum++
+                deferredRenderer?.render(scene)
+
+                if(deferredRenderer?.settings?.get<Boolean>("wantsFullscreen") == true && deferredRenderer?.settings?.get<Boolean>("isFullscreen") == false) {
+                    glWindow!!.setFullscreen(true)
+                    deferredRenderer?.settings?.set("wantsFullscreen", true)
+                    deferredRenderer?.settings?.set("isFullscreen", true)
+                }
+
+                if(deferredRenderer?.settings?.get<Boolean>("wantsFullscreen") == false && deferredRenderer?.settings?.get<Boolean>("isFullscreen") == true) {
+                    glWindow!!.setFullscreen(false)
+                    deferredRenderer?.settings?.set("wantsFullscreen", false)
+                    deferredRenderer?.settings?.set("isFullscreen", false)
+                }
+
+                clearGLWindow.windowTitle = "scenery: %s - %.1f fps".format(this.javaClass.enclosingClass.simpleName.substringAfterLast("."), pDrawable.animator?.lastFPS)
+            }
+
+            override fun setClearGLWindow(pClearGLWindow: ClearGLWindow) {
+                glWindow = pClearGLWindow
+            }
+
+            override fun getClearGLWindow(): ClearGLDisplayable {
+                return glWindow!!
+            }
+
+            override fun reshape(pDrawable: GLAutoDrawable,
+                                 pX: Int,
+                                 pY: Int,
+                                 pWidth: Int,
+                                 pHeight: Int) {
+                var pHeight = pHeight
+
+                if (pHeight == 0)
+                    pHeight = 1
+
+                super.reshape(pDrawable, pX, pY, pWidth, pHeight)
+                deferredRenderer?.reshape(pWidth, pHeight)
+            }
+
+        }
+
+        val glWindow = ClearGLWindow("",
+                1024,
+                1024,
+                lClearGLWindowEventListener)
+        glWindow.isVisible = true
+        glWindow.setFPS(60)
+
+        val inputHandler = ClearGLInputHandler(scene, deferredRenderer as Any, glWindow)
+        inputHandler.useDefaultBindings(System.getProperty("user.home") + "/.$applicationName.bindings")
+
+        glWindow.start()
+
+        while (glWindow.isVisible) {
+            Thread.sleep(10)
+        }
+    }
+}
--- src/test/kotlin/scenery/tests/examples/TexturedCubeExample.kt
+++ src/test/kotlin/scenery/tests/examples/TexturedCubeExample.kt
@@ -1,12 +1,11 @@
 package scenery.tests.examples
 
-import cleargl.*
+import cleargl.GLMatrix
+import cleargl.GLVector
 import com.jogamp.opengl.GLAutoDrawable
 import org.junit.Test
 import scenery.*
-import scenery.controls.ClearGLInputHandler
 import scenery.rendermodules.opengl.DeferredLightingRenderer
-import scenery.repl.REPL
 import kotlin.concurrent.thread
 
 /**
@@ -14,131 +13,68 @@ import kotlin.concurrent.thread
  *
  * @author Ulrik Günther <hello@ulrik.is>
  */
-class TexturedCubeExample {
-    private val scene: Scene = Scene()
-    private val repl: REPL = REPL()
-    private var frameNum = 0
-    private var deferredRenderer: DeferredLightingRenderer? = null
-    private var hub: Hub = Hub()
+class TexturedCubeExample : SceneryDefaultApplication("TexturedCubeExample") {
+    override fun init(pDrawable: GLAutoDrawable) {
+        deferredRenderer = DeferredLightingRenderer(pDrawable.gl.gL4, glWindow!!.width, glWindow!!.height)
+        hub.add(SceneryElement.RENDERER, deferredRenderer!!)
 
-    @Test fun demo() {
-        val lClearGLWindowEventListener = object : ClearGLDefaultEventListener() {
-
-            private var mClearGLWindow: ClearGLDisplayable? = null
-
-            override fun init(pDrawable: GLAutoDrawable) {
-                super.init(pDrawable)
-                deferredRenderer = DeferredLightingRenderer(pDrawable.gl.gL4, mClearGLWindow!!.width, mClearGLWindow!!.height)
-                hub.add(SceneryElement.RENDERER, deferredRenderer!!)
-
-                var box = Box(GLVector(1.0f, 1.0f, 1.0f))
-                var boxmaterial = Material()
-                boxmaterial.ambient = GLVector(1.0f, 0.0f, 0.0f)
-                boxmaterial.diffuse = GLVector(0.0f, 1.0f, 0.0f)
-                boxmaterial.specular = GLVector(1.0f, 1.0f, 1.0f)
-                boxmaterial.textures.put("diffuse", this.javaClass.getResource("textures/helix.png").file)
-                box.material = boxmaterial
-
-                box.position = GLVector(0.0f, 0.0f, 0.0f)
-                scene.addChild(box)
-
-                var lights = (0..2).map {
-                    PointLight()
-                }
-
-                lights.mapIndexed { i, light ->
-                    light.position = GLVector(2.0f * i, 2.0f * i, 2.0f * i)
-                    light.emissionColor = GLVector(1.0f, 0.0f, 1.0f)
-                    light.intensity = 0.2f*(i+1);
-                    scene.addChild(light)
-                }
-
-                val cam: Camera = DetachedHeadCamera()
-                cam.position = GLVector(0.0f, 0.0f, -5.0f)
-                cam.view = GLMatrix().setCamera(cam.position, cam.position + cam.forward, cam.up)
-                cam.projection = GLMatrix()
-                        .setPerspectiveProjectionMatrix(
-                                70.0f / 180.0f * Math.PI.toFloat(),
-                                1024f / 1024f, 0.1f, 1000.0f)
-                cam.active = true
-
-                scene.addChild(cam)
-
-                thread {
-                    while (true) {
-                        box.rotation.rotateByAngleY(0.01f)
-                        box.needsUpdate = true
-
-                        Thread.sleep(20)
-                    }
-                }
-
-                deferredRenderer?.initializeScene(scene)
-
-                repl.addAccessibleObject(scene)
-                repl.addAccessibleObject(deferredRenderer!!)
-                repl.showConsoleWindow()
-            }
-
-            override fun display(pDrawable: GLAutoDrawable) {
-                super.display(pDrawable)
-
-                frameNum++
-                deferredRenderer?.render(scene)
-
-                if(deferredRenderer?.settings?.get<Boolean>("wantsFullscreen") == true && deferredRenderer?.settings?.get<Boolean>("isFullscreen") == false) {
-                    mClearGLWindow!!.setFullscreen(true)
-                    deferredRenderer?.settings?.set("wantsFullscreen", true)
-                    deferredRenderer?.settings?.set("isFullscreen", true)
-                }
-
-                if(deferredRenderer?.settings?.get<Boolean>("wantsFullscreen") == false && deferredRenderer?.settings?.get<Boolean>("isFullscreen") == true) {
-                    mClearGLWindow!!.setFullscreen(false)
-                    deferredRenderer?.settings?.set("wantsFullscreen", false)
-                    deferredRenderer?.settings?.set("isFullscreen", false)
-                }
-
-                clearGLWindow.windowTitle = "scenery: %s - %.1f fps".format(this.javaClass.enclosingClass.simpleName.substringAfterLast("."), pDrawable.animator?.lastFPS)
-            }
-
-            override fun setClearGLWindow(pClearGLWindow: ClearGLWindow) {
-                mClearGLWindow = pClearGLWindow
-            }
-
-            override fun getClearGLWindow(): ClearGLDisplayable {
-                return mClearGLWindow!!
-            }
-
-            override fun reshape(pDrawable: GLAutoDrawable,
-                                 pX: Int,
-                                 pY: Int,
-                                 pWidth: Int,
-                                 pHeight: Int) {
-                var pHeight = pHeight
-
-                if (pHeight == 0)
-                    pHeight = 1
-
-                super.reshape(pDrawable, pX, pY, pWidth, pHeight)
-                deferredRenderer?.reshape(pWidth, pHeight)
-            }
-
+        var boxmaterial = Material()
+        with(boxmaterial) {
+            ambient = GLVector(1.0f, 0.0f, 0.0f)
+            diffuse = GLVector(0.0f, 1.0f, 0.0f)
+            specular = GLVector(1.0f, 1.0f, 1.0f)
+            textures.put("diffuse", this.javaClass.getResource("textures/helix.png").file)
         }
 
-        val lClearGLWindow = ClearGLWindow("",
-                1024,
-                1024,
-                lClearGLWindowEventListener)
-        lClearGLWindow.isVisible = true
-        lClearGLWindow.setFPS(60)
+        var box = Box(GLVector(1.0f, 1.0f, 1.0f))
 
-        val inputHandler = ClearGLInputHandler(scene, deferredRenderer as Any, lClearGLWindow)
-        inputHandler.useDefaultBindings(System.getProperty("user.home") + "/.sceneryExamples.bindings")
-
-        lClearGLWindow.start()
-
-        while (lClearGLWindow.isVisible) {
-            Thread.sleep(10)
+        with(box) {
+            box.material = boxmaterial
+            box.position = GLVector(0.0f, 0.0f, 0.0f)
+            scene.addChild(this)
         }
+
+        var lights = (0..2).map {
+            PointLight()
+        }
+
+        lights.mapIndexed { i, light ->
+            light.position = GLVector(2.0f * i, 2.0f * i, 2.0f * i)
+            light.emissionColor = GLVector(1.0f, 0.0f, 1.0f)
+            light.intensity = 0.2f*(i+1);
+            scene.addChild(light)
+        }
+
+        val cam: Camera = DetachedHeadCamera()
+        with(cam) {
+            position = GLVector(0.0f, 0.0f, -5.0f)
+            view = GLMatrix().setCamera(cam.position, cam.position + cam.forward, cam.up)
+            projection = GLMatrix()
+                    .setPerspectiveProjectionMatrix(
+                            70.0f / 180.0f * Math.PI.toFloat(),
+                            1024f / 1024f, 0.1f, 1000.0f)
+            active = true
+
+            scene.addChild(this)
+        }
+
+        thread {
+            while (true) {
+                box.rotation.rotateByAngleY(0.01f)
+                box.needsUpdate = true
+
+                Thread.sleep(20)
+            }
+        }
+
+        deferredRenderer?.initializeScene(scene)
+
+        repl.addAccessibleObject(scene)
+        repl.addAccessibleObject(deferredRenderer!!)
+        repl.showConsoleWindow()
+    }
+
+    @Test override fun main() {
+        super.main()
     }
 }
