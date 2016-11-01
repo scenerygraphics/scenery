9b51e3246874118dbb612ed65fdd7c567f87e7cb
Ulrik Guenther <hello@ulrik.is>
2016-11-01 17:40:00 +0100

    Added preallocation and texcoords to Sphere

--- src/main/kotlin/scenery/Sphere.kt
+++ src/main/kotlin/scenery/Sphere.kt
@@ -30,12 +30,13 @@ class Sphere(radius: Float, segments: Int) : Node("sphere"), HasGeometry {
         this.radius = radius
         this.segments = segments
 
-        var vbuffer = ArrayList<Float>()
-        var nbuffer = ArrayList<Float>()
+        var vbuffer = ArrayList<Float>(segments*segments*2*3)
+        var nbuffer = ArrayList<Float>(segments*segments*2*3)
+        var tbuffer = ArrayList<Float>(segments*segments*2*2)
 
-        for (i: Int in 1..segments) {
-            val lat0: Float = Math.PI.toFloat() * (-0.5f + (i.toFloat() - 1.0f) / segments.toFloat());
-            val lat1: Float = Math.PI.toFloat() * (-0.5f + i.toFloat() / segments.toFloat());
+        for (i: Int in 0..segments) {
+            val lat0: Float = Math.PI.toFloat() * (-0.5f + (i-1).toFloat() / segments.toFloat())
+            val lat1: Float = Math.PI.toFloat() * (-0.5f + i.toFloat() / segments.toFloat())
 
             val z0 = Math.sin(lat0.toDouble()).toFloat()
             val z1 = Math.sin(lat1.toDouble()).toFloat()
@@ -43,7 +44,7 @@ class Sphere(radius: Float, segments: Int) : Node("sphere"), HasGeometry {
             val zr0 = Math.cos(lat0.toDouble()).toFloat()
             val zr1 = Math.cos(lat1.toDouble()).toFloat()
 
-            for (j: Int in 1..segments) {
+            for (j: Int in 0..segments) {
                 val lng = 2 * Math.PI.toFloat() * (j - 1) / segments
                 val x = Math.cos(lng.toDouble()).toFloat()
                 val y = Math.sin(lng.toDouble()).toFloat()
@@ -63,11 +64,18 @@ class Sphere(radius: Float, segments: Int) : Node("sphere"), HasGeometry {
                 nbuffer.add(x)
                 nbuffer.add(y)
                 nbuffer.add(z1)
+
+                tbuffer.add(0.0f)
+                tbuffer.add(0.0f)
+
+                tbuffer.add(0.0f)
+                tbuffer.add(0.0f)
             }
         }
 
         vertices = BufferUtils.allocateFloatAndPut(vbuffer.toFloatArray())
         normals = BufferUtils.allocateFloatAndPut(nbuffer.toFloatArray())
+        texcoords = BufferUtils.allocateFloatAndPut(tbuffer.toFloatArray())
     }
 
 }
