aa2b3489c0e0edb65065ba4559dc509d281fe034
Ulrik Guenther <guenther@mpi-cbg.de>
2017-05-27 19:05:15 +0200

    VulkanShaderModule: Use source file instead of compiled SPIRV, if source file is newer

--- src/main/kotlin/graphics/scenery/backends/vulkan/VulkanShaderModule.kt
+++ src/main/kotlin/graphics/scenery/backends/vulkan/VulkanShaderModule.kt
@@ -35,30 +35,65 @@ open class VulkanShaderModule(device: VkDevice, entryPoint: String, clazz: Class
         logger.debug("Creating VulkanShaderModule $entryPoint, $shaderCodePath")
 
         this.device = device
-        val codeResource = if(clazz.javaClass.getResource(shaderCodePath) != null) {
+
+        // check if we have a compiled version, and it's newer than the source version
+        val sourceClass: Class<*>
+        var codeResource = if(clazz.javaClass.getResource(shaderCodePath) != null) {
+            sourceClass = clazz
             clazz.javaClass.getResource(shaderCodePath)
         } else {
+            sourceClass = VulkanRenderer::class.java
             VulkanRenderer::class.java.getResource(shaderCodePath)
+        }
+
+        val actualCodePath: String
+
+        val sourceNewer = if(shaderCodePath.toLowerCase().endsWith("spv")) {
+            // a slight bias is needed here, as if both files are compiled into the
+            // classes/ or jar at the same time, they'll carry the same timestamp.
+            val spirvModificationDate = Date(codeResource.openConnection().lastModified + 500)
+
+            val sourceCodeResource = sourceClass.getResource(shaderCodePath.substringBeforeLast(".spv"))
+
+            if(sourceCodeResource != null) {
+                val sourceModificationDate = Date(sourceCodeResource.openConnection().lastModified)
+
+                if(sourceModificationDate.after(spirvModificationDate)) {
+                    logger.info("Recompiling $shaderCodePath, as source file is newer than SPV file.")
+                    actualCodePath = shaderCodePath.substringBeforeLast(".spv")
+                    codeResource = sourceCodeResource
+                    true
+                } else {
+                    actualCodePath = shaderCodePath
+                    false
+                }
+            } else {
+                actualCodePath = shaderCodePath
+                false
+            }
+        } else {
+            actualCodePath = shaderCodePath
+            false
         }
 
         var code = ByteBuffer.allocate(0)
 
-        val spirv = if(shaderCodePath.toLowerCase().endsWith("spv")) {
+        val spirv = if(shaderCodePath.toLowerCase().endsWith("spv") && !sourceNewer) {
             code = BufferUtils.allocateByteAndPut(codeResource.readBytes())
             code.toSPIRVBytecode()
         } else {
-            logger.info("Compiling $shaderCodePath to SPIR-V...")
+            logger.info("Compiling $actualCodePath to SPIR-V...")
             // code needs to be compiled first
             val program = TProgram()
             val defaultResources = libspirvcrossj.getDefaultTBuiltInResource()
-            val shaderType = when (shaderCodePath.substringAfterLast(".")) {
+            val shaderType = when (actualCodePath.substringAfterLast(".")) {
                 "vert" -> EShLanguage.EShLangVertex
                 "frag" -> EShLanguage.EShLangFragment
                 "geom" -> EShLanguage.EShLangGeometry
                 "tesc" -> EShLanguage.EShLangTessControl
                 "tese" -> EShLanguage.EShLangTessEvaluation
                 "comp" -> EShLanguage.EShLangCompute
-                else -> { logger.warn("Unknown shader extension ." + shaderCodePath.substringAfterLast(".")); 0 }
+                else -> { logger.warn("Unknown shader extension ." + actualCodePath.substringAfterLast(".")); 0 }
             }
 
 
@@ -74,7 +109,7 @@ open class VulkanShaderModule(device: VkDevice, entryPoint: String, clazz: Class
 
             val compileFail = !shader.parse(defaultResources, 450, false, messages)
             if(compileFail) {
-                logger.error("Error in shader compilation of $shaderCodePath for ${clazz.simpleName}: ${shader.infoLog}")
+                logger.error("Error in shader compilation of $actualCodePath for ${clazz.simpleName}: ${shader.infoLog}")
             }
 
             program.addShader(shader)
@@ -89,7 +124,7 @@ open class VulkanShaderModule(device: VkDevice, entryPoint: String, clazz: Class
 
                 tmp
             } else {
-                logger.error("Error in shader linking of $shaderCodePath for ${clazz.simpleName}: ${program.infoLog}")
+                logger.error("Error in shader linking of $actualCodePath for ${clazz.simpleName}: ${program.infoLog}")
                 IntVec()
             }
         }
