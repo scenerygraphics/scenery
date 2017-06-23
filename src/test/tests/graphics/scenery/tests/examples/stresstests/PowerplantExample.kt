411d75fe11b66b477f48dabaa81b2be84d5517bd
Ulrik Günther <ulrik@42degreesoffreedom.com>
2017-06-23 11:33:29 +0200

    Added message to download additional models to PowerPlantExample and RungholtExample

--- src/test/tests/graphics/scenery/tests/examples/stresstests/PowerplantExample.kt
+++ src/test/tests/graphics/scenery/tests/examples/stresstests/PowerplantExample.kt
@@ -5,114 +5,93 @@ import graphics.scenery.*
 import graphics.scenery.backends.Renderer
 import graphics.scenery.utils.Numerics
 import org.junit.Test
-import java.io.IOException
 import kotlin.concurrent.thread
 
 /**
-* <Description>
-*
-* @author Ulrik Günther <hello@ulrik.is>
-*/
-class PowerplantExample: SceneryDefaultApplication("PowerplantExample", windowWidth = 1280, windowHeight = 720) {
+ * Stress-testing demo with the PowerPlant model from UNC, and lots of lights.
+ *
+ * @author Ulrik Günther <hello@ulrik.is>
+ */
+class PowerplantExample : SceneryDefaultApplication("PowerplantExample", windowWidth = 1280, windowHeight = 720) {
     override fun init() {
-        try {
-            val lightCount = 127
+        val lightCount = 127
 
-            renderer = Renderer.createRenderer(hub, applicationName, scene, windowWidth, windowHeight)
-            hub.add(SceneryElement.Renderer, renderer!!)
+        logger.warn("ADDITIONAL FILES NEEDED")
+        logger.warn("This example needs an additional model file, which is not available as part of the")
+        logger.warn("example models zip. Please download it from: http://graphics.cs.williams.edu/data/meshes.xml#12")
 
-            val cam: Camera = DetachedHeadCamera()
-            cam.position = GLVector(0.0f, 0.0f, 0.0f)
-            cam.perspectiveCamera(50.0f, windowWidth.toFloat(), windowHeight.toFloat(), nearPlaneLocation = 0.5f, farPlaneLocation = 1000.0f)
-            cam.active = true
+        renderer = Renderer.createRenderer(hub, applicationName, scene, windowWidth, windowHeight)
+        hub.add(SceneryElement.Renderer, renderer!!)
 
-            scene.addChild(cam)
+        val cam: Camera = DetachedHeadCamera()
+        with(cam) {
+            position = GLVector(0.0f, 0.0f, 0.0f)
+            perspectiveCamera(50.0f, windowWidth.toFloat(), windowHeight.toFloat(), nearPlaneLocation = 0.5f, farPlaneLocation = 1000.0f)
+            active = true
 
-            val boxes = (0..lightCount).map {
-                Box(GLVector(0.5f, 0.5f, 0.5f))
-            }
-
-            val lights = (0..lightCount).map {
-                PointLight()
-            }
-
-            boxes.mapIndexed { i, box ->
-                box.material = Material()
-                box.addChild(lights[i])
-                scene.addChild(box)
-            }
-
-            lights.map {
-                it.emissionColor = Numerics.randomVectorFromRange(3, 0.0f, 1.0f)
-                it.parent?.material?.diffuse = it.emissionColor
-                it.intensity = Numerics.randomFromRange(0.01f, 10f)
-                it.linear = 0.01f
-                it.quadratic = 0.01f
-
-                scene.addChild(it)
-            }
-
-            val hullbox = Box(GLVector(300.0f, 300.0f, 300.0f))
-            hullbox.position = GLVector(0.0f, 0.0f, 0.0f)
-
-            val hullboxMaterial = Material()
-            hullboxMaterial.ambient = GLVector(0.6f, 0.6f, 0.6f)
-            hullboxMaterial.diffuse = GLVector(0.4f, 0.4f, 0.4f)
-            hullboxMaterial.specular = GLVector(0.0f, 0.0f, 0.0f)
-            hullboxMaterial.doubleSided = true
-            hullbox.material = hullboxMaterial
-
-            val plantMaterial = Material()
-            plantMaterial.ambient = GLVector(0.8f, 0.8f, 0.8f)
-            plantMaterial.diffuse = GLVector(0.5f, 0.5f, 0.5f)
-            plantMaterial.specular = GLVector(0.1f, 0f, 0f)
-
-            val plant = Mesh()
-            plant.readFromOBJ(getDemoFilesPath() + "/powerplant.obj", useMTL = true)
-            plant.position = GLVector(0.0f, 0.0f, 0.0f)
-            // for powerplant
-            plant.scale = GLVector(0.001f, 0.001f, 0.001f)
-            plant.material = Material()
-            plant.children.forEach { it.material = plantMaterial }
-            plant.updateWorld(true, true)
-            plant.name = "rungholt"
-
-            scene.addChild(plant)
-
-            var ticks: Int = 0
-
-            System.out.println(scene.children)
-
-            thread {
-                while (true) {
-                    boxes.mapIndexed {
-                        i, box ->
-                        val phi = Math.PI * 2.0f * ticks / 2500.0f
-
-                        box.position = GLVector(
-                            -128.0f+18.0f*(i+1),
-                            5.0f+i*5.0f,
-                            (i+1) * 50 * Math.cos(phi+(i*0.2f)).toFloat())
-
-                        box.children[0].position = box.position
-
-                    }
-
-                    ticks++
-
-
-                    Thread.sleep(10)
-                }
-            }
-        } catch (e: IOException) {
-            e.printStackTrace()
+            scene.addChild(this)
         }
 
+        val boxes = (0..lightCount).map {
+            Box(GLVector(0.5f, 0.5f, 0.5f))
+        }
+
+        val lights = (0..lightCount).map {
+            PointLight()
+        }
+
+        boxes.mapIndexed { i, box ->
+            box.material = Material()
+            box.addChild(lights[i])
+            scene.addChild(box)
+        }
+
+        lights.map {
+            it.emissionColor = Numerics.randomVectorFromRange(3, 0.0f, 1.0f)
+            it.parent?.material?.diffuse = it.emissionColor
+            it.intensity = Numerics.randomFromRange(0.01f, 10f)
+            it.linear = 0.01f
+            it.quadratic = 0.01f
+
+            scene.addChild(it)
+        }
+
+        val plant = Mesh()
+        with(plant) {
+            readFromOBJ(getDemoFilesPath() + "/powerplant.obj", useMTL = true)
+            position = GLVector(0.0f, 0.0f, 0.0f)
+            scale = GLVector(0.001f, 0.001f, 0.001f)
+            material = Material()
+            updateWorld(true, true)
+            name = "rungholt"
+
+            scene.addChild(this)
+        }
+
+        thread {
+            var ticks = 0L
+            while (true) {
+                boxes.mapIndexed {
+                    i, box ->
+                    val phi = Math.PI * 2.0f * ticks / 2500.0f
+
+                    box.position = GLVector(
+                        -128.0f + 18.0f * (i + 1),
+                        5.0f + i * 5.0f,
+                        (i + 1) * 50 * Math.cos(phi + (i * 0.2f)).toFloat())
+
+                    box.children[0].position = box.position
+
+                }
+
+                ticks++
+                Thread.sleep(10)
+            }
+        }
     }
 
     @Test override fun main() {
         super.main()
     }
-
 }
 
--- src/test/tests/graphics/scenery/tests/examples/stresstests/RungholtExample.kt
+++ src/test/tests/graphics/scenery/tests/examples/stresstests/RungholtExample.kt
@@ -6,115 +6,94 @@ import graphics.scenery.backends.Renderer
 import graphics.scenery.controls.OpenVRHMD
 import graphics.scenery.utils.Numerics
 import org.junit.Test
-import java.io.IOException
 import kotlin.concurrent.thread
 
 /**
-* <Description>
-*
-* @author Ulrik Günther <hello@ulrik.is>
-*/
-class RungholtExample: SceneryDefaultApplication("RungholtExample", windowWidth = 1280, windowHeight = 720) {
+ * Stress-testing demo with the Rungholt model, and even greater number of lights.
+ *
+ * @author Ulrik Günther <hello@ulrik.is>
+ */
+class RungholtExample : SceneryDefaultApplication("RungholtExample", windowWidth = 1280, windowHeight = 720) {
     var hmd: OpenVRHMD? = null
     override fun init() {
-        try {
-            val lightCount = 512
+        val lightCount = 512
 
-            hmd = OpenVRHMD(useCompositor = true)
-            hub.add(SceneryElement.HMDInput, hmd!!)
+        logger.warn("ADDITIONAL FILES NEEDED")
+        logger.warn("This example needs an additional model file, which is not available as part of the")
+        logger.warn("example models zip. Please download it from: http://graphics.cs.williams.edu/data/meshes.xml#13")
 
-            renderer = Renderer.createRenderer(hub, applicationName, scene, windowWidth, windowHeight)
-            hub.add(SceneryElement.Renderer, renderer!!)
+        hmd = OpenVRHMD(useCompositor = true)
+        hub.add(SceneryElement.HMDInput, hmd!!)
 
-            val cam: Camera = DetachedHeadCamera(hmd)
-            cam.position = GLVector(0.0f, 50.0f, -100.0f)
-//            cam.rotation.setFromEuler(-0.3f, 0.5f, 0.0f)
-            cam.perspectiveCamera(50.0f, windowWidth.toFloat(), windowHeight.toFloat(), nearPlaneLocation = 0.5f, farPlaneLocation = 1000.0f)
-            cam.active = true
+        renderer = Renderer.createRenderer(hub, applicationName, scene, windowWidth, windowHeight)
+        hub.add(SceneryElement.Renderer, renderer!!)
 
-            scene.addChild(cam)
-
-            val boxes = (0..lightCount).map {
-                Box(GLVector(0.5f, 0.5f, 0.5f))
-            }
-
-            val lights = (0..lightCount).map {
-                PointLight()
-            }
-
-            boxes.mapIndexed { i, box ->
-                box.material = Material()
-                box.addChild(lights[i])
-                scene.addChild(box)
-            }
-
-            lights.map {
-                it.emissionColor = Numerics.randomVectorFromRange(3, 0.0f, 1.0f)
-                it.parent?.material?.diffuse = it.emissionColor
-                it.intensity = Numerics.randomFromRange(0.1f, 10f)
-                it.linear = 1.2f
-                it.quadratic = 0.2f
-
-                scene.addChild(it)
-            }
-
-            val hullbox = Box(GLVector(300.0f, 300.0f, 300.0f))
-            hullbox.position = GLVector(0.0f, 0.0f, 0.0f)
-
-            val hullboxMaterial = Material()
-            hullboxMaterial.ambient = GLVector(0.6f, 0.6f, 0.6f)
-            hullboxMaterial.diffuse = GLVector(0.4f, 0.4f, 0.4f)
-            hullboxMaterial.specular = GLVector(0.0f, 0.0f, 0.0f)
-            hullboxMaterial.doubleSided = true
-            hullbox.material = hullboxMaterial
-
-            val orcMaterial = Material()
-            orcMaterial.ambient = GLVector(0.8f, 0.8f, 0.8f)
-            orcMaterial.diffuse = GLVector(0.5f, 0.5f, 0.5f)
-            orcMaterial.specular = GLVector(0.1f, 0f, 0f)
-
-            val orcMesh = Mesh()
-            orcMesh.readFromOBJ(getDemoFilesPath() + "/rungholt.obj", useMTL = true)
-            orcMesh.position = GLVector(0.0f, 0.0f, 0.0f)
-            orcMesh.scale = GLVector(1.0f, 1.0f, 1.0f)
-            orcMesh.updateWorld(true, true)
-            orcMesh.name = "rungholt"
-
-            scene.addChild(orcMesh)
-
-            var ticks: Int = 0
-
-            System.out.println(scene.children)
-
-            thread {
-                while (true) {
-                    boxes.mapIndexed {
-                        i, box ->
-                        val phi = ticks / 1500.0f % (Math.PI * 2.0f)
-
-                        box.position = GLVector(
-                            -320.0f+5.0f*(i+1),
-                            15.0f+i*0.2f,
-                            250.0f * Math.cos(phi+(i*0.2f)).toFloat())
-                        box.children[0].position = box.position
-
-                    }
-
-                    ticks++
-
-
-                    Thread.sleep(10)
-                }
-            }
-        } catch (e: IOException) {
-            e.printStackTrace()
+        val cam: Camera = DetachedHeadCamera(hmd)
+        with(cam) {
+            perspectiveCamera(50.0f, windowWidth.toFloat(), windowHeight.toFloat(), nearPlaneLocation = 0.5f, farPlaneLocation = 1000.0f)
+            position = GLVector(0.0f, 50.0f, -100.0f)
+            active = true
+            scene.addChild(this)
         }
 
+        val boxes = (0..lightCount).map {
+            Box(GLVector(0.5f, 0.5f, 0.5f))
+        }
+
+        val lights = (0..lightCount).map {
+            PointLight()
+        }
+
+        boxes.mapIndexed { i, box ->
+            box.material = Material()
+            box.addChild(lights[i])
+            scene.addChild(box)
+        }
+
+        lights.map {
+            it.emissionColor = Numerics.randomVectorFromRange(3, 0.0f, 1.0f)
+            it.parent?.material?.diffuse = it.emissionColor
+            it.intensity = Numerics.randomFromRange(0.1f, 10f)
+            it.linear = 1.2f
+            it.quadratic = 0.2f
+
+            scene.addChild(it)
+        }
+
+        val rungholtMesh = Mesh()
+        with(rungholtMesh) {
+            readFromOBJ(getDemoFilesPath() + "/rungholt.obj", useMTL = true)
+            position = GLVector(0.0f, 0.0f, 0.0f)
+            scale = GLVector(1.0f, 1.0f, 1.0f)
+            updateWorld(true, true)
+            name = "rungholt"
+
+            scene.addChild(this)
+        }
+
+        thread {
+            var ticks = 0L
+            while (true) {
+                boxes.mapIndexed {
+                    i, box ->
+                    val phi = ticks / 1500.0f % (Math.PI * 2.0f)
+
+                    box.position = GLVector(
+                        -320.0f + 5.0f * (i + 1),
+                        15.0f + i * 0.2f,
+                        250.0f * Math.cos(phi + (i * 0.2f)).toFloat())
+                    box.children[0].position = box.position
+
+                }
+
+                ticks++
+                Thread.sleep(10)
+            }
+        }
     }
 
     @Test override fun main() {
         super.main()
     }
-
 }
 
