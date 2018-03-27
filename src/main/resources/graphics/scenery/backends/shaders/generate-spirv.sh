2345388c85f55889e7b628a33dc018ff33e54dd5
Ulrik Guenther <hello@ulrik.is>
2018-03-27 19:25:11 +0200

    Add option to run spirv-opt on compiled SPIR-V shaders

--- src/main/resources/graphics/scenery/backends/shaders/generate-spirv.sh
+++ src/main/resources/graphics/scenery/backends/shaders/generate-spirv.sh
@@ -1,20 +1,44 @@
 #!/bin/bash
 LS_CMD="ls *.{vert,geom,tese,tesc,frag,comp} 2>/dev/null"
 POST_CMD="grep --color -i ERROR"
+OPTIMISE=false
 
-while getopts ":v" opt; do
-	case $opt in
-		v)
-		echo "Verbose mode activated"
-		LS_CMD="$LS_CMD 2>/dev/null"
-		POST_CMD="cat"
-		;;
-	esac
+while getopts ":vO" opt; do
+    case $opt in
+        v)
+            echo "Verbose mode activated"
+            LS_CMD="$LS_CMD 2>/dev/null"
+            POST_CMD="cat"
+            ;;
+        O)
+            echo "Optimisations activated, use with care."
+            if hash spirv-opt 2>/dev/null; then
+                OPTIMISE=true
+                SPIRV_OPT=`which spirv-opt`
+            else
+                echo "Warning: spirv-opt not found, optimisations disabled."
+                OPTIMISE=false
+            fi
+            ;;
+    esac
 done
+
 
 files=`eval "$LS_CMD"`
 for i in $files; do
-	echo -n "# $i GLSL -> SPIR-V"
-	glslangValidator --aml -V $i -o $i.spv | $POST_CMD
-	echo -e "\e[92m ✓ \e[39m"
+    echo -n "# $i GLSL -> SPIR-V"
+
+    # compile to SPV with Vulkan semantics
+    glslangValidator --aml -V $i -o $i.spv.tmp | $POST_CMD
+
+    # optimise, if spirv-opt was found
+    if $OPTIMISE; then
+        $SPIRV_OPT -O -o $i.spv $i.spv.tmp
+        rm $i.spv.tmp
+    # otherwise, just use the unoptimised SPV binary
+    else
+        mv $i.spv.tmp $i.spv
+    fi
+
+    echo -e "\e[92m ✓ \e[39m"
 done;
