35eaa0ed5f19131ba7c5a97f5e5b4cac3bef2e8a
Ulrik Guenther <guenther@mpi-cbg.de>
2018-04-13 17:19:32 +0200

    Bump ClearGL to 2.1.5 to allow OpenGLRenderer to use linear interpolation when sampling from 3D textures

--- src/main/kotlin/graphics/scenery/backends/opengl/OpenGLRenderer.kt
+++ src/main/kotlin/graphics/scenery/backends/opengl/OpenGLRenderer.kt
@@ -2017,7 +2017,7 @@ class OpenGLRenderer(hub: Hub,
                                 dimensions.x().toInt(),
                                 dimensions.y().toInt(),
                                 dimensions.z().toInt() ?: 1,
-                                dimensions.z().toInt() == 1,
+                                true,
                                 miplevels)
 
                             if (generateMipmaps) {
