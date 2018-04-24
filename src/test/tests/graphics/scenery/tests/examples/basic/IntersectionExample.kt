662896f8b1c83ce20b56665c656f1c99b58762c1
Ulrik Guenther <hello@ulrik.is>
2018-04-24 11:34:33 +0200

    Node intersections (WIP)

---
+++ src/test/tests/graphics/scenery/tests/examples/basic/IntersectionExample.kt
@@ -0,0 +1,75 @@
+package graphics.scenery.tests.examples.basic
+
+import cleargl.GLVector
+import graphics.scenery.*
+import graphics.scenery.backends.Renderer
+import org.junit.Test
+import kotlin.concurrent.thread
+
+/**
+ * <Description>
+ *
+ * @author Ulrik Günther <hello@ulrik.is>
+ */
+class IntersectionExample: SceneryBase("IntersectionExample") {
+    override fun init() {
+        renderer = Renderer.createRenderer(hub, applicationName, scene, 512, 512)
+        hub.add(SceneryElement.Renderer, renderer!!)
+
+        val boxmaterial = Material()
+        with(boxmaterial) {
+            ambient = GLVector(1.0f, 0.0f, 0.0f)
+            diffuse = GLVector(0.0f, 1.0f, 0.0f)
+            specular = GLVector(1.0f, 1.0f, 1.0f)
+            roughness = 0.3f
+            metallic = 1.0f
+        }
+
+        val box = Box(GLVector(1.0f, 1.0f, 1.0f))
+        box.name = "le box du win"
+
+        val box2 = Box(GLVector(1.0f, 1.0f, 1.0f))
+
+        with(box) {
+            box.material = boxmaterial
+            scene.addChild(this)
+        }
+
+        with(box2) {
+            position = GLVector(-1.5f, 0.0f, 0.0f)
+            box.material = boxmaterial
+            scene.addChild(this)
+        }
+        val light = PointLight(radius = 15.0f)
+        light.position = GLVector(0.0f, 0.0f, 2.0f)
+        light.intensity = 100.0f
+        light.emissionColor = GLVector(1.0f, 1.0f, 1.0f)
+        scene.addChild(light)
+
+        val cam: Camera = DetachedHeadCamera()
+        with(cam) {
+            position = GLVector(0.0f, 0.0f, 5.0f)
+            perspectiveCamera(50.0f, 512.0f, 512.0f)
+            active = true
+
+            scene.addChild(this)
+        }
+
+        thread {
+            while (true) {
+                if (box.intersects(box2)) {
+                    box.material.diffuse = GLVector(1.0f, 0.0f, 0.0f)
+                } else {
+                    box.material.diffuse = GLVector(0.0f, 1.0f, 0.0f)
+                }
+
+                Thread.sleep(20)
+            }
+        }
+    }
+
+    @Test
+    override fun main() {
+        super.main()
+    }
+}
