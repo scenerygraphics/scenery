b177f3145554f7b210c607ad8047547245e7d01d
Ulrik Guenther <hello@ulrik.is>
2018-06-20 10:56:20 +0200

    SceneryWindow: Document class better

--- src/main/kotlin/graphics/scenery/backends/SceneryWindow.kt
+++ src/main/kotlin/graphics/scenery/backends/SceneryWindow.kt
@@ -11,30 +11,49 @@ import org.lwjgl.glfw.GLFW.*
  * @author Ulrik Günther <hello@ulrik.is>
  */
 sealed class SceneryWindow {
+    /** The default window state, before it becomes initialized to a specific window kind */
     class UninitializedWindow : SceneryWindow()
+    /** GLFW window, with [window] being the pointer to GLFW's window object. */
     class GLFWWindow(var window: Long): SceneryWindow()
+    /** ClearGL (JOGL) window, with [window] being the reference to a [cleargl.ClearGLWindow]. */
     class ClearGLWindow(var window: cleargl.ClearGLWindow): SceneryWindow()
+    /** JavaFX window or stage, with [panel] being the [SceneryPanel] scenery will render to. */
     class JavaFXStage(var panel: SceneryPanel): SceneryWindow()
+    /** Headless window with no chrome whatsoever. */
     class HeadlessWindow : SceneryWindow()
 
+    /** Whether the window should be closed on the next main loop iteration. */
     var shouldClose = false
 
+    /** Window width, can only be set from package-internal functions. */
     var width = 0
+        internal set
+    /** Window height, can only be set from package-internal functions. */
     var height = 0
+        internal set
+    /** Window fullscreen state, can only be set from package-internal functions. */
     var isFullscreen = false
+        internal set
 
+    /**
+     * Sets the title of this window to [title].
+     */
     fun setTitle(title: String) {
         when(this) {
             is UninitializedWindow -> {}
             is GLFWWindow -> glfwSetWindowTitle(window, title)
             is ClearGLWindow -> window.windowTitle = title
             is JavaFXStage -> {
-                Platform.runLater { (panel.scene.window as Stage).title = title }
+                Platform.runLater { (panel.scene.window as? Stage)?.title = title }
             }
             is HeadlessWindow -> {}
         }
     }
 
+    /**
+     * Poll events function, in case the window system requires event polling.
+     * (Only the case for GLFW so far)
+     */
     fun pollEvents() {
         if(this is GLFWWindow) {
             if (glfwWindowShouldClose(window)) {
