4ff8dd52c2c49d4d2b1669b3f212a8bb5a4df5ca
Ulrik Guenther <hello@ulrik.is>
2018-06-21 19:47:35 +0200

    OpenGLRenderer: Improve documentation

--- src/main/kotlin/graphics/scenery/backends/opengl/OpenGLRenderpass.kt
+++ src/main/kotlin/graphics/scenery/backends/opengl/OpenGLRenderpass.kt
@@ -9,23 +9,36 @@ import graphics.scenery.utils.StickyBoolean
 import java.util.concurrent.ConcurrentHashMap
 
 /**
- * Created by ulrik on 7/14/2017.
+ * Class to contain an OpenGL render pass with name [passName] and associated configuration
+ * [passConfig].
+ *
+ * @author Ulrik Guenther <hello@ulrik.is>
  */
-
-
 class OpenGLRenderpass(var passName: String = "", var passConfig: RenderConfigReader.RenderpassConfig) {
     private val logger by LazyLogger()
 
+    /** OpenGL metadata */
     var openglMetadata: OpenGLMetadata = OpenGLMetadata()
+    /** Output(s) of the pass */
     var output = ConcurrentHashMap<String, GLFramebuffer>()
+    /** Inputs of the pass */
     var inputs = ConcurrentHashMap<String, GLFramebuffer>()
+    /** The default shader the pass uses */
     var defaultShader: OpenGLShaderProgram? = null
+    /** UBOs required by this pass */
     var UBOs = ConcurrentHashMap<String, OpenGLUBO>()
 
+    /** Class to store 2D rectangles with [width], [height] and offsets [offsetX] and [offsetY] */
     data class Rect2D(var width: Int = 0, var height: Int = 0, var offsetX: Int = 0, var offsetY: Int = 0)
+    /** Class to store viewport information, [area], and minimal/maximal depth coordinates ([minDepth] and [maxDepth]). */
     data class Viewport(var area: Rect2D = Rect2D(), var minDepth: Float = 0.0f, var maxDepth: Float = 1.0f)
+    /** Class to store clear values for color targets ([clearColor]) and depth targets ([clearDepth]) */
     data class ClearValue(var clearColor: GLVector = GLVector(0.0f, 0.0f, 0.0f, 1.0f), var clearDepth: Float = 0.0f)
 
+    /**
+     * OpenGL metadata class, storing [scissor] areas, [renderArea]s, [clearValues], [viewports], and
+     * for which [eye] this metadata is valid.
+     */
     data class OpenGLMetadata(
         var scissor: Rect2D = Rect2D(),
         var renderArea: Rect2D = Rect2D(),
@@ -34,7 +47,11 @@ class OpenGLRenderpass(var passName: String = "", var passConfig: RenderConfigRe
         var eye: Int = 0
     )
 
-    fun initializeShaderParameters(settings: Settings, backingBuffer: OpenGLRenderer.OpenGLBuffer){
+    /**
+     * Initialises shader parameters for this pass from [settings], which will be serialised
+     * into [backingBuffer].
+     */
+    fun initializeShaderParameters(settings: Settings, backingBuffer: OpenGLRenderer.OpenGLBuffer) {
         passConfig.parameters?.let { params ->
             val ubo = OpenGLUBO(backingBuffer)
 
@@ -71,6 +88,11 @@ class OpenGLRenderpass(var passName: String = "", var passConfig: RenderConfigRe
         }
     }
 
+    /**
+     * Updates previously set-up shader parameters.
+     *
+     * Returns true if the parameters have been updated, and false if not.
+     */
     fun updateShaderParameters(): Boolean {
         var updated: Boolean by StickyBoolean(false)
 
