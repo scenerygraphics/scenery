350650ef89d3d839a83ae8a591c0b67a73d7708d
Ulrik Guenther <guenther@mpi-cbg.de>
2018-06-22 15:11:40 +0200

    Renderdoc: Improve documentation

--- src/main/kotlin/graphics/scenery/utils/Renderdoc.kt
+++ src/main/kotlin/graphics/scenery/utils/Renderdoc.kt
@@ -6,28 +6,30 @@ import com.sun.jna.ptr.PointerByReference
 import com.sun.jna.win32.StdCallLibrary
 
 /**
- * Minimal Renderdoc integration. At the moment, only loads the Renderdoc library into
- * the processes' address space. Has to be run before any renderers are initialiased.
- *
- * @author Ulrik Guenther <hello@ulrik.is>
+ * Interface for the Windows version of the Renderdoc library.
  */
-
 interface RenderdocWindowsLibrary : StdCallLibrary {
     fun RENDERDOC_GetAPI(version: Int, out: PointerByReference): Int
 
     companion object {
-        val instance = Native.loadLibrary("renderdoc", RenderdocWindowsLibrary::class.java) as RenderdocWindowsLibrary
+        val instance = Native.loadLibrary("renderdoc", RenderdocWindowsLibrary::class.java) as? RenderdocWindowsLibrary
+            ?: throw IllegalStateException("Could not match interfaces to Renderdoc library (renderdoc.dll)")
     }
 }
 
+/**
+ * Interface for the Linux version of the Renderdoc library.
+ */
 interface RenderdocLinuxLibrary : Library {
     fun RENDERDOC_GetAPI(version: Int, out: PointerByReference): Int
 
     companion object {
-        val instance = Native.loadLibrary("renderdoc", RenderdocLinuxLibrary::class.java) as RenderdocLinuxLibrary
+        val instance = Native.loadLibrary("renderdoc", RenderdocLinuxLibrary::class.java) as? RenderdocLinuxLibrary
+            ?: throw IllegalStateException("Could not match interfaces to Renderdoc library (librenderdoc.so)")
     }
 }
 
+/** These enums represent the different Renderdoc versions */
 enum class RenderdocVersion(val versionNumber: Int) {
     eRENDERDOC_API_Version_1_0_0(10000),    // RENDERDOC_API_1_0_0 = 1 00 00
     eRENDERDOC_API_Version_1_0_1(10001),    // RENDERDOC_API_1_0_1 = 1 00 01
@@ -36,6 +38,12 @@ enum class RenderdocVersion(val versionNumber: Int) {
     eRENDERDOC_API_Version_1_1_1(10101),    // RENDERDOC_API_1_1_1 = 1 01 01
 }
 
+/**
+ * Minimal Renderdoc integration. At the moment, only loads the Renderdoc library into
+ * the processes' address space. Has to be run before any renderers are initialiased.
+ *
+ * @author Ulrik Guenther <hello@ulrik.is>
+ */
 class Renderdoc : AutoCloseable {
     private val logger by LazyLogger()
 
@@ -68,11 +76,13 @@ class Renderdoc : AutoCloseable {
         }
     }
 
+    /** Closes this Renderdoc interface. */
     override fun close() {
         logger.debug("Closing Renderdoc")
     }
 
     companion object {
+        /** Check whether Renderdoc is attached to this scenery instance. */
         var renderdocAttached: Boolean = false
     }
 }
