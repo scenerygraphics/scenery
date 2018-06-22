f1cea80ca7401af287b1548b0b6bae1db50726cb
Ulrik Guenther <guenther@mpi-cbg.de>
2018-06-22 15:22:23 +0200

    GPUStats: Document interface

--- src/main/kotlin/graphics/scenery/utils/GPUStats.kt
+++ src/main/kotlin/graphics/scenery/utils/GPUStats.kt
@@ -3,12 +3,30 @@ package graphics.scenery.utils
 import java.util.*
 
 /**
- * Created by ulrik on 2/1/2017.
+ * Interface for collection of GPU statistics.
+ *
+ * @author Ulrik Guenther <hello@ulrik.is>
  */
 interface GPUStats {
     val utilisations: HashMap<String, Float>
+
+    /**
+     * Updates the state of the GPU statistics for a GPU with index [gpuIndex].
+     */
     fun update(gpuIndex: Int)
+
+    /**
+     * Returns a specific statistic with [name].
+     */
     fun get(name: String): Float
+
+    /**
+     * Returns the total GPU utilisation as String.
+     */
     fun utilisationToString(): String
+
+    /**
+     * Returns the total GPU memory utilisation as String.
+     */
     fun memoryUtilisationToString(): String
 }
