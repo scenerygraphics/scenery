9baf2fe67ebb4fc566924224a0cf3906b80dad97
Ulrik Guenther <guenther@mpi-cbg.de>
2018-06-22 15:06:27 +0200

    LazyLogger, ShaderCompilationException: Improve documentation

--- src/main/kotlin/graphics/scenery/utils/LazyLogger.kt
+++ src/main/kotlin/graphics/scenery/utils/LazyLogger.kt
@@ -4,14 +4,6 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import kotlin.reflect.full.companionObject
 
-/**
- * Loggable - interface to flexibly return correct logger names for classes implementing this interface
- *
- * Heavily inspired by [Idiomatic way of logging in Kotlin](https://stackoverflow.com/questions/34416869/idiomatic-way-of-logging-in-kotlin).
- *
- * @author Ulrik Guenther <hello@ulrik.is>
- */
-
 private fun <T : Any> unwrapCompanionClass(ofClass: Class<T>): Class<*> {
     return if (ofClass.enclosingClass != null && ofClass.enclosingClass.kotlin.companionObject?.java == ofClass) {
         ofClass.enclosingClass
@@ -20,6 +12,13 @@ private fun <T : Any> unwrapCompanionClass(ofClass: Class<T>): Class<*> {
     }
 }
 
+/**
+ * LazyLogger - Extension function to flexibly return correct logger names for classes implementing this interface
+ *
+ * Heavily inspired by [Idiomatic way of logging in Kotlin](https://stackoverflow.com/questions/34416869/idiomatic-way-of-logging-in-kotlin).
+ *
+ * @author Ulrik Guenther <hello@ulrik.is>
+ */
 fun <R : Any> R.LazyLogger(): Lazy<Logger> {
     System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", System.getProperty("scenery.LogLevel", "info"))
     return lazyOf(LoggerFactory.getLogger(unwrapCompanionClass(this.javaClass).simpleName))
