7d809f9887181dbb690c2b49a335ca51ad1a4902
Ulrik Günther <ulrik@42degreesoffreedom.com>
2018-06-22 19:08:13 +0200

    VolumeEffector/SlicingVolumeEffector: Add documentation

--- src/main/kotlin/graphics/scenery/effectors/SlicingVolumeEffector.kt
+++ src/main/kotlin/graphics/scenery/effectors/SlicingVolumeEffector.kt
@@ -1,4 +1,15 @@
 package graphics.scenery.effectors
 
+import cleargl.GLVector
+import graphics.scenery.Node
+import graphics.scenery.Plane
+
+/**
+ * Slicing volume effector
+ *
+ * @author Ulrik Guenther <hello@ulrik.is>
+ */
 class SlicingVolumeEffector : VolumeEffector() {
+    /** Proxy plane for slicing */
+    override var proxy: Node = Plane(GLVector(1.0f, 1.0f, 1.0f))
 }
--- src/main/kotlin/graphics/scenery/effectors/VolumeEffector.kt
+++ src/main/kotlin/graphics/scenery/effectors/VolumeEffector.kt
@@ -9,12 +9,15 @@ import graphics.scenery.volumes.Volume
  * @author Ulrik Guenther <hello@ulrik.is>
  */
 open class VolumeEffector : Node("VolumeEffector") {
+    /** Whether this effector node is currently active */
     var active: Boolean = false
         private set
 
+    /** The volume this effector is associated with */
     var activeVolume: Node? = null
         private set
 
+    /** Proxy node to display e.g. auxiliary geometry. */
     open var proxy = Node()
 
     init {
