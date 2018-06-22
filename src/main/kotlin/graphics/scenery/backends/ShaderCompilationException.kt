1d9cd0ffed6fcaa508f0baf5359f6671b2fd6b70
Ulrik Guenther <guenther@mpi-cbg.de>
2018-06-22 15:06:27 +0200

    LazyLogger, ShaderCompilationException: Improve documentation

--- src/main/kotlin/graphics/scenery/backends/ShaderCompilationException.kt
+++ src/main/kotlin/graphics/scenery/backends/ShaderCompilationException.kt
@@ -1,3 +1,9 @@
 package graphics.scenery.backends
 
+/**
+ * Exception to throw when a problem occurs during shader compilation. [message] may specify the
+ * problem in more detail.
+ *
+ * @author Ulrik Guenther <hello@ulrik.is>
+ */
 class ShaderCompilationException(message: String = "Shader compilation failed") : Exception(message)
