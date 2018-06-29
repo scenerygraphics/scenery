0252c20bf703d961981cc5f2f19db898abfdb2b6
Ulrik Günther <ulrik@42degreesoffreedom.com>
2018-06-29 18:49:56 +0200

    OpenCLContext: Add documentation

--- src/main/kotlin/graphics/scenery/compute/OpenCLContext.kt
+++ src/main/kotlin/graphics/scenery/compute/OpenCLContext.kt
@@ -1,13 +1,11 @@
 package graphics.scenery.compute
 
-import org.jocl.*
-import org.jocl.CL.*
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import graphics.scenery.Hub
 import graphics.scenery.Hubable
 import graphics.scenery.SceneryElement
 import graphics.scenery.utils.LazyLogger
+import org.jocl.*
+import org.jocl.CL.*
 import java.io.File
 import java.net.URL
 import java.nio.Buffer
@@ -18,12 +16,12 @@ import java.nio.charset.StandardCharsets
 import java.util.concurrent.ConcurrentHashMap
 
 /**
- * <Description>
+ * OpenCL context creation and handling.
  *
  * @author Ulrik Günther <hello@ulrik.is>
  */
 
-class OpenCLContext(override var hub: Hub?, val devicePreference: String = System.getProperty("scenery.OpenCLDevice", "0,0")) : Hubable {
+class OpenCLContext(override var hub: Hub?, devicePreference: String = System.getProperty("scenery.OpenCLDevice", "0,0")) : Hubable {
     private val logger by LazyLogger()
 
     var device: cl_device_id
@@ -38,9 +36,8 @@ class OpenCLContext(override var hub: Hub?, val devicePreference: String = Syste
         val devicePref = devicePreference.substringAfter(",").toInt()
 
         val deviceType = CL_DEVICE_TYPE_GPU
-        val deviceIndex = devicePref
         // Enable exceptions and subsequently omit error checks in this sample
-        setExceptionsEnabled(true);
+        setExceptionsEnabled(true)
 
         // Obtain a platform ID
         val platforms = query<cl_platform_id> { l, a, n ->
@@ -56,59 +53,81 @@ class OpenCLContext(override var hub: Hub?, val devicePreference: String = Syste
         val devices = query({ l, a, n ->
             clGetDeviceIDs(platform, deviceType, l, a, n)
         }, { cl_device_id() })
-        device = devices[deviceIndex]
+        device = devices[devicePref]
 
 		logger.info("Selected device: ${getString(device, CL_DEVICE_NAME)} running ${getString(device, CL_DEVICE_VERSION)}")
 
         // Create a context for the selected device
         context = clCreateContext(
                 contextProperties, 1, arrayOf(device),
-                null, null, null);
+                null, null, null)
 
         // Create a command-queue for the selected device
         @Suppress("DEPRECATION")
         queue = clCreateCommandQueue(context, device, 0, null)
     }
 
+    /**
+     * Returns the device info for [device], specifically the parameter
+     * named [paramName].
+     */
     fun getString(device: cl_device_id, paramName: Int): String
     {
         // Obtain the length of the string that will be queried
         val size = LongArray(1)
-        clGetDeviceInfo(device, paramName, 0, null, size);
+        clGetDeviceInfo(device, paramName, 0, null, size)
 
         // Create a buffer of the appropriate size and fill it with the info
         val buffer = ByteArray(size[0].toInt())
-        clGetDeviceInfo(device, paramName, buffer.size.toLong(), Pointer.to(buffer), null);
+        clGetDeviceInfo(device, paramName, buffer.size.toLong(), Pointer.to(buffer), null)
 
         // Create a string from the buffer (excluding the trailing \0 byte)
-        return String(buffer, 0, buffer.size-1);
+        return String(buffer, 0, buffer.size-1)
     }
 
+    /**
+     * Loads an OpenCL kernel from a String [source], storing it under [name], and returning
+     * the compiled kernel.
+     */
+    @Suppress("unused")
     fun loadKernel(source: String, name: String): OpenCLContext {
         if(!kernels.containsKey(name)) {
             // Create the program from the source code
-            val program = clCreateProgramWithSource(context, 1, arrayOf(source), null, null);
+            val program = clCreateProgramWithSource(context, 1, arrayOf(source), null, null)
 
             // Build the program
-            clBuildProgram(program, 0, null, null, null, null);
+            clBuildProgram(program, 0, null, null, null, null)
 
             // Create the kernel
-            val kernel = clCreateKernel(program, name, null);
+            val kernel = clCreateKernel(program, name, null)
             kernels.put(name, kernel)
         }
 
         return this
     }
 
+    /**
+     * Loads an OpenCL kernel from a file [source], storing it under [name], and returning
+     * the compiled kernel.
+     */
+    @Suppress("unused")
     fun loadKernel(source: File, name: String): OpenCLContext {
         return loadKernel(source.readLines(charset = Charsets.UTF_8).joinToString("\n"), name)
 
     }
 
+    /**
+     * Loads an OpenCL kernel from a URL [source], storing it under [name], and returning
+     * the compiled kernel.
+     */
+    @Suppress("unused")
     fun loadKernel(source: URL, name: String): OpenCLContext {
         return loadKernel(String(source.readBytes(), StandardCharsets.UTF_8), name)
     }
 
+    /**
+     * Returns the OpenCL size of different JVM objects as Long.
+     */
     protected fun getSizeof(obj: Any): Long {
         return when(obj.javaClass) {
             Float::class.java   -> Sizeof.cl_float
@@ -134,42 +153,44 @@ class OpenCLContext(override var hub: Hub?, val devicePreference: String = Syste
         }.toLong()
     }
 
+    /**
+     * Sets arguments for a specific OpenCL kernel.
+     */
     protected fun cl_kernel.setArgs(vararg arguments: Any) {
         arguments.forEachIndexed { i, arg ->
-            if(arg is NativePointerObject) {
-                clSetKernelArg(this,
-                        i,
-                        getSizeof(arg),
-                        Pointer.to(arg))
-            } else if(arg is Buffer) {
-                clSetKernelArg(this,
-                        i,
-                        getSizeof(arg),
-                        Pointer.to(arg))
-            } else if(arg is cl_mem) {
-                clSetKernelArg(this,
-                        i,
-                        getSizeof(arg),
-                        Pointer.to(arg))
-            } else if(arg is Int) {
-                clSetKernelArg(this,
-                        i,
-                        getSizeof(arg),
-                        Pointer.to(arrayOf(arg).toIntArray()))
-            } else if(arg is Float) {
-                clSetKernelArg(this,
-                        i,
-                        getSizeof(arg),
-                        Pointer.to(arrayOf(arg).toFloatArray()))
-            } else if(arg is Byte) {
-                clSetKernelArg(this,
-                        i,
-                        getSizeof(arg),
-                        Pointer.to(arrayOf(arg).toByteArray()))
+            when (arg) {
+                is NativePointerObject -> clSetKernelArg(this,
+                    i,
+                    getSizeof(arg),
+                    Pointer.to(arg))
+                is Buffer -> clSetKernelArg(this,
+                    i,
+                    getSizeof(arg),
+                    Pointer.to(arg))
+                is cl_mem -> clSetKernelArg(this,
+                    i,
+                    getSizeof(arg),
+                    Pointer.to(arg))
+                is Int -> clSetKernelArg(this,
+                    i,
+                    getSizeof(arg),
+                    Pointer.to(arrayOf(arg).toIntArray()))
+                is Float -> clSetKernelArg(this,
+                    i,
+                    getSizeof(arg),
+                    Pointer.to(arrayOf(arg).toFloatArray()))
+                is Byte -> clSetKernelArg(this,
+                    i,
+                    getSizeof(arg),
+                    Pointer.to(arrayOf(arg).toByteArray()))
             }
         }
     }
 
+    /**
+     * Runs the kernel specified by [name] with a number of [wavefronts], passing
+     * [arguments] to the kernel.
+     */
     fun runKernel(name: String, wavefronts: Int, vararg arguments: Any) {
         val k: cl_kernel = kernels[name]!!
         k.setArgs(*arguments)
@@ -180,32 +201,40 @@ class OpenCLContext(override var hub: Hub?, val devicePreference: String = Syste
 
         // Execute the kernel
         clEnqueueNDRangeKernel(this.queue, k, 1, null,
-                global_work_size, local_work_size, 0, null, null);
+                global_work_size, local_work_size, 0, null, null)
 
     }
 
+    /**
+     * Wraps a [buffer] for use with a kernel. The buffer can be defined as [readonly].
+     */
     fun wrapInput(buffer: Buffer, readonly: Boolean = false): cl_mem {
         buffer.rewind()
 
         val p = Pointer.to(buffer)
         var flags = if(readonly) CL_MEM_READ_ONLY else CL_MEM_READ_WRITE
         flags = flags or CL_MEM_COPY_HOST_PTR
-        val mem = clCreateBuffer(this.context, flags.toLong(), getSizeof(buffer)*buffer.remaining(), p, null)
+        val mem = clCreateBuffer(this.context, flags, getSizeof(buffer)*buffer.remaining(), p, null)
 
         return mem
     }
 
+    /**
+     * Wraps an output [buffer].
+     */
     fun wrapOutput(buffer: Buffer): cl_mem {
         buffer.rewind()
         val flags = CL_MEM_READ_WRITE
-        val mem = clCreateBuffer(this.context, flags.toLong(), getSizeof(buffer)*buffer.remaining(), null, null)
 
-        return mem
+        return clCreateBuffer(context, flags, getSizeof(buffer) *buffer.remaining(), null, null)
     }
 
+    /**
+     * Reads from OpenCL memory specified by [memory] into the [Buffer] [target].
+     */
     fun readBuffer(memory: cl_mem, target: Buffer) {
         val p = Pointer.to(target)
-        clEnqueueReadBuffer(queue, memory, CL_TRUE, 0, target.remaining() * getSizeof(target), p, 0, null, null);
+        clEnqueueReadBuffer(queue, memory, CL_TRUE, 0, target.remaining() * getSizeof(target), p, 0, null, null)
     }
 
 
@@ -235,7 +264,7 @@ class OpenCLContext(override var hub: Hub?, val devicePreference: String = Syste
          *      T initializer for creating an Array<T>
          */
         inline fun <reified T> query(fn: (Int, Array<T>?, IntArray?) -> Unit, noinline init: (Int) -> T): Array<T> {
-            val num = IntArray(1);
+            val num = IntArray(1)
             fn(0, null, num)
             val things = Array<T>(num[0], init)
             fn(num[0], things, null)
@@ -246,7 +275,7 @@ class OpenCLContext(override var hub: Hub?, val devicePreference: String = Syste
          * Variant of [query] without array initializer. It creates arrays with nullable entries, and returns Array<T?> instead of Array<T>.
          */
         inline fun <reified T> query(fn: (Int, Array<T?>?, IntArray?) -> Unit): Array<T?> {
-            val num = IntArray(1);
+            val num = IntArray(1)
             fn(0, null, num)
             val things = arrayOfNulls<T>(num[0])
             fn(num[0], things, null)
