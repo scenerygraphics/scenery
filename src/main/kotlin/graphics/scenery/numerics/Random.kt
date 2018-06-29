26abe5f797ab5c82e5dd726d841dfbc842f7f2a2
Ulrik Günther <ulrik@42degreesoffreedom.com>
2018-06-29 18:05:39 +0200

    Random: add documentation

--- src/main/kotlin/graphics/scenery/numerics/Random.kt
+++ src/main/kotlin/graphics/scenery/numerics/Random.kt
@@ -4,20 +4,30 @@ import cleargl.GLVector
 import com.jogamp.opengl.math.Quaternion
 
 /**
- * <Description>
+ * Helper class to generate random numbers.
  *
  * @author Ulrik Günther <hello@ulrik.is>
  */
 class Random {
     companion object {
+        /**
+         * Returns a random float from the range [min]-[max].
+         */
         @JvmStatic
         fun randomFromRange(min: Float, max: Float): Float = (max - min)*Math.random().toFloat() + min
 
+        /**
+         * Returns a random [GLVector] with given [dimension], where all elements are in the
+         * range [min]-[max].
+         */
         @JvmStatic
         fun randomVectorFromRange(dimension: Int, min: Float, max: Float): GLVector {
             return GLVector(*(1..dimension).map { randomFromRange(min, max) }.toFloatArray())
         }
 
+        /**
+         * Returns a random [Quaternion] generated from random Euler angles.
+         */
         @JvmStatic
         fun randomQuaternion(): Quaternion {
             val values = (0..2).map { randomFromRange(0.0f, 1.0f) }
