cf52bdfc95c458a1df646a3534738b4454d93040
Ulrik Guenther <guenther@mpi-cbg.de>
2018-07-05 14:18:59 +0200

    Eye tracking fixes (WIP)

--- src/main/kotlin/graphics/scenery/Camera.kt
+++ src/main/kotlin/graphics/scenery/Camera.kt
@@ -145,19 +145,27 @@ open class Camera : Node("Camera") {
      * @param v - The vector to be transformed into world space.
      * @return GLVector - [v] transformed into world space.
      */
-    @JvmOverloads fun viewportToWorld(v: GLVector, offset: Float = 0.01f): GLVector {
-        val unproject = projection.clone()
-        unproject.mult(getTransformation())
-        unproject.invert()
+    @JvmOverloads fun viewportToWorld(vector: GLVector, offset: Float = 0.01f): GLVector {
+        val view = (target - position).normalize()
+        var h = view.cross(up).normalize()
+        var v = h.cross(view)
 
-        val clipSpace = unproject.mult(when (v.dimension) {
-            1 -> GLVector(v.x(), 1.0f, nearPlaneDistance + offset, 1.0f)
-            2 -> GLVector(v.x(), v.y(), nearPlaneDistance + offset, 1.0f)
-            3 -> GLVector(v.x(), v.y(), v.z(), 1.0f)
-            else -> v
-        })
+        val aspect = 1.0f/projection.get(0, 0)
 
-        return clipSpace.xyz() * (1.0f/clipSpace.w())
+        val fov = fov * Math.PI / 180.0f
+        val lengthV = Math.tan(fov / 2.0).toFloat() * nearPlaneDistance
+        val lengthH = lengthV * aspect
+
+        v *= lengthV
+        h *= lengthH
+
+        val posX = (vector.x() - 0.5f) * 2.0f
+        val posY = -1.0f * (vector.y() - 0.5f) * 2.0f
+
+        val worldPos = position + view * nearPlaneDistance + h * posX + v * posY
+        val worldDir = (worldPos - position).normalized
+
+        return worldPos + worldDir * (if(vector.dimension > 2 ) { vector.z() } else { offset })
     }
 }
 
