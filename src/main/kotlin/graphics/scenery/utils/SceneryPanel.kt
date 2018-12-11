b8d4ede61b2a9fa1a4b573f555e7c4be71e5ea7e
Ulrik Günther <ulrik@42degreesoffreedom.com>
2018-12-11 12:32:19 +0100

    SceneryPanel/SceneryJPanel/SceneryFXPanel: Update docs

--- src/main/kotlin/graphics/scenery/utils/SceneryPanel.kt
+++ src/main/kotlin/graphics/scenery/utils/SceneryPanel.kt
@@ -1,15 +1,32 @@
 package graphics.scenery.utils
 
-import graphics.scenery.backends.Renderer
 import java.nio.ByteBuffer
 
+/**
+ * Interface for embeddable windows scenery can render to.
+ *
+ * @author Ulrik Guenther <hello@ulrik.is>
+ */
 interface SceneryPanel {
+
+    /** Number of frames displayed so far. */
     var displayedFrames: Long
+
+    /** Image scaling in Y, can be used to flip the image. */
     var imageScaleY: Float
+
+    /** Current width of the panel. */
     var panelWidth: Int
+
+    /** Current height of the panel. */
     var panelHeight: Int
+
+    /** Refresh rate. */
     var refreshRate: Int
 
+    /** Updates the backing byte buffer of the image. */
     fun update(buffer: ByteBuffer, id: Int = -1)
+
+    /** Sets the preferred dimensions of the panel. */
     fun setPreferredDimensions(w: Int, h: Int)
 }
