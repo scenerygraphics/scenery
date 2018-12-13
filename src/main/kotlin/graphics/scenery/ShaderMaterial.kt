01aa0e1934b5fad110351536880045ea7fc603f7
Ulrik Günther <ulrik@42degreesoffreedom.com>
2018-12-13 14:53:59 +0100

    ShaderMaterial: Add @JvmStatic annotation to factory methods

--- src/main/kotlin/graphics/scenery/ShaderMaterial.kt
+++ src/main/kotlin/graphics/scenery/ShaderMaterial.kt
@@ -15,12 +15,12 @@ class ShaderMaterial(var shaders: Shaders) : Material() {
     companion object {
 
         /** Creates a new file-based ShaderMaterial from a list of [files]. */
-        fun fromFiles(vararg files: String): ShaderMaterial {
+        @JvmStatic fun fromFiles(vararg files: String): ShaderMaterial {
             return ShaderMaterial(Shaders.ShadersFromFiles(files.toList().toTypedArray()))
         }
 
         /** Creates a new file-based ShaderMaterial the simpleName of the class [clazz]. */
-        @JvmOverloads fun fromClass(clazz: Class<*>, types: List<ShaderType> = listOf(ShaderType.VertexShader, ShaderType.FragmentShader)): ShaderMaterial {
+        @JvmStatic @JvmOverloads fun fromClass(clazz: Class<*>, types: List<ShaderType> = listOf(ShaderType.VertexShader, ShaderType.FragmentShader)): ShaderMaterial {
             return ShaderMaterial(Shaders.ShadersFromClassName(clazz, types))
         }
     }
