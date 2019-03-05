726070837c1d52f42413d98fbbcd54151b2a66b4
Vladimír Ulman <ulman@mpi-cbg.de>
2019-03-05 18:50:28 +0100

    ADD: example code

---
+++ src/test/tests/graphics/scenery/tests/examples/basic/ArrowExample.kt
@@ -0,0 +1,129 @@
+package graphics.scenery.tests.examples.basic
+
+import cleargl.GLVector
+import graphics.scenery.*
+import graphics.scenery.backends.Renderer
+import graphics.scenery.numerics.Random
+import org.junit.Test
+import kotlin.concurrent.thread
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
+
+/**
+ * Simple example to demonstrate the drawing of 3D arrows/vectors.
+ *
+ * This example will draw a circle made of vectors using
+ * the [Arrow] class. One vector will, however, always
+ * illuminate differently, and the position of such will
+ * circulate...
+ *
+ * @author Vladimir Ulman <ulman@mpi-cbg.de>
+ */
+class ArrowExample : SceneryBase("ArrowExample") {
+    protected var lineAnimating = true
+
+    override fun init() {
+        renderer = Renderer.createRenderer(hub, applicationName, scene, windowWidth, windowHeight)
+        hub.add(SceneryElement.Renderer, renderer!!)
+
+        val hull = Box(GLVector(50.0f, 50.0f, 50.0f), insideNormals = true)
+        hull.material.diffuse = GLVector(0.2f, 0.2f, 0.2f)
+        hull.material.cullingMode = Material.CullingMode.Front
+        scene.addChild(hull)
+
+        //we shall have faint and bright vectors...
+        val matBright = Material()
+        matBright.diffuse  = GLVector(0.0f, 1.0f, 0.0f)
+        matBright.ambient  = GLVector(1.0f, 1.0f, 1.0f)
+        matBright.specular = GLVector(1.0f, 1.0f, 1.0f)
+        matBright.cullingMode = Material.CullingMode.None
+
+        val matFaint = Material()
+        matFaint.diffuse  = GLVector(0.0f, 0.6f, 0.6f)
+        matFaint.ambient  = GLVector(1.0f, 1.0f, 1.0f)
+        matFaint.specular = GLVector(1.0f, 1.0f, 1.0f)
+        matFaint.cullingMode = Material.CullingMode.None
+
+
+        //... arranged along a circle
+        val circleCentre = GLVector(0.0f,3)
+        val circleRadius = 6.0f
+
+        val arrowsInCircle = 30
+
+
+        //create the circle of vectors
+        val al = arrayOfNulls<Arrow>(arrowsInCircle)
+        var lastPos = GLVector(circleRadius,0f,0f)
+        var currPos : GLVector
+        for (i in 1..arrowsInCircle)
+        {
+            val curAng = (i*2.0f* PI/arrowsInCircle).toFloat()
+            currPos = GLVector(circleRadius*cos(curAng) +circleCentre.x(),
+                               circleRadius*sin(curAng) +circleCentre.y(),
+                               circleCentre.z())
+
+            // ========= this is how you create an Arrow =========
+            val a = Arrow(currPos.minus(lastPos))  //shape of the vector itself
+            a.position = lastPos                   //position/base of the vector
+            a.material = matFaint                  //usual stuff follows...
+            a.edgeWidth = 0.5f
+            scene.addChild(a)
+
+            // if you want to change the shape and position of the vector later,
+            // you can use this (instead of creating a new vector)
+            // a.reshape( newVector )
+            // a.position = newBase
+            // ========= this is how you create an Arrow =========
+
+            al[i-1] = a
+
+            lastPos = currPos
+        }
+
+
+        //create the scene: lights and camera
+        var pl = arrayOfNulls<PointLight>(4)
+        for (i in 1..pl.size)
+        {
+            val l = PointLight(radius = 200.0f)
+            l.intensity = 600.0f
+            l.emissionColor = GLVector(1.0f,3)
+
+            scene.addChild(l)
+            pl[i-1] = l
+        }
+        pl[0]!!.position = GLVector(0f,10f,0f)
+        pl[1]!!.position = GLVector(0f,-10f,0f)
+        pl[2]!!.position = GLVector(-10f,0f,0f)
+        pl[3]!!.position = GLVector(10f,0f,0f)
+
+        val cam: Camera = DetachedHeadCamera()
+        cam.position = GLVector(0.0f, 0.0f, 15.0f)
+        cam.perspectiveCamera(50.0f, windowWidth.toFloat(), windowHeight.toFloat())
+        cam.active = true
+        scene.addChild(cam)
+
+
+        //have some fun...
+        thread {
+            var i = 0
+            while (true) {
+                al[i]!!.material = matFaint
+                i = (i+1).mod(arrowsInCircle)
+                al[i]!!.material = matBright
+
+                Thread.sleep(150)
+            }
+        }
+    }
+
+    override fun inputSetup() {
+        setupCameraModeSwitching(keybinding = "C")
+    }
+
+    @Test override fun main() {
+        super.main()
+    }
+}
