06a8ffe80e0e2481bc431cded127cc9f215bb88b
Ulrik Günther <ulrik@42degreesoffreedom.com>
2019-06-07 17:10:56 +0200

    Add tests for Box, Cylinder, Icosphere and PointLight

---
+++ src/test/tests/graphics/scenery/tests/unit/PointLightTests.kt
@@ -0,0 +1,23 @@
+package graphics.scenery.tests.unit
+
+import graphics.scenery.PointLight
+import graphics.scenery.Scene
+import graphics.scenery.numerics.Random
+import org.junit.Test
+import kotlin.test.assertEquals
+
+/**
+ * Tests for [PointLight].
+ *
+ * @author Ulrik Günther <hello@ulrik.is>
+ */
+class PointLightTests {
+    @Test
+    fun testCreation() {
+        val s = Scene()
+        val size = Random.randomFromRange(1.0f, 500.0f)
+        val pl = PointLight(size)
+
+        assertEquals(size, pl.lightRadius)
+    }
+}
