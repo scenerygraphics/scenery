f1d9ea201b9fc020e5811c5ad8ec1ba87665ee54
Ulrik Günther <ulrik@42degreesoffreedom.com>
2019-06-07 13:51:20 +0200

    Settings: Fix wrong conversion when pulling settings from system properties and extend unit tests

--- src/test/tests/graphics/scenery/tests/unit/SettingsTests.kt
+++ src/test/tests/graphics/scenery/tests/unit/SettingsTests.kt
@@ -4,7 +4,10 @@ import graphics.scenery.Hub
 import graphics.scenery.Settings
 import graphics.scenery.utils.LazyLogger
 import org.junit.Test
+import kotlin.random.Random
+import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
+import kotlin.test.assertTrue
 
 /**
  * Tests for [Settings].
@@ -28,7 +31,7 @@ class SettingsTests {
         val settings = prepareSettings()
         settings.set("Test.Setting", 1337)
 
-        assert(settings.get<Int>("Test.Setting") == 1337)
+        assertEquals(1337, settings.get<Int>("Test.Setting"))
     }
 
     @Test
@@ -38,10 +41,10 @@ class SettingsTests {
 
         settings.set("Test.Setting", 1337)
         settings.setIfUnset("Test.Setting", 1338)
-        assert(settings.get<Int>("Test.Setting") == 1337)
+        assertEquals(1337, settings.get<Int>("Test.Setting"))
 
         settings.setIfUnset("Test.NewSetting", 1339)
-        assert(settings.get<Int>("Test.NewSetting") == 1339)
+        assertEquals(1339, settings.get<Int>("Test.NewSetting"))
     }
 
     @Test
@@ -60,7 +63,7 @@ class SettingsTests {
         val settings = prepareSettings()
 
         val value = settings.get("Test.MissingSetting", 1337L)
-        assert(value == 1337L)
+        assertEquals(1337L, value)
     }
 
     @Test
@@ -82,10 +85,85 @@ class SettingsTests {
         settings.set("Test.Setting", 1337.0f)
 
         val v = settings.get<Long>("Test.Setting")
-        assert(v == 1337L)
+        assertEquals(1337L, v)
 
         settings.set("Test.Setting", 1337.0)
         val w = settings.get<Float>("Test.Setting")
-        assert(w == 1337.0f)
+        assertEquals(1337.0f, w)
+    }
+
+    @Test
+    fun testSettingsFromSystemProperties() {
+        logger.info("Testing creation of settings from system properties")
+        val nextFloat = Random.nextFloat()
+        val nextInt = Random.nextInt()
+        val nextLong = Random.nextLong()
+        val nextBoolean = Random.nextBoolean()
+        val randomString = Random.nextBytes(32).contentToString()
+
+        System.setProperty("scenery.FloatSetting", nextFloat.toString() + "f")
+        System.setProperty("scenery.IntSetting", nextInt.toString())
+        System.setProperty("scenery.LongSetting", nextLong.toString() + "l")
+        System.setProperty("scenery.BoolSetting", nextBoolean.toString())
+        System.setProperty("scenery.StringSetting", randomString)
+        val settings = prepareSettings()
+
+        assertEquals(nextFloat, settings.get<Float>("FloatSetting"))
+        assertEquals(nextInt, settings.get<Int>("IntSetting"))
+        assertEquals(nextLong, settings.get<Long>("LongSetting"))
+        assertEquals(nextBoolean, settings.get<Boolean>("BoolSetting"))
+        assertEquals(randomString, settings.get<String>("StringSetting"))
+    }
+
+    @Test
+    fun testSettingsListing() {
+        logger.info("Testing settings listing as string...")
+        val settings = prepareSettings()
+
+        val vars = (0 until 10).map {
+            val type = Random.nextInt(0, 5)
+            Random.nextBytes(10).toString() to when(type) {
+                0 -> Random.nextInt()
+                1 -> Random.nextLong()
+                2 -> Random.nextFloat()
+                3 -> Random.nextBoolean()
+                else -> Random.nextBytes(16).toString()
+            }
+        }
+
+        vars.forEach { settings.set(it.first, it.second) }
+
+        val list = settings.list()
+
+        vars.forEach {
+            assertTrue(list.contains(it.first), "List contains key ${it.first}")
+            assertTrue(list.contains(it.second.toString()), "List contains value ${it.second} of key ${it.first}")
+        }
+    }
+
+    @Test
+    fun testSettingsList() {
+        logger.info("Testing listing of settings keys ...")
+        val settings = prepareSettings()
+
+        val vars = (0 until 10).map {
+            val type = Random.nextInt(0, 5)
+            Random.nextBytes(10).toString() to when(type) {
+                0 -> Random.nextInt()
+                1 -> Random.nextLong()
+                2 -> Random.nextFloat()
+                3 -> Random.nextBoolean()
+                else -> Random.nextBytes(16).toString()
+            }
+        }
+
+        vars.forEach { settings.set(it.first, it.second) }
+
+        val list = settings.getAllSettings()
+
+        vars.forEach {
+            assertTrue(list.contains(it.first), "List contains key ${it.first}")
+        }
+
     }
 }
