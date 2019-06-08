7115167ab9a515152488fd7bc091ebb172b7080e
Ulrik Günther <hello@ulrik.is>
2019-06-08 20:52:32 +0200

    Add VideoRecordingExample

---
+++ src/test/tests/graphics/scenery/tests/examples/advanced/VideoRecordingExample.kt
@@ -0,0 +1,82 @@
+package graphics.scenery.tests.examples.advanced
+
+import cleargl.GLVector
+import graphics.scenery.*
+import graphics.scenery.backends.Renderer
+import graphics.scenery.numerics.Random
+import graphics.scenery.tests.examples.basic.TexturedCubeExample
+import org.junit.Test
+import java.io.File
+import kotlin.concurrent.thread
+import kotlin.test.assertTrue
+
+/**
+ * Example to show programmatic video recording.
+ *
+ * @author Ulrik Günther <hello@ulrik.is>
+ */
+class VideoRecordingExample: SceneryBase("VideoRecordingExample") {
+    override fun init() {
+        renderer = hub.add(SceneryElement.Renderer,
+            Renderer.createRenderer(hub, applicationName, scene, 512, 512))
+
+        val box = Box(GLVector(1.0f, 1.0f, 1.0f))
+        with(box) {
+            box.name = "le box du win"
+            box.material.textures.put("diffuse", TexturedCubeExample::class.java.getResource("textures/helix.png").file)
+            scene.addChild(this)
+        }
+
+        val light = PointLight(radius = 15.0f)
+        light.position = GLVector(0.0f, 0.0f, 2.0f)
+        light.intensity = 5.0f
+        light.emissionColor = GLVector(1.0f, 1.0f, 1.0f)
+        scene.addChild(light)
+
+        val cam: Camera = DetachedHeadCamera()
+        with(cam) {
+            position = GLVector(0.0f, 0.0f, 5.0f)
+            perspectiveCamera(50.0f, 512.0f, 512.0f)
+            active = true
+
+            scene.addChild(this)
+        }
+
+        thread {
+            while (true) {
+                box.rotation.rotateByAngleY(Random.randomFromRange(-0.04f, 0.04f))
+                box.rotation.rotateByAngleZ(Random.randomFromRange(-0.04f, 0.04f))
+                box.needsUpdate = true
+
+                Thread.sleep(20)
+            }
+        }
+
+        thread {
+            while(renderer?.firstImageReady == false) {
+                Thread.sleep(50)
+            }
+
+            renderer?.recordMovie("./VideoRecordingExample.mp4")
+            Thread.sleep(5000)
+            renderer?.recordMovie()
+        }
+    }
+
+    @Test override fun main() {
+        // add assertions, these only get called when the example is called
+        // as part of scenery's integration tests
+        assertions[AssertionCheckPoint.AfterClose]?.add {
+            val f = File("./VideoRecordingExample.mp4")
+            try {
+                assertTrue(f.length() > 0, "Size of recorded video is larger than zero.")
+            } finally {
+                if(f.exists()) {
+                    f.delete()
+                }
+            }
+        }
+
+        super.main()
+    }
+}
