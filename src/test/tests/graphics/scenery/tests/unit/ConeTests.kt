079cb4bb486a64a1f8d7ee36e3f33c5aa8cfd81f
Aryaman Gupta <aryaman1994@gmail.com>
2019-06-13 18:42:24 +0200

    Added height test for cone primitive

--- src/test/tests/graphics/scenery/tests/unit/ConeTests.kt
+++ src/test/tests/graphics/scenery/tests/unit/ConeTests.kt
@@ -23,43 +23,23 @@ class ConeTests {
         val s = Scene()
         val radius = Random.randomFromRange(0.1f, 5000.0f)
         val height = Random.randomFromRange(0.1f, 5000.0f)
-        val axis = Random.randomVectorFromRange(3, -1.0f, 1.0f)
-        val segments = kotlin.random.Random.nextInt(1, 10)
+        val segments = kotlin.random.Random.nextInt(1, 5)
+        val axisN = GLVector(0.0f, 1.0f, 0.0f)
+        //TODO: Generalise
 
         val epsilon = 0.0001f
-        val axisN = axis.normalize()
 
-        val c = Cone(radius, height, 4, axisN)
+        val c = Cone(radius, height, 4 * segments, axisN)
         s.addChild(c)
+        c.updateWorld(true)
 
         val bb = c.boundingBox
-        logger.info("min x: ${bb!!.min.x()}")
-        logger.info("min y: ${bb!!.min.y()}")
-        logger.info("min z: ${bb!!.min.z()}")
 
-        logger.info("max x: ${bb!!.max.x()}")
-        logger.info("max y: ${bb!!.max.y()}")
-        logger.info("max z: ${bb!!.max.z()}")
-
-        logger.info("height is $height")
-        logger.info("radius is $radius")
-
-
-        val directedHeight = axisN * height
-
-        val d = -1 * ((bb.min.x() * axisN.x()) + (bb.min.y() * axisN.y()) + (bb.min.z() * axisN.z()))
-
+        val d = -1 * ((bb!!.min.x() * axisN.x()) + (bb.min.y() * axisN.y()) + (bb.min.z() * axisN.z()))
         val expHeight = ((bb.max.x() * axisN.x()) + (bb.max.y() * axisN.y()) + (bb.max.z() * axisN.z())) + d
-
-        logger.info("Expected height is: $expHeight")
+        //TODO: Add test for radius
 
         assertNotNull(bb)
-//        assertTrue(bb.min.x() >= -size.x() / 2.0f - epsilon)
-//        assertTrue(bb.min.y() >= -size.y() / 2.0f - epsilon)
-//        assertTrue(bb.min.z() >= -size.z() / 2.0f - epsilon)
-//
-//        assertTrue(bb.max.x() <= size.x() / 2.0f + epsilon)
-//        assertTrue(bb.max.y() <= size.y() / 2.0f + epsilon)
-//        assertTrue(bb.max.z() <= size.z() / 2.0f + epsilon)
+        assertTrue { expHeight <= height + epsilon }
     }
 }
