e77f0d0786eb41a2f9d3b286d77ea388b0e1576a
Ulrik Günther <hello@ulrik.is>
2019-06-15 18:11:00 +0200

    Add tests for SDFFontAtlas

---
+++ src/test/tests/graphics/scenery/tests/unit/fonts/SDFFontAtlasTests.kt
@@ -0,0 +1,53 @@
+package graphics.scenery.tests.unit.fonts
+
+import graphics.scenery.Hub
+import graphics.scenery.compute.OpenCLContext
+import graphics.scenery.fonts.SDFFontAtlas
+import graphics.scenery.utils.LazyLogger
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+/**
+ * Tests for [SDFFontAtlas]
+ */
+class SDFFontAtlasTests {
+    val logger by LazyLogger()
+
+    @Before
+    fun checkOpenCLAvailability() {
+        val hasOpenCL: Boolean
+        hasOpenCL = try {
+            val hub = Hub()
+            val ctx = OpenCLContext(hub)
+            true
+        } catch (e: Exception) {
+            false
+        }
+
+        org.junit.Assume.assumeTrue(hasOpenCL)
+    }
+
+    @Test
+    fun testAtlasAndMeshCreation() {
+        logger.info("Testing SDF atlas and mesh creation ...")
+        val hub = Hub()
+        val sdf = SDFFontAtlas(hub, "Helvetica", 512, 10, cache = false)
+        val atlas = sdf.getAtlas()
+
+        assertTrue(atlas.capacity() > 0 && atlas.remaining() > 0, "SDF atlas should have remaining data")
+
+        for (char in sdf.charset) {
+            if(char == 32) continue
+            val glyph = sdf.getTexcoordsForGlyph(char.toChar())
+            assertFalse(glyph.x() == 0.0f && glyph.y() == 0.0f && glyph.z() == 1.0f && glyph.w() == 1.0f,
+                "Glyph should not return default texture coordinates, but returned $glyph.")
+        }
+
+        val mesh = sdf.createMeshForString("hello world")
+        assertTrue(mesh.vertices.remaining() > 0)
+        assertTrue(mesh.normals.remaining() > 0)
+        assertTrue(mesh.texcoords.remaining() > 0)
+    }
+}
