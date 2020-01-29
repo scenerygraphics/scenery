6881d78ffeb5b80adb65d62377ee8c161905a4c2
Ulrik Guenther <hello@ulrik.is>
2020-01-29 14:49:09 +0100

    Add RAIIExample

---
+++ src/test/tests/graphics/scenery/tests/examples/bdv/RAIIExample.kt
@@ -0,0 +1,61 @@
+package graphics.scenery.tests.examples.bdv
+
+import bdv.util.AxisOrder
+import cleargl.GLVector
+import graphics.scenery.Camera
+import graphics.scenery.DetachedHeadCamera
+import graphics.scenery.PointLight
+import graphics.scenery.SceneryBase
+import graphics.scenery.backends.Renderer
+import graphics.scenery.volumes.bdv.Volume
+import ij.IJ
+import ij.ImagePlus
+import net.imglib2.img.Img
+import net.imglib2.img.display.imagej.ImageJFunctions
+import net.imglib2.type.numeric.integer.UnsignedShortType
+import org.junit.Test
+import tpietzsch.example2.VolumeViewerOptions
+
+/**
+ * BDV Rendering Example loading a RAII
+ *
+ * @author Ulrik Günther <hello@ulrik.is>
+ * @author Tobias Pietzsch <tpietzsch@mpi-cbg.de>
+ */
+class RAIIExample: SceneryBase("BDV Rendering example", 1280, 720) {
+    lateinit var volume: Volume
+
+    override fun init() {
+        renderer = hub.add(Renderer.createRenderer(hub, applicationName, scene, windowWidth, windowHeight))
+
+        val cam: Camera = DetachedHeadCamera()
+        with(cam) {
+            perspectiveCamera(50.0f, 1.0f*windowWidth, 1.0f*windowHeight)
+            active = true
+
+            scene.addChild(this)
+        }
+
+        val imp: ImagePlus = IJ.openImage("https://imagej.nih.gov/ij/images/t1-head.zip")
+        val img: Img<UnsignedShortType> = ImageJFunctions.wrapShort(imp)
+
+        volume = Volume.fromRAII(img, UnsignedShortType(), AxisOrder.DEFAULT, "T1 head", hub, VolumeViewerOptions())
+        volume.updateWorld(recursive = true, force = true)
+        scene.addChild(volume)
+
+        val lights = (0 until 3).map {
+            PointLight(radius = 15.0f)
+        }
+
+        lights.mapIndexed { i, light ->
+            light.position = GLVector(2.0f * i - 4.0f,  i - 1.0f, 0.0f)
+            light.emissionColor = GLVector(1.0f, 1.0f, 1.0f)
+            light.intensity = 50.0f
+            scene.addChild(light)
+        }
+    }
+
+    @Test override fun main() {
+        super.main()
+    }
+}
