10b8c3cf6c02526794ec0976329b2b086c11a4d4
Ulrik Guenther <hello@ulrik.is>
2020-02-14 14:57:54 +0100

    Colormap: Support loading from ImageJ LUTs

--- src/main/kotlin/graphics/scenery/volumes/Colormap.kt
+++ src/main/kotlin/graphics/scenery/volumes/Colormap.kt
@@ -2,7 +2,10 @@ package graphics.scenery.volumes
 
 import graphics.scenery.utils.Image
 import graphics.scenery.utils.LazyLogger
+import net.imagej.lut.LUTService
+import org.scijava.plugin.Parameter
 import java.io.FileNotFoundException
+import java.io.IOException
 import java.io.InputStream
 import java.nio.ByteBuffer
 
@@ -13,6 +16,9 @@ class Colormap(val buffer: ByteBuffer, val width: Int, val height: Int) {
 
     companion object {
         val logger by LazyLogger()
+
+        @Parameter
+        protected var lutService: LUTService? = null
 
         /**
          * Creates a new color map from a [ByteBuffer], with dimensions given as [width] and [height].
@@ -33,6 +39,7 @@ class Colormap(val buffer: ByteBuffer, val width: Int, val height: Int) {
          */
         fun fromStream(stream: InputStream, extension: String): Colormap {
             val image = Image.readFromStream(stream, extension)
+            logger.info("Read image from $stream with ${image.contents.remaining()} bytes, size=${image.width}x${image.height}")
             return Colormap(image.contents, image.width, image.height)
         }
 
@@ -40,10 +47,40 @@ class Colormap(val buffer: ByteBuffer, val width: Int, val height: Int) {
          * Tries to load a colormap from a file. Available colormaps can be queried with [list].
          */
         fun get(name: String): Colormap {
-            val resource = this::class.java.getResourceAsStream("colormap-$name.png")
+            try {
+                val luts = lutService?.findLUTs()
+                val colorTable = luts?.let {
+                    val url = it[name]
+                    lutService?.loadLUT(url)
+                } ?: throw IOException("Color map $name not found in ImageJ colormaps")
+
+                val copies = 16
+                val byteBuffer = ByteBuffer.allocateDirect(
+                    4 * colorTable.length * copies) // Num bytes * num components * color map length * height of color map texture
+                val tmp = ByteArray(4 * colorTable.length)
+                for (k in 0 until colorTable.length) {
+                    for (c in 0 until colorTable.componentCount) { // TODO this assumes numBits is 8, could be 16
+                        tmp[4 * k + c] = colorTable[c, k].toByte()
+                    }
+                    if (colorTable.componentCount == 3) {
+                        tmp[4 * k + 3] = 255.toByte()
+                    }
+                }
+                for (i in 0 until copies) {
+                    byteBuffer.put(tmp)
+                }
+                byteBuffer.flip()
+
+                logger.info("Using ImageJ colormap $name with size ${colorTable.length}x$copies")
+                return fromBuffer(byteBuffer, colorTable.length, copies)
+            } catch(e: IOException) {
+                logger.debug("LUT $name not found as ImageJ colormap, trying stream")
+                logger.info("Using colormap $name from stream")
+                val resource = this::class.java.getResourceAsStream("colormap-$name.png")
                     ?: throw FileNotFoundException("Could not find color map for name $name (colormap-$name.png)")
 
-            return fromStream(resource, "png")
+                return fromStream(resource, "png")
+            }
         }
 
         /**
