53dfa4d35f67362286641ea810b4945197a0a9d5
Ulrik Guenther <hello@ulrik.is>
2020-07-02 21:36:37 +0200

    ComputeShaderExample: Extend with texture saving

--- src/test/tests/graphics/scenery/tests/examples/compute/ComputeShaderExample.kt
+++ src/test/tests/graphics/scenery/tests/examples/compute/ComputeShaderExample.kt
@@ -8,8 +8,15 @@ import graphics.scenery.tests.examples.basic.TexturedCubeExample
 import graphics.scenery.tests.examples.basic.TexturedCubeJavaExample
 import graphics.scenery.textures.Texture
 import graphics.scenery.utils.Image
+import graphics.scenery.utils.SystemHelpers
+import kotlinx.coroutines.*
+import kotlinx.coroutines.selects.select
 import org.junit.Test
+import org.lwjgl.system.MemoryUtil
+import org.scijava.ui.behaviour.ClickBehaviour
+import java.util.concurrent.Executors
 import kotlin.concurrent.thread
+import kotlin.coroutines.CoroutineContext
 
 /**
  * Example showing simple image processing with compute shaders.
@@ -22,12 +29,14 @@ class ComputeShaderExample : SceneryBase("ComputeShaderExample") {
         renderer = hub.add(SceneryElement.Renderer,
             Renderer.createRenderer(hub, applicationName, scene, 512, 512))
 
+        val helix = Texture.fromImage(Image.fromResource("textures/helix.png", TexturedCubeExample::class.java), usage = hashSetOf(Texture.UsageType.LoadStoreImage, Texture.UsageType.Texture))
+        val buffer = MemoryUtil.memCalloc(helix.dimensions.x * helix.dimensions.y * helix.dimensions.z * 4)
+
         val compute = Box()
         compute.name = "compute node"
         compute.material = ShaderMaterial(Shaders.ShadersFromFiles(arrayOf("BGRAMosaic.comp"), this::class.java))
-        compute.material.textures["OutputViewport"] = Texture.fromImage(Image.fromResource("textures/helix.png", TexturedCubeExample::class.java), usage = hashSetOf(Texture.UsageType.LoadStoreImage, Texture.UsageType.Texture))
-        compute.material.textures["InputColor"] = Texture.fromImage(Image.fromResource("textures/helix.png", TexturedCubeExample::class.java), usage = hashSetOf(Texture.UsageType.LoadStoreImage, Texture.UsageType.Texture))
-
+        compute.material.textures["OutputViewport"] = Texture.fromImage(Image(buffer, helix.dimensions.x, helix.dimensions.y, helix.dimensions.z), usage = hashSetOf(Texture.UsageType.LoadStoreImage, Texture.UsageType.Texture))
+        compute.material.textures["InputColor"] = helix
         scene.addChild(compute)
 
         val box = Box(Vector3f(1.0f, 1.0f, 1.0f))
@@ -56,11 +65,36 @@ class ComputeShaderExample : SceneryBase("ComputeShaderExample") {
             while (running) {
                 box.rotation.rotateY(0.01f)
                 box.needsUpdate = true
-                box.material.textures["diffuse"] = compute.material.textures["OutputViewport"]!!
+//                box.material.textures["diffuse"] = compute.material.textures["OutputViewport"]!!
 
                 Thread.sleep(20)
             }
         }
+    }
+
+    override fun inputSetup() {
+        super.inputSetup()
+
+        inputHandler?.addBehaviour("save_texture", ClickBehaviour { _, _ ->
+            logger.info("Finding node")
+            val node = scene.find("compute node") ?: return@ClickBehaviour
+            val texture = node.material.textures["OutputViewport"]!!
+            val r = renderer ?: return@ClickBehaviour
+            logger.info("Node found, saving texture")
+
+            val result = r.requestTexture(texture) { tex ->
+                logger.info("Received texture")
+
+                tex.contents?.let { buffer ->
+                    logger.info("Dumping to file")
+                    SystemHelpers.dumpToFile(buffer, "texture-${SystemHelpers.formatDateTime(delimiter = "_")}.raw")
+                    logger.info("File dumped")
+                }
+
+            }
+
+        })
+        inputHandler?.addKeyBinding("save_texture", "E")
     }
 
     @Test override fun main() {
