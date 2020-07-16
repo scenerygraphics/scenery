36944126c24e8bda77c42f3760663d32c75ab390
Ulrik Guenther <hello@ulrik.is>
2020-07-16 17:18:13 +0200

    EdgeBundlerExample: Refactor

--- src/test/tests/graphics/scenery/tests/examples/basic/EdgeBundlerExample.kt
+++ src/test/tests/graphics/scenery/tests/examples/basic/EdgeBundlerExample.kt
@@ -4,8 +4,12 @@ import org.joml.Vector3f
 import graphics.scenery.*
 import graphics.scenery.backends.Renderer
 import graphics.scenery.compute.EdgeBundler
+import graphics.scenery.numerics.Random
 import org.junit.Test
 import kotlin.concurrent.thread
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
 
 /**
  * Simple example to demonstrate Edge Bundling
@@ -14,7 +18,7 @@ import kotlin.concurrent.thread
 class EdgeBundlerExample : SceneryBase("EdgeBundlerExample") {
 
     override fun init() {
-        val eb = EdgeBundler(Line.createLines(800, 50, 10.0f, 0.2f), hub) // From Line set
+        val eb = EdgeBundler(Line.createLines(800, 50, 10.0f, 0.25f), hub) // From Line set
 
         // Do the actual work. Optionally, you can change parameters before calling this function
         eb.calculate()
@@ -23,11 +27,15 @@ class EdgeBundlerExample : SceneryBase("EdgeBundlerExample") {
         initScene()
 
         // Look how many clusters were created, use this information for colormap
-        val colorMap = getRandomColors(eb.numberOfClusters)
-        renderLinePairs(eb.getLinePairs(), eb.getClusterOfTracks(), colorMap)
+        val colorMap = Array(eb.numberOfClusters) { Random.random3DVectorFromRange(0.2f, 0.8f) }
+
+        eb.getLinePairsAndClusters().forEach { (track, clusterId) ->
+            track.material.ambient = colorMap[clusterId]
+            track.material.diffuse = colorMap[clusterId]
+            track.material.specular = colorMap[clusterId]
+            scene.addChild(track)
+        }
     }
-
-
 
     /**
      * Prepare the 3D environment
@@ -46,9 +54,8 @@ class EdgeBundlerExample : SceneryBase("EdgeBundlerExample") {
         }
 
         val cam: Camera = DetachedHeadCamera()
-        cam.position = Vector3f(0.0f, 0.0f, 15.0f)
+        cam.position = Vector3f(0.0f, -2.0f, 15.0f)
         cam.perspectiveCamera(50.0f, windowWidth, windowHeight)
-        cam.target = Vector3f(0.0f, 0.0f, 0.0f)
         scene.addChild(cam)
 
         // Light show
@@ -57,9 +64,9 @@ class EdgeBundlerExample : SceneryBase("EdgeBundlerExample") {
                 val t = runtime/100.0f
                 lights.forEachIndexed { i, pointLight ->
                     pointLight.position = Vector3f(
-                        33.0f*Math.sin(2.0f*i*Math.PI/3.0f+t*Math.PI/50.0f).toFloat(),
+                        33.0f* sin(2.0f*i*PI/3.0f+t*PI/50.0f).toFloat(),
                         0.0f,
-                        -33.0f*Math.cos(2.0f*i*Math.PI/3.0f+t*Math.PI/50.0f).toFloat())
+                        -33.0f* cos(2.0f*i*PI/3.0f+t*PI/50.0f).toFloat())
                 }
                 Thread.sleep(20)
             }
@@ -68,48 +75,11 @@ class EdgeBundlerExample : SceneryBase("EdgeBundlerExample") {
         // Bundling ... de-bundling ... bundling ... de-bundling...
         thread {
             while (true) {
-                val t = Math.sin(runtime * Math.PI/2000.0f) * 0.5f + 0.5f
+                val t = sin(runtime * PI/2000.0f) * 0.5f + 0.5f
                 scene.children.forEach() { n-> if(n is LinePair) {n.interpolationState = t.toFloat()} }
                 Thread.sleep(20)
             }
         }
-    }
-
-    /**
-     * Creates a random color for each cluster
-     * @return An array of RGB colors
-     */
-    private fun getRandomColors(numClusters: Int): Array<Vector3f> {
-        val result: Array<Vector3f> = Array(numClusters) { Vector3f(0.0f, 0.0f, 0.0f) }
-        val values = FloatArray(1000) {i -> i.toFloat() / 1000.0f}
-        for(i in 0 until numClusters)
-        {
-            result[i] = Vector3f(values.random(), values.random(), values.random())
-        }
-        return result
-    }
-
-    /**
-     * Renders the lines and gives them colors based on their cluster
-     * @param lines The array of lines
-     * @param cluster The mapping between lineId and clusterId
-     * @param colorMap Color map with a color for each cluster
-     */
-    private fun renderLinePairs(lines: Array<LinePair>, cluster: Array<Int>, colorMap: Array<Vector3f>) {
-        for(t in lines.indices) {
-            val track = lines[t]
-            val clusterId = cluster[t]
-
-            track.material.ambient = colorMap[clusterId]
-            track.material.diffuse = colorMap[clusterId]
-            track.material.specular = colorMap[clusterId]
-            scene.addChild(track)
-        }
-        scene.dirty = true
-    }
-
-    override fun inputSetup() {
-        setupCameraModeSwitching(keybinding = "C")
     }
 
     @Test override fun main() {
