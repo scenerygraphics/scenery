fabdd2df13dae69a1cde855d07bd8227733b0cb9
Ulrik Guenther <hello@ulrik.is>
2020-07-30 18:11:53 +0200

    Add DisableFrustumCulling interface and use for Volume and TextBoard

---
+++ src/main/kotlin/graphics/scenery/DisableFrustumCulling.kt
@@ -0,0 +1,9 @@
+package graphics.scenery
+
+/**
+ * Marker interface to disable frustum culling for a certain node type
+ *
+ * @author Ulrik Guenther <hello@ulrik.is>
+ */
+interface DisableFrustumCulling {
+}
