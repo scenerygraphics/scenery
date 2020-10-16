fc38d75e767475f8d1c8f477b9b6d4c6c439d6bb
Domino2357 <JustinBuerger@googlemail.com>
2020-10-16 20:20:40 +0200

    Added and customized new unit tests

---
+++ src/test/tests/graphics/scenery/tests/unit/ProteinTests.kt
@@ -0,0 +1,27 @@
+package graphics.scenery.tests.unit
+
+import graphics.scenery.*
+import graphics.scenery.utils.LazyLogger
+import org.junit.Test
+import kotlin.test.assertFails
+
+/**
+ * Unit test for the Protein class
+ *
+ * @author  Justin Buerger <burger@mpi-cbg.de>
+ */
+class ProteinTests {
+    private val logger by LazyLogger()
+
+    @Test
+    fun testInvalidPath() {
+        logger.info("Tests that an invalid path gets caught")
+        assertFails { Protein.fromFile("LetsGetSchwifty") }
+    }
+
+    @Test
+    fun testInvalidID() {
+        logger.info("Test if an invalid pdb entry is caught")
+        assertFails { Protein.fromID("3mbn") }
+    }
+}
