0a209c4a66affb2ce583d50ccbe13b88b2ed5e40
Ulrik Günther <hello@ulrik.is>
2020-11-04 17:40:01 +0100

    VulkanTexture: Use host-cached memory for improved performance when reading back from GPU

--- src/main/kotlin/graphics/scenery/backends/vulkan/VulkanTexture.kt
+++ src/main/kotlin/graphics/scenery/backends/vulkan/VulkanTexture.kt
@@ -577,7 +577,7 @@ open class VulkanTexture(val device: VulkanDevice,
                 tmpBuffer = VulkanBuffer(this@VulkanTexture.device,
                     image.maxSize,
                     VK_BUFFER_USAGE_TRANSFER_SRC_BIT or VK_BUFFER_USAGE_TRANSFER_DST_BIT,
-                    VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT or VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+                    VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT or VK_MEMORY_PROPERTY_HOST_COHERENT_BIT or VK_MEMORY_PROPERTY_HOST_CACHED_BIT,
                     wantAligned = true)
             }
 
