14ac6b18880b426c7423e17ec32aaec1b79891ef
Giuseppe Barbieri <elect86@gmail.com>
2020-11-12 06:59:00 +0100

    - enriched .gitignore comments - moved println debugs beyonds variable

--- buildSrc/src/main/kotlin/util.kt
+++ buildSrc/src/main/kotlin/util.kt
@@ -8,8 +8,8 @@ import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
 import java.net.URL
 import java.util.regex.Pattern
 
-// last commit of pom-scijava-29.2.1
-val pomSci = URL("https://raw.githubusercontent.com/scijava/pom-scijava/10b42e8e/pom.xml").readText()
+val pomSciVersion = "29.2.1"
+val pomSci = URL("https://maven.scijava.org/content/groups/public/org/scijava/pom-scijava/$pomSciVersion/pom-scijava-$pomSciVersion.pom").readText()
 
 val is64: Boolean = run {
     val arch = System.getenv("PROCESSOR_ARCHITECTURE")
@@ -47,29 +47,29 @@ val ffmpegNatives = when {
  */
 fun DependencyHandlerScope.sci(dep_: String, native: String? = null, test: Boolean = false) {
     var dep = dep_
-//    println("sci($dep)")
+    val println = false
+    //    println("sci($dep)")
     val deps = dep.split(':')
     var vers = ""
     if (deps.size == 3) {
         vers = deps[2]
-//        println("deps.size=3, vers=$vers")
-        if(vers.startsWith('$')) { // we need to prepare for later extraction
+        //        println("deps.size=3, vers=$vers")
+        if (vers.startsWith('$')) { // we need to prepare for later extraction
             vers = vers.substring(1)
             dep = dep.substringBeforeLast(':') // remove tail
-        }
-        else { // complete, eg: com.github.scenerygraphics:vector:958f2e6
+        } else { // complete, eg: com.github.scenerygraphics:vector:958f2e6
             if (test) {
-                println("testImplementation(\"$dep\")")
+                if (println) println("testImplementation(\"$dep\")")
                 testImplementation(dep)
             } else {
-                println("implementation(\"$dep\")")
+                if (println) println("implementation(\"$dep\")")
                 implementation(dep)
             }
             if (native != null) if (test) {
-                println("testRuntimeOnly(${deps[0]}, ${deps[1]}, ${deps[2]}, classifier = $native)")
+                if (println) println("testRuntimeOnly(${deps[0]}, ${deps[1]}, ${deps[2]}, classifier = $native)")
                 testRuntimeOnly(deps[0], deps[1], deps[2], classifier = native)
             } else {
-                println("runtimeOnly(${deps[0]}, ${deps[1]}, ${deps[2]}, classifier = $native)")
+                if (println) println("runtimeOnly(${deps[0]}, ${deps[1]}, ${deps[2]}, classifier = $native)")
                 runtimeOnly(deps[0], deps[1], deps[2], classifier = native)
             }
             return
@@ -81,19 +81,19 @@ fun DependencyHandlerScope.sci(dep_: String, native: String? = null, test: Boole
 
     // we need to extract the version
     val version = vers.version
-//    println("vers=$vers, version=$version")
+    //    println("vers=$vers, version=$version")
     if (test) {
-        println("testImplementation(\"$dep:$version\")")
+        if (println) println("testImplementation(\"$dep:$version\")")
         testImplementation("$dep:$version")
     } else {
-        println("implementation(\"$dep:$version\")")
+        if (println) println("implementation(\"$dep:$version\")")
         implementation("$dep:$version")
     }
     if (native != null) if (test) {
-        println("testRuntimeOnly(${dep.substringBefore(':')}, ${dep.substringAfter(':')}, classifier = $native)")
+        if (println) println("testRuntimeOnly(${dep.substringBefore(':')}, ${dep.substringAfter(':')}, classifier = $native)")
         testRuntimeOnly(dep.substringBefore(':'), dep.substringAfter(':'), classifier = native)
     } else {
-        println("runtimeOnly(${dep.substringBefore(':')}, ${dep.substringAfter(':')}, classifier = $native)")
+        if (println) println("runtimeOnly(${dep.substringBefore(':')}, ${dep.substringAfter(':')}, classifier = $native)")
         runtimeOnly(dep.substringBefore(':'), dep.substringAfter(':'), classifier = native)
     }
 }
@@ -102,17 +102,17 @@ fun DependencyHandlerScope.testSci(dep: String, native: String? = null) = sci(de
 
 val String.version: String
     get() {
-//        println("$this.version")
+        //        println("$this.version")
         var matcher = Pattern.compile("<$this\\.version>(.*)</$this\\.version>").matcher(pomSci) // exact
         if (!matcher.find()) {
-//            println("generic")
+            //            println("generic")
             matcher = Pattern.compile("<$this.*\\.version>(.*)</$this.*\\.version>").matcher(pomSci) // generic
         }
-//        println(matcher)
+        //        println(matcher)
         matcher.reset().find()
         val vers = matcher.group(1)
-//        println(vers)
-//        println(matcher)
+        //        println(vers)
+        //        println(matcher)
         return when { // recursive, ie: ${jogl.version}
             vers.startsWith("\${") && vers.endsWith(".version}") -> vers.drop(2).dropLast(9).version
             else -> vers
@@ -124,30 +124,30 @@ fun DependencyHandler.implementation(dependencyNotation: Any): Dependency? = add
 fun DependencyHandler.runtimeOnly(dependencyNotation: Any): Dependency? = add("runtimeOnly", dependencyNotation)
 
 fun DependencyHandler.runtimeOnly(
-    group: String,
-    name: String,
-    version: String? = null,
-    configuration: String? = null,
-    classifier: String? = null,
-    ext: String? = null,
-    dependencyConfiguration: Action<ExternalModuleDependency>? = null
+        group: String,
+        name: String,
+        version: String? = null,
+        configuration: String? = null,
+        classifier: String? = null,
+        ext: String? = null,
+        dependencyConfiguration: Action<ExternalModuleDependency>? = null
 ): ExternalModuleDependency = addExternalModuleDependencyTo(
-    this, "runtimeOnly", group, name, version, configuration, classifier, ext, dependencyConfiguration
+        this, "runtimeOnly", group, name, version, configuration, classifier, ext, dependencyConfiguration
 )
 
 fun DependencyHandler.testImplementation(dependencyNotation: Any): Dependency? =
-    add("testImplementation", dependencyNotation)
+        add("testImplementation", dependencyNotation)
 
 fun DependencyHandler.testRuntimeOnly(dependencyNotation: Any): Dependency? = add("testRuntimeOnly", dependencyNotation)
 
 fun DependencyHandler.testRuntimeOnly(
-    group: String,
-    name: String,
-    version: String? = null,
-    configuration: String? = null,
-    classifier: String? = null,
-    ext: String? = null,
-    dependencyConfiguration: Action<ExternalModuleDependency>? = null
+        group: String,
+        name: String,
+        version: String? = null,
+        configuration: String? = null,
+        classifier: String? = null,
+        ext: String? = null,
+        dependencyConfiguration: Action<ExternalModuleDependency>? = null
 ): ExternalModuleDependency = addExternalModuleDependencyTo(
-    this, "testRuntimeOnly", group, name, version, configuration, classifier, ext, dependencyConfiguration
+        this, "testRuntimeOnly", group, name, version, configuration, classifier, ext, dependencyConfiguration
 )
