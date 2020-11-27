7e6a2d75c5b023e568513cd72901f221618a1ec0
Domino2357 <JustinBuerger@googlemail.com>
2020-11-27 16:23:01 +0100

    Added CreationExample, RulerExample and DistanceMeasurement

---
+++ src/test/kotlin/graphics/scenery/tests/examples/basic/RulerExample.kt
@@ -0,0 +1,51 @@
+package graphics.scenery.tests.examples.basic
+
+import graphics.scenery.*
+import graphics.scenery.backends.Renderer
+import graphics.scenery.Ruler
+import org.joml.Vector3f
+import org.junit.Test
+
+/**
+ * Sketch that shows how the ruler works. Run, press R (and hold it) then you can create a line on a Mouse-click, and
+ * drag it on your screen.
+ *
+ * @author Justin Buerger <burger@mpi-cbg.de>
+ */
+class RulerExample: SceneryBase("RulerSketch", wantREPL = true) {
+    override fun init() {
+        renderer = hub.add(Renderer.createRenderer(hub, applicationName, scene, 512, 512))
+
+        val box = Box(Vector3f(10.0f, 10.0f, 10.0f), insideNormals = true)
+        box.material.diffuse = Vector3f(1.0f, 1.0f, 1.0f)
+        box.material.cullingMode = Material.CullingMode.Front
+        scene.addChild(box)
+
+        val light = PointLight(radius = 15.0f)
+        light.position = Vector3f(0.0f, 0.0f, 2.0f)
+        light.intensity = 1.0f
+        light.emissionColor = Vector3f(1.0f, 1.0f, 1.0f)
+        scene.addChild(light)
+
+        val cam: Camera = DetachedHeadCamera()
+        with(cam) {
+            position = Vector3f(0.0f, 0.0f, 5.0f)
+            perspectiveCamera(50.0f, 512, 512)
+
+            scene.addChild(this)
+        }
+    }
+
+    override fun inputSetup() {
+        super.inputSetup()
+        val sphere = Icosphere(0.25f, 6)
+        inputHandler?.addBehaviour("ruler", Ruler("create", { scene.findObserver() }, scene))
+        inputHandler?.addKeyBinding("ruler", "R")
+    }
+
+    @Test
+    override fun main() {
+        super.main()
+    }
+}
+
