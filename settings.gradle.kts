daa4c71f74f190181e7473e257d434863b9edd76
Giuseppe Barbieri <elect86@gmail.com>
2021-01-25 18:28:32 +0100

    Git: removed manually all files from `gradle` branch, erroneously merged into master

--- settings.gradle.kts
+++
@@ -1,7 +0,0 @@
-rootProject.name = "scenery"
-
-gradle.rootProject {
-//    group = "scenery"
-    version = "0.7.0-beta-8-SNAPSHOT"
-    description = "flexible scenegraphing and rendering for scientific visualisation"
-}
