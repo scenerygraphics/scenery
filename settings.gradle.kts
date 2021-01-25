7f8aa2a51a8d64264417a3cc52d04a3513fb94eb
Giuseppe Barbieri <elect86@gmail.com>
2021-01-25 18:57:26 +0100

    buildSrc, CI yaml and other gradle files

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "scenery"
+
+gradle.rootProject {
+    //    group = "scenery"
+    version = "0.7.0-beta-8-SNAPSHOT"
+    description = "flexible scenegraphing and rendering for scientific visualisation"
+}
