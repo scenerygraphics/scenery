a6cc19c9fde2479ca9c63a7811d3d4e8285e7387
Jan T <janhentie@web.de>
2021-03-15 17:16:46 +0100

    mouseDrag: porting NodeRotateControl and NodeTranslateControl from SciView

---
+++ src/main/kotlin/graphics/scenery/controls/behaviours/MouseDragPlane.kt
@@ -0,0 +1,140 @@
+/*-
+ * #%L
+ * Scenery-backed 3D visualization package for ImageJ.
+ * %%
+ * Copyright (C) 2016 - 2020 SciView developers.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+package graphics.scenery.controls.behaviours
+
+import graphics.scenery.BoundingGrid
+import graphics.scenery.Camera
+import graphics.scenery.Node
+import graphics.scenery.utils.LazyLogger
+import org.joml.Vector3f
+import org.scijava.ui.behaviour.DragBehaviour
+import org.scijava.ui.behaviour.ScrollBehaviour
+import kotlin.reflect.KProperty
+
+/**
+ * Drag nodes along the viewplane by mouse.
+ *
+ * @author Kyle Harrington
+ * @author Jan Tiemann
+ */
+open class MouseDragPlane(
+    protected val name: String,
+    protected val camera: () -> Camera?,
+    protected val alternativeTargetNode: (() -> Node?)? = null,
+    protected var debugRaycast: Boolean = false,
+    protected var ignoredObjects: List<Class<*>> = listOf<Class<*>>(BoundingGrid::class.java),
+    protected val mouseSpeed: () -> Float = { 0.25f },
+    protected val FPSSpeedSlow: () -> Float = { 0.05f }
+) : DragBehaviour, ScrollBehaviour {
+
+    protected val logger by LazyLogger()
+    protected val cam: Camera? by CameraDelegate()
+
+    protected var currentNode: Node? = null
+    private var lastX = 0
+    private var lastY = 0
+
+
+    /** Camera delegate class, converting lambdas to Cameras. */
+    protected inner class CameraDelegate {
+        /** Returns the [graphics.scenery.Camera] resulting from the evaluation of [camera] */
+        operator fun getValue(thisRef: Any?, property: KProperty<*>): Camera? {
+            return camera.invoke()
+        }
+
+        /** Setting the value is not supported */
+        operator fun setValue(thisRef: Any?, property: KProperty<*>, value: Camera?) {
+            throw UnsupportedOperationException()
+        }
+    }
+
+    /**
+     * This function is called upon mouse down and initializes the camera control
+     * with the current window size.
+     *
+     * x position in window
+     * y position in window
+     */
+    override fun init(x: Int, y: Int) {
+        if (alternativeTargetNode != null) {
+            currentNode = alternativeTargetNode.invoke()
+        } else {
+            cam?.let { cam ->
+                val matches = cam.getNodesForScreenSpacePosition(x, y, ignoredObjects, debugRaycast)
+                currentNode = matches.matches.firstOrNull()?.node
+            }
+        }
+
+        lastX = x
+        lastY = y
+    }
+
+    override fun drag(x: Int, y: Int) {
+        val targetedNode = currentNode
+
+        cam?.let {
+            if (targetedNode == null || !targetedNode.lock.tryLock()) return
+
+            it.right.mul((x - lastX) * FPSSpeedSlow() * mouseSpeed(), dragPosUpdater)
+            targetedNode.position.add(dragPosUpdater)
+            it.up.mul((lastY - y) * FPSSpeedSlow() * mouseSpeed(), dragPosUpdater)
+            targetedNode.position.add(dragPosUpdater)
+            targetedNode.needsUpdate = true
+
+            targetedNode.lock.unlock()
+
+            lastX = x
+            lastY = y
+        }
+    }
+
+    override fun end(x: Int, y: Int) {
+    }
+
+    override fun scroll(wheelRotation: Double, isHorizontal: Boolean, x: Int, y: Int) {
+        val targetedNode = currentNode
+
+        cam?.let {
+            if (targetedNode == null || !targetedNode.lock.tryLock()) return
+
+            it.forward.mul(
+                wheelRotation.toFloat() * FPSSpeedSlow() * mouseSpeed(),
+                scrollPosUpdater
+            )
+            targetedNode.position.add(scrollPosUpdater)
+            targetedNode.needsUpdate = true
+
+            targetedNode.lock.unlock()
+        }
+    }
+
+    //aux vars to prevent from re-creating them over and over
+    private val dragPosUpdater: Vector3f = Vector3f()
+    private val scrollPosUpdater: Vector3f = Vector3f()
+}
