634ec18928bcb375e28fdc6e49ed34dedbece912
Jan T <janhentie@web.de>
2021-03-29 11:25:12 +0200

    OrthoViewExample.kt: Full create Ortho View Method

--- src/test/kotlin/graphics/scenery/tests/examples/volumes/OrthoViewExample.kt
+++ src/test/kotlin/graphics/scenery/tests/examples/volumes/OrthoViewExample.kt
@@ -6,7 +6,6 @@ import graphics.scenery.backends.Renderer
 import graphics.scenery.controls.behaviours.MouseDragSphere
 import graphics.scenery.effectors.LineRestrictionEffector
 import graphics.scenery.utils.extensions.minus
-import graphics.scenery.utils.extensions.plus
 import graphics.scenery.utils.extensions.times
 import ij.IJ
 import ij.ImagePlus
@@ -24,7 +23,6 @@ import graphics.scenery.volumes.*
  */
 class OrthoViewExample : SceneryBase("Ortho View example", 1280, 720) {
     lateinit var volume: Volume
-    lateinit var planeXZ: Box
 
     override fun init() {
         renderer = hub.add(Renderer.createRenderer(hub, applicationName, scene, windowWidth, windowHeight))
@@ -58,61 +56,86 @@ class OrthoViewExample : SceneryBase("Ortho View example", 1280, 720) {
         volume = Volume.fromRAI(img, UnsignedShortType(), AxisOrder.DEFAULT, "T1 head", hub, VolumeViewerOptions())
         volume.transferFunction = TransferFunction.ramp(0.001f, 0.5f, 0.3f)
         scene.addChild(volume)
-        volume.cropInsteadOfSlice = false
 
         val bGrid = BoundingGrid()
         bGrid.node = volume
         volume.addChild(bGrid)
 
+        createOrthoView(volume)
+    }
+
+    fun createOrthoView(volume: Volume) {
+        this.volume.cropInsteadOfSlice = false
         val sliceXZ = SlicingPlane()
         val sliceXY = SlicingPlane()
         val sliceYZ = SlicingPlane()
 
-        sliceXY.rotation = sliceXY.rotation.rotateX((Math.PI/2).toFloat())
-        sliceYZ.rotation = sliceYZ.rotation.rotateZ((Math.PI/2).toFloat())
+        sliceXY.rotation = sliceXY.rotation.rotateX((Math.PI / 2).toFloat())
+        sliceYZ.rotation = sliceYZ.rotation.rotateZ((Math.PI / 2).toFloat())
 
-        sliceXZ.addTargetVolume(volume)
-        sliceXY.addTargetVolume(volume)
-        sliceYZ.addTargetVolume(volume)
+        sliceXZ.addTargetVolume(this.volume)
+        sliceXY.addTargetVolume(this.volume)
+        sliceYZ.addTargetVolume(this.volume)
 
-        volume.boundingBox?.let { boundingBox ->
+        this.volume.boundingBox?.let { boundingBox ->
 
-            val center = (boundingBox.max - boundingBox.min)*0.5f
-            planeXZ = Box(Vector3f(boundingBox.max.x,1f,boundingBox.max.z ))
+            val center = (boundingBox.max - boundingBox.min) * 0.5f
+
+            val planeXZ = Box(Vector3f(boundingBox.max.x, 1f, boundingBox.max.z))
+            val planeXY = Box(Vector3f(boundingBox.max.x, boundingBox.max.y, 1f))
+            val planeYZ = Box(Vector3f(1f, boundingBox.max.y, boundingBox.max.z))
+
             planeXZ.position = center
+            planeXY.position = center
+            planeYZ.position = center
+
             // make transparent
             planeXZ.material.blending.setOverlayBlending()
             planeXZ.material.blending.transparent = true
             planeXZ.material.blending.opacity = 0f
             //planeXZ.material.wireframe = true
+            planeXY.material = planeXZ.material
+            planeYZ.material = planeXZ.material
 
             planeXZ.addChild(sliceXZ)
-            volume.addChild(planeXZ)
+            planeXY.addChild(sliceXY)
+            planeYZ.addChild(sliceYZ)
+
+            this.volume.addChild(planeXZ)
+            this.volume.addChild(planeXY)
+            this.volume.addChild(planeYZ)
 
             val yTop = Node()
             yTop.position = Vector3f(center.x, boundingBox.max.y, center.z)
-            volume.addChild(yTop)
+            this.volume.addChild(yTop)
 
             val yBottom = Node()
             yBottom.position = Vector3f(center.x, boundingBox.min.y, center.z)
-            volume.addChild(yBottom)
+            this.volume.addChild(yBottom)
 
-            LineRestrictionEffector(planeXZ,{yTop.position},{yBottom.position})
+            LineRestrictionEffector(planeXZ, { yTop.position }, { yBottom.position })
 
+            val zTop = Node()
+            zTop.position = Vector3f(center.x, center.y, boundingBox.max.z)
+            this.volume.addChild(/*z*/zTop)
 
+            val zBottom = Node()
+            zBottom.position = Vector3f(center.x, center.y, boundingBox.min.z)
+            this.volume.addChild(zBottom)
+
+            LineRestrictionEffector(planeXY, { zTop.position }, { zBottom.position })
+
+            val xTop = Node()
+            xTop.position = Vector3f(boundingBox.max.x, center.y, center.z)
+            this.volume.addChild(xTop)
+
+            val xBottom = Node()
+            xBottom.position = Vector3f(boundingBox.min.x, center.y, center.z)
+            this.volume.addChild(xBottom)
+
+            LineRestrictionEffector(planeYZ, { xTop.position }, { xBottom.position })
 
         }
-
-
-
-
-
-        /*
-        scene.addChild(sliceXZ)
-        scene.addChild(sliceXY)
-        scene.addChild(sliceYZ)
-        */
-
     }
 
     override fun inputSetup() {
