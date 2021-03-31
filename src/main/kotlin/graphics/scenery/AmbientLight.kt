70cea2ee377836f5bac20b9995fff50374130fbb
Ulrik Guenther <hello@ulrik.is>
2021-03-31 17:14:16 +0200

    Add AmbientLight class and Light type, enable in shader

---
+++ src/main/kotlin/graphics/scenery/AmbientLight.kt
@@ -0,0 +1,76 @@
+package graphics.scenery
+
+import org.joml.Vector3f
+
+/**
+ * Point light class.
+ *
+ * Point lights have an extent given as [lightRadius].
+ * They also have an optional [Box] to accompany them for easier visualisation.
+ *
+ * @author Ulrik Günther <hello@ulrik.is>
+ * @constructor Creates a PointLight with default settings, e.g. white emission color.
+ */
+class AmbientLight(intensity: Float = 1.0f, emissionColor: Vector3f = Vector3f(1.0f)) : Light("AmbientLight") {
+    /** The intensity of the point light. Bound to [0.0, 1.0] if using non-HDR rendering. */
+    @ShaderProperty
+    override var intensity: Float = intensity
+
+    /** The emission color of the point light. Setting it will also affect the accompanying Box' color. */
+    @ShaderProperty
+    override var emissionColor: Vector3f = emissionColor
+
+    /** Type of the light, can be DirectionalLight or PointLight */
+    @ShaderProperty
+    override val lightType: LightType = LightType.AmbientLight
+
+    @ShaderProperty private var lightRadius: Float = 1.0f
+
+    @Suppress("unused") // will be serialised into ShaderProperty buffer
+    @ShaderProperty val worldPosition
+        get() = Vector3f(0.0f)
+
+    @Suppress("unused") // will be serialised into ShaderProperty buffer
+    @ShaderProperty var debugMode = 0
+
+    init {
+        // fake geometry
+        this.vertices = BufferUtils.allocateFloatAndPut(
+            floatArrayOf(
+                -1.0f, -1.0f, 0.0f,
+                1.0f, -1.0f, 0.0f,
+                1.0f, 1.0f, 0.0f,
+                -1.0f, 1.0f, 0.0f))
+
+        this.normals = BufferUtils.allocateFloatAndPut(
+            floatArrayOf(
+                1.0f, 0.0f, 0.0f,
+                0.0f, 1.0f, 0.0f,
+                0.0f, 0.0f, 1.0f,
+                0.0f, 0.0f, 1.0f))
+
+        this.texcoords = BufferUtils.allocateFloatAndPut(
+            floatArrayOf(
+                0.0f, 0.0f,
+                1.0f, 0.0f,
+                1.0f, 1.0f,
+                0.0f, 1.0f))
+
+        this.indices = BufferUtils.allocateIntAndPut(
+            intArrayOf(0, 1, 2, 0, 2, 3))
+
+        this.geometryType = GeometryType.TRIANGLES
+        this.vertexSize = 3
+        this.texcoordSize = 2
+
+        material.blending.transparent = true
+        material.blending.colorBlending = Blending.BlendOp.add
+        material.blending.sourceColorBlendFactor = Blending.BlendFactor.One
+        material.blending.destinationColorBlendFactor = Blending.BlendFactor.One
+        material.blending.sourceAlphaBlendFactor = Blending.BlendFactor.One
+        material.blending.destinationAlphaBlendFactor = Blending.BlendFactor.One
+        material.blending.alphaBlending = Blending.BlendOp.add
+        material.cullingMode = Material.CullingMode.Front
+        material.depthTest = Material.DepthTest.Greater
+    }
+}
