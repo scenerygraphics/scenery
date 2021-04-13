2a381949b751b4143e309dd2e17bd1e4ae196e4c
Ulrik Guenther <hello@ulrik.is>
2021-04-13 20:55:06 +0200

    Node: Bring back delegates for handling changes, and set modifiedAt time accordingly

--- src/main/kotlin/graphics/scenery/Node.kt
+++ src/main/kotlin/graphics/scenery/Node.kt
@@ -129,29 +129,13 @@ open class Node(open var name: String = "Node") : Renderable, Serializable, Real
     override var mvp: Matrix4f by Delegates.observable(Matrix4f().identity()) { property, old, new -> propertyChanged(property, old, new) }
 
     /** World position of the Node. Setting will trigger [world] update. */
-//    @delegate:Transient
-    override var position: Vector3f = Vector3f(0.0f) //by Delegates.observable(Vector3f(0.0f, 0.0f, 0.0f)) { property, old, new -> propertyChanged(property, old, new) }
-        set(value) {
-            field = value
-            needsUpdate = true
-            needsUpdateWorld = true
-        }
+    override var position by Delegates.observable(Vector3f(0.0f, 0.0f, 0.0f)) { property, old, new -> propertyChanged(property, old, new) }
 
     /** x/y/z scale of the Node. Setting will trigger [world] update. */
-    override var scale: Vector3f = Vector3f(1.0f)//by Delegates.observable(Vector3f(1.0f, 1.0f, 1.0f)) { property, old, new -> propertyChanged(property, old, new) }
-        set(value) {
-            field = value
-            needsUpdate = true
-            needsUpdateWorld = true
-        }
+    override var scale by Delegates.observable(Vector3f(1.0f, 1.0f, 1.0f)) { property, old, new -> propertyChanged(property, old, new) }
 
     /** Rotation of the Node, used to construct [model]. Setting will trigger [model] and [world] update. */
-    override var rotation: Quaternionf = Quaternionf()//by Delegates.observable(Quaternionf(0.0f, 0.0f, 0.0f, 1.0f)) { property, old, new -> propertyChanged(property, old, new) }
-    set(value) {
-        field = value
-        needsUpdate = true
-        needsUpdateWorld = true
-    }
+    override var rotation by Delegates.observable(Quaternionf(0.0f, 0.0f, 0.0f, 1.0f)) { property, old, new -> propertyChanged(property, old, new) }
 
     /** Children of the Node. */
     @Transient
@@ -189,6 +173,8 @@ open class Node(open var name: String = "Node") : Renderable, Serializable, Real
             needsUpdate = true
             needsUpdateWorld = true
         }
+
+        modifiedAt = System.nanoTime()
     }
 
     init {
