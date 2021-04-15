f1308e40b135d923336614f40af36cd7cad912ff
Ulrik Guenther <hello@ulrik.is>
2021-04-15 13:52:07 +0200

    SceneryBase/InputHandler: Add convenience functions for adding behaviours and key bindings

--- src/main/kotlin/graphics/scenery/controls/InputHandler.kt
+++ src/main/kotlin/graphics/scenery/controls/InputHandler.kt
@@ -350,4 +350,15 @@ class InputHandler(scene: Scene, renderer: Renderer, override var hub: Hub?, for
         //return reference on the Editor, so that users can hook own extra stuff
         return editorPanel
     }
+
+    operator fun plusAssign(behaviourAndBinding: Pair<String, Pair<String, Behaviour>>) {
+        addBehaviour(behaviourAndBinding.first, behaviourAndBinding.second.second)
+        addKeyBinding(behaviourAndBinding.first, behaviourAndBinding.second.first)
+    }
+
+    operator fun minusAssign(name: String) {
+        removeBehaviour(name)
+        removeKeyBinding(name)
+    }
+
 }
