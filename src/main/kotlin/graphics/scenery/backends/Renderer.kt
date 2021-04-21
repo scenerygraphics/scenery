b1c84fe66a19a6ade31380afa3c567222c616f7d
Aryaman Gupta <argupta@mpi-cbg.de>
2021-04-21 15:53:12 +0200

    Renderer: adds postRenderLambdas, property counting total rendered frames

--- src/main/kotlin/graphics/scenery/backends/Renderer.kt
+++ src/main/kotlin/graphics/scenery/backends/Renderer.kt
@@ -41,6 +41,10 @@ abstract class Renderer : Hubable {
     abstract var firstImageReady: Boolean
         protected set
 
+    /** The total number of frames rendered so far. */
+    var totalFrames = 0L
+        protected set
+
     /** [Settings] instance the renderer is using. */
     abstract var settings: Settings
 
@@ -175,7 +179,11 @@ abstract class Renderer : Hubable {
     }
 
     @Volatile protected var textureRequests = ConcurrentLinkedQueue<Pair<Texture, Channel<Texture>>>()
-    var persistentTextureRequests = ArrayList<Pair<Texture, AtomicInteger>>()
+
+    /**
+     * A list of user-defined lambdas that will be executed once per iteration of the render loop
+     */
+    val postRenderLambdas = ArrayList<()->Unit>()
 
     /**
      * Requests the renderer to update [texture]'s contents from the GPU. [onReceive] is executed
