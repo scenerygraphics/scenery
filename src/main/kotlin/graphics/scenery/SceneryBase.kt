66b569c8a711715bf53ac1a51ceeeb83b8ca0222
Ulrik Guenther <hello@ulrik.is>
2021-04-28 17:33:05 +0200

    SceneryBase: Add helper function for sanitising URLs on filesystems

--- src/main/kotlin/graphics/scenery/SceneryBase.kt
+++ src/main/kotlin/graphics/scenery/SceneryBase.kt
@@ -24,6 +24,7 @@ import org.scijava.Context
 import org.scijava.ui.behaviour.ClickBehaviour
 import java.lang.Boolean.parseBoolean
 import java.lang.management.ManagementFactory
+import java.net.URL
 import java.nio.file.Paths
 import java.util.*
 import java.util.concurrent.CountDownLatch
@@ -545,6 +546,15 @@ open class SceneryBase @JvmOverloads constructor(var applicationName: String,
                 }
             } else {
                 demoDir
+            }
+        }
+
+        @JvmStatic fun URL.sanitizedPath(): String {
+            // cuts off the initial / on Windows
+            return if(Platform.get() == Platform.WINDOWS) {
+                this.path.substringAfter("/")
+            } else {
+                this.path
             }
         }
 
