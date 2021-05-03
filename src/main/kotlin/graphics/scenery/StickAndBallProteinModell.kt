a058162abe5d3e4564139e044b8ebf8a0fafe7ac
Justin <JustinBuerger@googlemail.com>
2021-05-03 17:20:08 +0200

    StickAndBallProteinModell: tweaked the coloring.

--- src/main/kotlin/graphics/scenery/StickAndBallProteinModell.kt
+++ src/main/kotlin/graphics/scenery/StickAndBallProteinModell.kt
@@ -11,8 +11,9 @@ import org.biojava.nbio.structure.*
  *
  * @author  Justin Buerger <burger@mpi-cbg.de>
  */
-class StickAndBallProteinModell(protein: Protein): Mesh("PrimaryStructure") {
+class StickAndBallProteinModell(protein: Protein, spaceFilling: Boolean = false): Mesh("PrimaryStructure") {
     val structure = protein.structure
+    val periodicTable = PeriodicTable()
 
     init {
 
@@ -21,35 +22,36 @@ class StickAndBallProteinModell(protein: Protein): Mesh("PrimaryStructure") {
         val atomMasters = HashMap<Element, Node>()
 
         enumValues<Element>().forEach {
-            val s = Icosphere(0.05f, 2)
+            val sceneryElement = periodicTable.findElementByNumber(it.atomicNumber)
+             val s = if(spaceFilling) {
+                        if(sceneryElement.atomicRadius != null) {
+                            Icosphere(sceneryElement.atomicRadius/100f, 2)
+                        }
+                        else {
+                            Icosphere(0.05f, 2)
+                        }
+                    }
+                    else {  Icosphere(0.05f, 2) }
             s.material = ShaderMaterial.fromFiles("DefaultDeferredInstanced.vert", "DefaultDeferred.frag")
+            if (sceneryElement.color != null) {
+                s.material.diffuse = sceneryElement.color
+                //s.material.ambient = element.color
+                //s.material.specular = element.color
+            }
             s.instancedProperties["ModelMatrix"] = { s.world }
             atomMasters[it] = s
         }
 
         atoms.forEach {
+            val element = periodicTable.findElementByNumber(it.element.atomicNumber)
             val s = Node()
             s.position = (Vector3f(it.x.toFloat(), it.y.toFloat(), it.z.toFloat()))
             s.instancedProperties["ModelMatrix"] = { s.world }
-
-            when (it.element) {
-                Element.H -> {
-                    s.material.diffuse = Vector3f(1.0f, 1.0f, 1.0f)
-                }
-                Element.C -> {
-                    s.material.diffuse = Vector3f(0.0f, 0.0f, 0.0f)
-                }
-                Element.N -> {
-                    s.material.diffuse = Vector3f(0.0f, 0.0f, 1.0f)
-                }
-                Element.O -> {
-                    s.material.diffuse = Vector3f(1.0f, 0.0f, 0.0f)
-                }
-                else -> {
-                    s.material.diffuse = Vector3f(1.0f, 0.2f, 0.8f)
-                }
+            if (element.color != null) {
+                s.material.diffuse = element.color
+                //s.material.ambient = element.color
+                //s.material.specular = element.color
             }
-
             val master = atomMasters[it.element]
             master?.instances?.add(s)
         }
@@ -57,66 +59,70 @@ class StickAndBallProteinModell(protein: Protein): Mesh("PrimaryStructure") {
         atomMasters.filter { it.value.instances.isNotEmpty() }
             .forEach { this.addChild(it.value) }
 
-        val c = Cylinder(0.025f, 1.0f, 10)
-        c.material = ShaderMaterial.fromFiles("DefaultDeferredInstanced.vert", "DefaultDeferred.frag")
-        c.instancedProperties["ModelMatrix1"] = { c.model }
-        c.material.diffuse = Vector3f(1.0f, 1.0f, 1.0f)
+        if (!spaceFilling) {
+            val c = Cylinder(0.025f, 1.0f, 10)
+            c.material = ShaderMaterial.fromFiles("DefaultDeferredInstanced.vert", "DefaultDeferred.frag")
+            c.instancedProperties["ModelMatrix1"] = { c.model }
+            c.material.diffuse = Vector3f(1.0f, 1.0f, 1.0f)
 
-        val bonds: MutableList<Bond> = atoms.filter { it.bonds != null }.flatMap { it.bonds }.toMutableList()
+            val bonds: MutableList<Bond> = atoms.filter { it.bonds != null }.flatMap { it.bonds }.toMutableList()
 
-        val aminoList = AminoList()
-        val chains = structure.chains
-        val groups = chains.flatMap { it.atomGroups }
+            val aminoList = AminoList()
+            val chains = structure.chains
+            val groups = chains.flatMap { it.atomGroups }
 
-        //This creates bonds for all the amino acids stored in the pdb
-        aminoList.forEach {residue ->
-            val name = residue.name
-            //please note that these bonds are not the bonds stored in the pdb-file but the hardcoded bonds from the AminoList
-            val bondList = residue.bonds
-            groups.forEach{ group ->
-                if(group.pdbName == name) {
-                    bondList.forEach { triple ->
-                        group.atoms.forEach{ atom1 ->
-                            group.atoms.forEach {atom2 ->
-                                if((atom1.name + "'") == triple.first && (atom2.name +"'") == triple.second){
-                                    val bond = BondImpl(atom1, atom2, triple.third)
-                                    bonds.add(bond)
+            //This creates bonds for all the amino acids stored in the pdb
+            aminoList.forEach { residue ->
+                val name = residue.name
+                //please note that these bonds are not the bonds stored in the pdb-file but the hardcoded bonds from the AminoList
+                val bondList = residue.bonds
+                groups.forEach { group ->
+                    if (group.pdbName == name) {
+                        bondList.forEach { triple ->
+                            group.atoms.forEach { atom1 ->
+                                group.atoms.forEach { atom2 ->
+                                    if ((atom1.name + "'") == triple.first && (atom2.name + "'") == triple.second) {
+                                        val bond = BondImpl(atom1, atom2, triple.third)
+                                        bonds.add(bond)
+                                    }
                                 }
                             }
                         }
                     }
                 }
             }
-        }
 
-        //computes the bonds between amino acids
-        chains.forEach{ chain ->
-            chain.atomGroups.windowed(2, 1) {
-                val group1 = it[0]
-                val group2 = it[1]
-                group1.atoms.forEach{ atom1 ->
-                    group2.atoms.forEach { atom2 ->
-                        if (atom1.name == "CA" && atom2.name == "N") {
-                            val bond = BondImpl(atom1, atom2, 1)
-                            bonds.add(bond)
+            //computes the bonds between amino acids
+            chains.forEach { chain ->
+                chain.atomGroups.windowed(2, 1) {
+                    val group1 = it[0]
+                    val group2 = it[1]
+                    group1.atoms.forEach { atom1 ->
+                        group2.atoms.forEach { atom2 ->
+                            if (atom1.name == "CA" && atom2.name == "N") {
+                                val bond = BondImpl(atom1, atom2, 1)
+                                bonds.add(bond)
+                            }
                         }
                     }
                 }
             }
-        }
 
-        val cylinders = bonds.map {
-            val bond = Mesh()
-            bond.parent = this
-            val atomA = it.atomA
-            val atomB = it.atomB
-            bond.orientBetweenPoints(Vector3f(atomA.x.toFloat(), atomA.y.toFloat(), atomA.z.toFloat()),
-                Vector3f(atomB.x.toFloat(), atomB.y.toFloat(), atomB.z.toFloat()), true, true)
-            bond.instancedProperties["ModelMatrix1"] = { bond.model }
-            bond
-        }
-        c.instances.addAll(cylinders)
+            val cylinders = bonds.map {
+                val bond = Mesh()
+                bond.parent = this
+                val atomA = it.atomA
+                val atomB = it.atomB
+                bond.orientBetweenPoints(
+                    Vector3f(atomA.x.toFloat(), atomA.y.toFloat(), atomA.z.toFloat()),
+                    Vector3f(atomB.x.toFloat(), atomB.y.toFloat(), atomB.z.toFloat()), true, true
+                )
+                bond.instancedProperties["ModelMatrix1"] = { bond.model }
+                bond
+            }
+            c.instances.addAll(cylinders)
 
-        this.addChild(c)
+            this.addChild(c)
+        }
     }
 }
