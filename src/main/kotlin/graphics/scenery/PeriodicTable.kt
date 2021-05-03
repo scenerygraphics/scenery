4e5ecb3b1c2bd82685b5e1cb65f6cc967777c9a1
Domino2357 <JustinBuerger@googlemail.com>
2021-05-03 11:27:09 +0200

    PeriodicTable: fixed Json reading and implemented String conversion.

--- src/main/kotlin/graphics/scenery/PeriodicTable.kt
+++ src/main/kotlin/graphics/scenery/PeriodicTable.kt
@@ -2,39 +2,52 @@ package graphics.scenery
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.joml.Vector3f
 import java.io.FileReader
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 import java.util.*
+import kotlin.collections.ArrayList
 
+/**
+ *
+ */
 class PeriodicTable {
-    init {
-        data class ChemicalElement(@JsonProperty("AtomicNumber") val atomicNumber: Int,
-                                   @JsonProperty("Symbol") val symbol: String,
-                                   @JsonProperty("Name") val name: String,
-                                   @JsonProperty("AtomicMass")val atomicMass: Float,
-                                   @JsonProperty("CPKHexColor") val color: String,
-                                   @JsonProperty("ElectronConfiguration") val electronConfiguration: String,
-                                   @JsonProperty("Electronegativity") val electroNegativity: Float,
-                                   @JsonProperty("AtomicRadius") val atomicRadius: Float,
-                                   @JsonProperty("IonizationEnergy") val ionizationEnergy: Float,
-                                   @JsonProperty("ElectronAffinity") val electronAffinity: Float,
-                                   @JsonProperty("OxidationStates") val oxidationStates: String,
-                                   @JsonProperty("StandardState") val standardState: String,
-                                   @JsonProperty("MeltingPoint") val meltingPoint: Float,
-                                   @JsonProperty("BoilingPoint") val boilingPoint: Float,
-                                   @JsonProperty("Density") val density: Float,
-                                   @JsonProperty("GroupBlock") val groupBlock: String,
-                                   @JsonProperty("YearDiscovered")val yearDiscovered: String)
-        data class ChemCell(val cell: Map<String, ChemicalElement>)
-        data class ChemTable(val column: Map<String, ArrayList<String>>, val row: ArrayList<ChemCell>)
-        data class PeriodicTableau(val thumbnail: Map<String?, ChemTable>)
+    //data-classes to store the JsonProperties
+    data class ChemCell(@JsonProperty("Cell") val cell: ArrayList<String>)
+    data class ChemColumn(@JsonProperty("Column") val column: ArrayList<String>)
+    data class ChemTable(@JsonProperty("Columns") val columns: ChemColumn,@JsonProperty("Row") val row: ArrayList<ChemCell>)
+    data class PeriodicTableau(@JsonProperty("Table") val table: ChemTable)
 
+    val elementList = ArrayList<ChemicalElement>()
+
+    init {
         val mapper = jacksonObjectMapper()
         val file = FileReader("src/main/resources/graphics/scenery/proteins/PubChemElements_all.json").readText()
-        print(file)
         val periodicTable = mapper.readValue(file, PeriodicTableau::class.java)
-        print(periodicTable)
+    }
+
+    fun extractDataTypes(periodicTable: PeriodicTableau) {
+        periodicTable.table.row.forEach { pureStringElement ->
+            val elementNumber = pureStringElement.cell[0].toInt()
+            val symbol = pureStringElement.cell[1]
+            val name = pureStringElement.cell[2]
+            val atomicMass = pureStringElement.cell[3].toFloat()
+            val color: Vector3f //TODO
+            val electronConfiguration = pureStringElement.cell[5]
+            val electroNegativity = pureStringElement.cell[6].toFloat()
+            val atomicRadius = pureStringElement.cell[7].toFloat()
+            val ionizationEnergy = pureStringElement.cell[8].toFloat()
+            val electronAffinity = pureStringElement.cell[9].toFloat()
+            val oxidationStates = pureStringElement.cell[10]
+            val standardState = pureStringElement.cell[11]
+            val meltingPoint = pureStringElement.cell[12].toFloat()
+            val boilingPoint = pureStringElement.cell[13].toFloat()
+            val density = pureStringElement.cell[14].toFloat()
+            val groupBlock = pureStringElement.cell[15]
+            val yearDiscovered = pureStringElement.cell[16]
+
+        }
     }
 }
