b4a3c069fd05db084cc17062706ac7a25042fc9f
Ulrik Guenther <hello@ulrik.is>
2021-05-06 12:10:47 +0200

    Add WithCooldown class to enable e.g. Behaviours to not be re-triggered within a certain amount of time

---
+++ src/main/kotlin/graphics/scenery/controls/behaviours/WithCooldown.kt
@@ -0,0 +1,36 @@
+package graphics.scenery.controls.behaviours
+
+import org.scijava.ui.behaviour.ClickBehaviour
+import java.util.*
+import kotlin.concurrent.schedule
+import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
+
+/**
+ * Class to enable e.g. [ClickBehaviour]s to be activated once, and then
+ * only again after a [cooldown] given as [Duration]. Can e.g. be used
+ * in [ClickBehaviour.click]. The time [block] will be executed again after
+ * is stored with respect to the object [obj] given.
+ *
+ * @author Ulrik Guenther <hello@ulrik.is>
+ */
+@OptIn(ExperimentalTime::class)
+open class WithCooldown(val obj: Any, cooldown: Duration, block: () -> Any) {
+    companion object {
+        protected val invocations = HashSet<Any>()
+    }
+
+    init {
+        val invoked = invocations.contains(obj)
+        if (!invoked) {
+            block.invoke()
+            invocations.add(obj)
+            Timer(true).schedule(cooldown.toLongMilliseconds()) { invocations.remove(obj) }
+        }
+    }
+}
+
+@OptIn(ExperimentalTime::class)
+fun ClickBehaviour.withCooldown(cooldown: Duration, block: () -> Any) {
+    WithCooldown(this, cooldown, block)
+}
