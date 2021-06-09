7bbea57594ee6daad997e00639ff2b6152b20d49
Justin <JustinBuerger@googlemail.com>
2021-06-09 10:20:37 +0200

    Cross: modified positions

--- src/main/kotlin/graphics/scenery/Cross.kt
+++ src/main/kotlin/graphics/scenery/Cross.kt
@@ -5,9 +5,9 @@ import org.joml.Matrix4f
 import org.joml.Vector3f
 
 class Cross(private val orientPoint1: Vector3f = Vector3f(-0.3f, 0f, 0f), private val orientPoint2: Vector3f = Vector3f(0f, -0.3f, 0f)): Mesh("Cross") {
-    private val sphere = Icosphere(0.3f, 4)
-    private val cylinder1 = Cylinder(0.02f, 2f, 4)
-    private val cylinder2 = Cylinder(0.02f, 2f, 4)
+    private val sphere = Icosphere(0.03f, 4)
+    private val cylinder1 = Cylinder(0.002f, 2f, 4)
+    private val cylinder2 = Cylinder(0.002f, 2f, 4)
 
     init {
         this.addChild(sphere)
@@ -31,14 +31,19 @@ class Cross(private val orientPoint1: Vector3f = Vector3f(-0.3f, 0f, 0f), privat
         val nn = Vector3f()
         inverseMatrix.getColumn(1, nn).normalize()
         val nt = Vector3f()
-        val tr = translation
         inverseMatrix.getColumn(2, nt).normalize()
         Matrix4f(
             nb.x(), nn.x(), nt.x(), 0f,
             nb.y(), nn.y(), nt.y(), 0f,
             nb.z(), nn.z(), nt.z(), 0f,
-            tr.x(), tr.y(), tr.z(), 1f)
+            translation.x(), translation.y(), translation.z(), 1f
+        )
         cylinder1.orientBetweenPoints(inverseMatrix.transform(orientPoint1), Vector3f())
         cylinder2.orientBetweenPoints(inverseMatrix.transform(orientPoint2), Vector3f())
+        val cylPos = Vector3f()
+        translation.add(zAxis.mul(2f, cylPos), cylPos)
+        cylinder1.position = cylPos
+        cylinder2.position = cylPos
+        sphere.position = cylPos
     }
 }
