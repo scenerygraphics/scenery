251c0c3aa5f8ab6a9e6e12cadf58236f50e1aa33
ruoshan <lanruoshan@gmail.com>
2021-06-21 14:45:48 +0200

    update PupilEeyeTracking and EyeTrackingExample

--- src/test/kotlin/graphics/scenery/tests/examples/advanced/EyeTrackingExample.kt
+++ src/test/kotlin/graphics/scenery/tests/examples/advanced/EyeTrackingExample.kt
@@ -1,10 +1,11 @@
 package graphics.scenery.tests.examples.advanced
 
+import cleargl.GLVector
 import org.joml.Vector3f
 import graphics.scenery.*
 import graphics.scenery.backends.Renderer
 import graphics.scenery.controls.OpenVRHMD
-import graphics.scenery.controls.eyetracking.PupilEyeTracker
+import graphics.scenery.controls.eyetracking.PupilEyeTrackerNew
 import graphics.scenery.controls.TrackedDeviceType
 import graphics.scenery.numerics.Random
 import graphics.scenery.utils.extensions.plus
@@ -19,9 +20,11 @@ import kotlin.concurrent.thread
  * @author Ulrik Günther <hello@ulrik.is>
  */
 class EyeTrackingExample: SceneryBase("Eye Tracking Example", windowWidth = 1280, windowHeight = 720) {
-    val pupilTracker = PupilEyeTracker(calibrationType = PupilEyeTracker.CalibrationType.ScreenSpace)
+    val pupilTracker = PupilEyeTrackerNew(calibrationType = PupilEyeTrackerNew.CalibrationType.WorldSpace)
     val hmd = OpenVRHMD(seated = false, useCompositor = true)
-    val referenceTarget = Icosphere(0.005f, 2)
+    val referenceTarget = Icosphere(0.004f, 2)
+    val calibrationTarget = Icosphere(0.02f, 2)
+    val confidenceThreshold = 0.60f
 
     override fun init() {
         hub.add(SceneryElement.HMDInput, hmd)
@@ -41,9 +44,17 @@ class EyeTrackingExample: SceneryBase("Eye Tracking Example", windowWidth = 1280
 
         referenceTarget.visible = false
         referenceTarget.material.roughness = 1.0f
-        referenceTarget.material.metallic = 0.5f
+        referenceTarget.material.metallic = 0.0f
         referenceTarget.material.diffuse = Vector3f(0.8f, 0.8f, 0.8f)
-        scene.addChild(referenceTarget)
+        cam.addChild(referenceTarget)
+
+        calibrationTarget.visible = false
+        calibrationTarget.material.roughness = 1.0f
+        calibrationTarget.material.metallic = 0.0f
+        calibrationTarget.material.diffuse = Vector3f(1.0f, 1.0f, 1.0f)
+        calibrationTarget.runRecursive { it.material.diffuse = Vector3f(1.0f, 1.0f, 1.0f) }
+        cam.addChild(calibrationTarget)
+
 
         val lightbox = Box(Vector3f(20.0f, 20.0f, 20.0f), insideNormals = true)
         lightbox.name = "Lightbox"
@@ -128,36 +139,28 @@ class EyeTrackingExample: SceneryBase("Eye Tracking Example", windowWidth = 1280
                     logger.info("Starting eye tracker calibration")
                     pupilTracker.calibrate(cam, hmd,
                         generateReferenceData = true,
-                        calibrationTarget = referenceTarget)
+                        calibrationTarget = calibrationTarget)
 
                     pupilTracker.onGazeReceived = when (pupilTracker.calibrationType) {
-                        PupilEyeTracker.CalibrationType.ScreenSpace -> { gaze ->
+
+
+                        PupilEyeTrackerNew.CalibrationType.WorldSpace -> { gaze ->
                             when {
-                                gaze.confidence < 0.85f -> referenceTarget.material.diffuse = Vector3f(0.8f, 0.0f, 0.0f)
-                                gaze.confidence > 0.85f -> referenceTarget.material.diffuse = Vector3f(0.0f, 0.5f, 0.5f)
+                                gaze.confidence < confidenceThreshold -> referenceTarget.material.diffuse = Vector3f(0.8f, 0.0f, 0.0f)
+                                gaze.confidence > confidenceThreshold -> referenceTarget.material.diffuse = Vector3f(0.0f, 0.5f, 0.5f)
                                 gaze.confidence > 0.95f -> referenceTarget.material.diffuse = Vector3f(0.0f, 1.0f, 0.0f)
                             }
-
-                            if(gaze.confidence > 0.85f) {
+//                            logger.info("will judge whether the confidence is enough!")
+                            if(gaze.confidence > confidenceThreshold) {
+                                val p = Vector3f(gaze.gazeDirection().x* gaze.gazeDistance(),gaze.gazeDirection().y* gaze.gazeDistance(),gaze.gazeDirection().z* gaze.gazeDistance())
+                                logger.info(gaze.gazeDirection().toString())
+                                logger.info(gaze.gazeDistance().toString())
+                                logger.info(gaze.gazePoint().toString())
+//                                logger.info("confidence is enough")
+                                referenceTarget.position =p
                                 referenceTarget.visible = true
-                                referenceTarget.position = cam.viewportToWorld(
-                                    Vector2f(
-                                        gaze.normalizedPosition().x() * 2.0f - 1.0f,
-                                        gaze.normalizedPosition().y() * 2.0f - 1.0f),
-                                    ) + cam.forward * 0.15f
-                            }
-                        }
-
-                        PupilEyeTracker.CalibrationType.WorldSpace -> { gaze ->
-                            when {
-                                gaze.confidence < 0.85f -> referenceTarget.material.diffuse = Vector3f(0.8f, 0.0f, 0.0f)
-                                gaze.confidence > 0.85f -> referenceTarget.material.diffuse = Vector3f(0.0f, 0.5f, 0.5f)
-                                gaze.confidence > 0.95f -> referenceTarget.material.diffuse = Vector3f(0.0f, 1.0f, 0.0f)
-                            }
-
-                            if(gaze.confidence > 0.85f) {
-                                referenceTarget.visible = true
-                                referenceTarget.position = gaze.gazePoint()
+                                logger.info(calibrationTarget.position.toString())
+                                logger.info(referenceTarget.position.toString())
                             }
                         }
                     }
