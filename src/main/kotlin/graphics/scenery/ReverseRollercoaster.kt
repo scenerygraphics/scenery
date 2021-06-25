0d03fa5b4b37a97039785cf37e68049eab934725
Justin <JustinBuerger@googlemail.com>
2021-06-25 11:20:27 +0200

    ReverseRollercoaster: added inheritance; BoundingBoxSketch: added to abserve bounding boxes.

--- src/main/kotlin/graphics/scenery/ReverseRollercoaster.kt
+++ src/main/kotlin/graphics/scenery/ReverseRollercoaster.kt
@@ -1,9 +1,11 @@
 package graphics.scenery
 
+import org.scijava.ui.behaviour.ClickBehaviour
+
 /**
  * class that lets a curve or spline object fly in front of the camera from beginning to end
  */
-class ReverseRollercoaster(val cam: ()-> Camera?, val curve: Curve) {
+class ReverseRollercoaster(val cam: ()-> Camera?, val curve: Curve): ClickBehaviour {
     val frenetFrames = curve.frenetFrames
 
     /*
@@ -12,6 +14,12 @@ class ReverseRollercoaster(val cam: ()-> Camera?, val curve: Curve) {
     As the position choose the cam position plus forward vec of the cam times the length of the box.
      */
     init {
+        val boundingBox = curve.getMaximumBoundingBox()
+        val box = Box()
 
+
+        }
+    override fun click(x: Int, y: Int) {
+        TODO("Not yet implemented")
     }
 }
