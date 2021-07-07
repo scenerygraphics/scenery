4d2c2928d607405dc0811caa38bd9866df2d53b1
Justin <JustinBuerger@googlemail.com>
2021-07-07 23:14:14 +0200

    Curve: fixed transformationError

--- src/main/kotlin/graphics/scenery/Curve.kt
+++ src/main/kotlin/graphics/scenery/Curve.kt
@@ -18,7 +18,7 @@ import kotlin.math.acos
  * @param [baseShape] a lambda which returns all the baseShapes along the curve
  * @param [firstPerpendicularVector] vector to which the first frenet tangent shall be perpendicular to.
  */
-class Curve(spline: Spline, private val firstPerpendicularVector: Vector3f = Vector3f(0f, 0f, 0f),
+class Curve(spline: Spline, partitionAlongControlpoints: Boolean = true, private val firstPerpendicularVector: Vector3f = Vector3f(0f, 0f, 0f),
             baseShape: () -> List<List<Vector3f>>): Mesh("CurveGeometry"), HasGeometry {
     val chain = spline.splinePoints()
     private val sectionVertices = spline.verticesCountPerSection()
@@ -52,85 +52,98 @@ class Curve(spline: Spline, private val firstPerpendicularVector: Vector3f = Vec
                     tr.x(), tr.y(), tr.z(), 1f)
         }
         val baseShapes = baseShape.invoke()
-
-        var partialCurveSize = 1
-        baseShapes.windowed(2, 1) { frame ->
-            when (frame[0].size) {
-                frame[1].size -> {
-                    partialCurveSize++
-                }
-                else -> {
-                    countList.add(partialCurveSize)
-                    partialCurveSize = 1
-                }
+        val transformedBaseShapes = ArrayList<List<Vector3f>>(baseShapes.size)
+        baseShapes.forEachIndexed { index, shape ->
+            val tranformedShape = ArrayList<Vector3f>(shape.size)
+            shape.forEach { point ->
+                val transformedPoint = Vector3f()
+                bases[index].transformPosition(point, transformedPoint)
+                tranformedShape.add(transformedPoint)
             }
-        }
-        countList.add(partialCurveSize)
-        var position = 0
-        var lastShapeUnique = false
-        if(countList.last() == 1) {
-            countList.removeAt(countList.lastIndex)
-            lastShapeUnique = true
+            transformedBaseShapes.add(tranformedShape)
         }
 
-        countList.forEach {count ->
-            val partialCurve = Mesh("partialCurve")
-            val partialCurveGeometry = ArrayList<ArrayList<Vector3f>>(count)
-            for(j in 0 until count) {
-                val shape = baseShapes[position]
-                val shapeVertexList = ArrayList<Vector3f>(shape.size)
-                shape.forEach {
-                    val vec = Vector3f()
-                    shapeVertexList.add(bases[position].transformPosition(it, vec))
+        if(partitionAlongControlpoints) {
+            val subshapes = transformedBaseShapes.windowed(sectionVertices+1, sectionVertices+1)
+            subshapes.forEachIndexed { index, list ->
+                //fill gaps
+                val arrayList = list as ArrayList
+                if(index != subshapes.size -1) {
+                    arrayList.add(arrayList[0])
                 }
-                partialCurveGeometry.add(shapeVertexList)
-                position++
-            }
-            val helpPosition = position
-            //fill the gaps between the different shapes
-            if(helpPosition < bases.lastIndex) {
-                val shape = baseShapes[helpPosition-1]
-                val shapeVertexList = ArrayList<Vector3f>(shape.size)
-                shape.forEach {
-                    val vec = Vector3f()
-                    shapeVertexList.add(bases[helpPosition].transformPosition(it, vec))
-                }
-                partialCurveGeometry.add(shapeVertexList)
-            }
-            //edge case: the last shape is different from its predecessor
-            if(lastShapeUnique && helpPosition == bases.lastIndex) {
-                val shape = baseShapes[helpPosition-1]
-                val shapeVertexList = ArrayList<Vector3f>(shape.size)
-                shape.forEach {
-                    val vec = Vector3f()
-                    shapeVertexList.add(bases[helpPosition].transformPosition(it, vec))
-                }
-                partialCurveGeometry.add(shapeVertexList)
-            }
-            val remainder = partialCurveGeometry.size%sectionVertices
-            val n = ((partialCurveGeometry.size)/sectionVertices)
-            val add = if(n == 0) { 1 } else { remainder/n }
-            partialCurveGeometry.windowed(sectionVertices + add, sectionVertices-1 + add, true)
-            { section ->
-                val i = when {
-                    section.contains(partialCurveGeometry.first()) && section.contains(partialCurveGeometry.last()) -> {
+                val i = when (index) {
+                    0 -> {
                         0
                     }
-                    section.contains(partialCurveGeometry.first()) -> {
+                    subshapes.size - 1 -> {
                         1
                     }
-                    section.contains(partialCurveGeometry.last()) -> {
+                    else -> {
                         2
                     }
+                }
+                val partialCurve = PartialCurve(calculateTriangles(arrayList, i))
+                this.addChild(partialCurve)
+            }
+        }
+        else {
+            var partialCurveSize = 1
+            baseShapes.windowed(2, 1) { frame ->
+                when (frame[0].size) {
+                    frame[1].size -> {
+                        partialCurveSize++
+                    }
                     else -> {
-                        3
+                        countList.add(partialCurveSize)
+                        partialCurveSize = 1
                     }
                 }
-                val partialCurveSectionVertices = calculateTriangles(section, i)
-                val partialCurveSection = PartialCurve(partialCurveSectionVertices)
-                partialCurve.addChild(partialCurveSection)
             }
-            this.addChild(partialCurve)
+            countList.add(partialCurveSize)
+            var position = 0
+            var lastShapeUnique = false
+            if (countList.last() == 1) {
+                countList.removeAt(countList.lastIndex)
+                lastShapeUnique = true
+            }
+
+            countList.forEachIndexed { index, count ->
+                val partialCurveGeometry = ArrayList<List<Vector3f>>(count)
+                for (j in 0 until count) {
+                    partialCurveGeometry.add(transformedBaseShapes[position])
+                    position++
+                }
+                val helpPosition = position
+                //fill the gaps between the different shapes
+                if (helpPosition < bases.lastIndex) {
+                    val shape = baseShapes[helpPosition - 1]
+                    val shapeVertexList = ArrayList<Vector3f>(shape.size)
+                    shape.forEach {
+                        val vec = Vector3f()
+                        shapeVertexList.add(bases[helpPosition].transformPosition(it, vec))
+                    }
+                    partialCurveGeometry.add(shapeVertexList)
+                }
+                //edge case: the last shape is different from its predecessor
+                if (lastShapeUnique && helpPosition == bases.lastIndex) {
+                    val shape = baseShapes[helpPosition - 1]
+                    val shapeVertexList = ArrayList<Vector3f>(shape.size)
+                    shape.forEach {
+                        val vec = Vector3f()
+                        shapeVertexList.add(bases[helpPosition].transformPosition(it, vec))
+                    }
+                    partialCurveGeometry.add(shapeVertexList)
+                }
+                val i = if (index == 0) {
+                    0
+                } else if (index == countList.size - 1) {
+                    1
+                } else {
+                    2
+                }
+                val partialCurve = PartialCurve(calculateTriangles(partialCurveGeometry, i))
+                this.addChild(partialCurve)
+            }
         }
     }
 
