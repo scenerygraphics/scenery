62f3cbda06cb61812a2355e09f294625ba6b23ba
Justin <JustinBuerger@googlemail.com>
2021-07-08 18:30:31 +0200

    RainobowTests: corrected iteration depth

--- src/test/kotlin/graphics/scenery/tests/unit/RainbowTests.kt
+++ src/test/kotlin/graphics/scenery/tests/unit/RainbowTests.kt
@@ -19,12 +19,10 @@ class RainbowTests {
     @Test
     fun rainbowTest() {
         val mesh = Mesh("Parent")
-        val child1 = Mesh("Child1")
-        val child2 = Mesh("Child2")
-        mesh.addChild(child1)
-        child1.addChild(child2)
+        val child = Mesh("Child1")
+        mesh.addChild(child)
         for(i in 1 .. 7) {
-            child2.addChild(Mesh("Child$i"))
+            child.addChild(Mesh("Child$i"))
         }
         val rainbow = Rainbow()
         rainbow.colorVector(mesh)
@@ -32,7 +30,7 @@ class RainbowTests {
             Vector3f(255f, 255f, 0f), Vector3f(0f, 128f, 0f), Vector3f(0f, 0f, 255f),
             Vector3f(75f, 0f, 135f), Vector3f(238f, 130f, 238f))
         val rainbowPalette = rainbowPaletteNotScaled.map { it.mul(1/255f) }
-        val children = child2.children
+        val children = child.children
         for(j in 0 until 6) {
             assertEquals(children[j].material.diffuse, rainbowPalette[j])
         }
