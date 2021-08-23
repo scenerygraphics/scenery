904a39318a27f09a13ad249444b871a608deda0c
Justin <JustinBuerger@googlemail.com>
2021-08-23 14:49:03 +0200

    ReverseRollercoaster: added translation.

--- src/main/kotlin/graphics/scenery/flythroughs/ReverseRollercoaster.kt
+++ src/main/kotlin/graphics/scenery/flythroughs/ReverseRollercoaster.kt
@@ -1,26 +1,28 @@
-package graphics.scenery
+package graphics.scenery.flythroughs
 
+import graphics.scenery.Camera
+import graphics.scenery.Scene
 import graphics.scenery.geometry.Curve
+import org.joml.Vector3f
 import org.scijava.ui.behaviour.ClickBehaviour
 
 /**
  * class that lets a curve or spline object fly in front of the camera from beginning to end
  */
-class ReverseRollercoaster(val cam: ()-> Camera?, val curve: Curve): ClickBehaviour {
-    val frenetFrames = curve.frenetFrames
-
-    /*
-    Top level idea: create a box, corresponding to the boundingBox of the curve. Then set rotation of the curve equal to the rotation of the box
-    (need to find out whether the box is even necessary) take the box and rotate it along the curve with help of the FrenetFrames.
-    As the position choose the cam position plus forward vec of the cam times the length of the box.
-     */
-    init {
-        val boundingBox = curve.getMaximumBoundingBox()
-        val box = Box()
-
-
-        }
+class ReverseRollercoaster(val scene: Scene, val cam: ()->Camera?, val name: String): ClickBehaviour {
+    val curve = scene.children.filter{it.name == name}[0]
+    val camera = cam.invoke()
+    var i = 0
     override fun click(x: Int, y: Int) {
-        TODO("Not yet implemented")
+        if (curve is Curve) {
+            val frames = curve.frenetFrames
+            if (i <= frames.lastIndex) {
+                val intermediateVector = Vector3f()
+                curve.spatial().position.add((frames[i].translation.sub(curve.spatial().position, intermediateVector)))
+                curve.spatial().position.add(camera?.spatial()?.position?.add(camera?.forward?.mul(0.5f, intermediateVector)), intermediateVector)
+                scene.children.filter{it.name == name}[0].ifSpatial { position =  curve.spatial().position}
+                i += 1
+            }
+        }
     }
 }
