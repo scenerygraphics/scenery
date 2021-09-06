afa51aeb8157f8137efe0433d9117d83b2a9f7d5
Justin <JustinBuerger@googlemail.com>
2021-09-06 12:28:45 +0200

    RibbonDiagramClickByClick: tweaked a bit

--- src/main/kotlin/graphics/scenery/controls/behaviours/RibbonDiagramClickByClick.kt
+++ src/main/kotlin/graphics/scenery/controls/behaviours/RibbonDiagramClickByClick.kt
@@ -4,15 +4,17 @@ import graphics.scenery.Scene
 import graphics.scenery.proteins.RibbonDiagram
 import org.scijava.ui.behaviour.ClickBehaviour
 
-class RibbonDiagramClickByClick(private val ribbon: RibbonDiagram, val scene: Scene): ClickBehaviour {
+class RibbonDiagramClickByClick(private val name: String, val scene: Scene): ClickBehaviour {
     private var i = 0
-    init {
-        ribbon.visible = false
-        scene.addChild(ribbon)
-    }
 
     override fun click(x: Int, y: Int) {
-        ribbon.children.flatMap { subProtein -> subProtein.children }.flatMap { curve -> curve.children }[i].visible = true
-        i+=1
+        if(scene.children.filter { it.name == name }[0] is RibbonDiagram) {
+            if (i <= scene.children.filter { it.name == name }[0].children.flatMap { subProtein -> subProtein.children }
+                    .flatMap { curve -> curve.children }.lastIndex) {
+                scene.children.filter { it.name == name }[0].children.flatMap { subProtein -> subProtein.children }
+                    .flatMap { curve -> curve.children }[i].visible = true
+            }
+            i+=1
+        }
     }
 }
