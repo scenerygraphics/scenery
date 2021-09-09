af9629f0d0227faa373483d002829d3f1f86ffc5
Aryaman Gupta <argupta@mpi-cbg.de>
2021-09-09 14:25:25 +0200

    changes to use ndc coords everywhere

--- src/test/kotlin/graphics/scenery/tests/examples/volumes/VDIRenderingExample.kt
+++ src/test/kotlin/graphics/scenery/tests/examples/volumes/VDIRenderingExample.kt
@@ -3,22 +3,25 @@ package graphics.scenery.tests.examples.volumes
 import graphics.scenery.*
 import graphics.scenery.backends.Renderer
 import graphics.scenery.backends.Shaders
+import graphics.scenery.backends.vulkan.VulkanRenderer
 import graphics.scenery.compute.ComputeMetadata
 import graphics.scenery.compute.InvocationType
 import graphics.scenery.textures.Texture
 import graphics.scenery.utils.Image
+import graphics.scenery.utils.SystemHelpers
 import graphics.scenery.volumes.VolumeManager
 import org.joml.Quaternionf
 import org.joml.Vector3f
 import org.joml.Vector3i
 import org.lwjgl.system.MemoryUtil
 import java.io.File
+import java.util.concurrent.atomic.AtomicInteger
 import kotlin.concurrent.thread
 
 /**
  * @author Aryaman Gupta <argupta@mpi-cbg.de>
  */
-class VDIRenderingExample : SceneryBase("VDI Rendering", 600, 600, wantREPL = false) {
+class VDIRenderingExample : SceneryBase("VDI Rendering", 1280, 720, wantREPL = false) {
 
     override fun init () {
         renderer = hub.add(Renderer.createRenderer(hub, applicationName, scene, windowWidth, windowHeight))
@@ -32,17 +35,30 @@ class VDIRenderingExample : SceneryBase("VDI Rendering", 600, 600, wantREPL = fa
         val cam = DetachedHeadCamera()
         with(cam) {
             position = Vector3f(-4.365f, 0.38f, 0.62f)
-            perspectiveCamera(50.0f, 600, 600)
+            perspectiveCamera(50.0f, windowWidth, windowHeight)
 
             scene.addChild(this)
         }
 
-        cam.position = Vector3f(3.213f, 8.264E-1f, -9.844E-1f)
-        cam.rotation = Quaternionf(3.049E-2, 9.596E-1, -1.144E-1, -2.553E-1)
+//        cam.position = Vector3f(3.213f, 8.264E-1f, -9.844E-1f)
+//        cam.rotation = Quaternionf(3.049E-2, 9.596E-1, -1.144E-1, -2.553E-1)
 
-        val numSupersegments = 40
+//         optimized depth calculation working at this view point, opacity calculation looking reasonable
+        cam.rotation = Quaternionf(5.449E-2,  8.801E-1, -1.041E-1, -4.601E-1)
+        cam.position = Vector3f(6.639E+0f,  1.092E+0f, -1.584E-1f)
 
-        val buff: ByteArray = File("/home/aryaman/Repositories/openfpm_pdata/example/Grid/3_gray_scott_3d/size:1Final_VDICol200.raw").readBytes()
+//        same as previous
+//        cam.position = Vector3f(1.881E+0f,  5.558E+0f, -7.854E-1f)
+//        cam.rotation = Quaternionf(-2.733E-2, 9.552E-1, 2.793E-1, -9.365E-2)
+
+//        cam.position = Vector3f(-3.435E+0f,  1.109E+0f,  6.433E+0f)
+//        cam.rotation = Quaternionf(-3.985E-2,  5.315E-1, -2.510E-2,  8.457E-1)
+
+        cam.farPlaneDistance = 20.0f
+
+        val numSupersegments = 20
+
+        val buff: ByteArray = File("/home/aryaman/Repositories/scenery-insitu/VDI10_ndc").readBytes()
 
         val opBuffer = MemoryUtil.memCalloc(windowWidth * windowHeight * 4)
         val bufferVDI = MemoryUtil.memCalloc(3 * windowWidth * windowHeight * numSupersegments * 4)
@@ -53,7 +69,8 @@ class VDIRenderingExample : SceneryBase("VDI Rendering", 600, 600, wantREPL = fa
 
         val compute = Node()
         compute.name = "compute node"
-        compute.material = ShaderMaterial(Shaders.ShadersFromFiles(arrayOf("RaycastVDI.comp"), this::class.java))
+//        compute.material = ShaderMaterial(Shaders.ShadersFromFiles(arrayOf("EfficientVDIRaycast.comp"), this::class.java))
+        compute.material = ShaderMaterial(Shaders.ShadersFromFiles(arrayOf("EfficientVDIRaycast.comp"), this::class.java))
         compute.material.textures["OutputViewport"] = Texture.fromImage(Image(opBuffer, windowWidth, windowHeight), usage = hashSetOf(Texture.UsageType.LoadStoreImage, Texture.UsageType.Texture))
         compute.material.textures["InputVDI"] = Texture(Vector3i(3*numSupersegments, windowHeight, windowWidth), 4, contents = bufferVDI, usageType = hashSetOf(Texture.UsageType.LoadStoreImage, Texture.UsageType.Texture))
         compute.metadata["ComputeMetadata"] = ComputeMetadata(
@@ -67,22 +84,27 @@ class VDIRenderingExample : SceneryBase("VDI Rendering", 600, 600, wantREPL = fa
         scene.addChild(plane)
         plane.material.textures["diffuse"] = compute.material.textures["OutputViewport"]!!
         thread {
-//            while(true) {
-//
-//                logger.info(cam.view.toString())
-//
-//                logger.info("The projection matrix is:")
-//
-//                logger.info(cam.projection.toString())
-//                Thread.sleep(2000)
-//            }
-//            Thread.sleep(10000)
-//            cam.position = Vector3f(3.213f, 8.264E-1f, -9.844E-1f)
-//            cam.rotation = Quaternionf(3.049E-2, 9.596E-1, -1.144E-1, -2.553E-1)
-//            Thread.sleep(40000)
-//            renderer?.shouldClose = true
+            Thread.sleep(50000)
+            renderer?.shouldClose = true
         }
 
+//        val opTexture = compute.material.textures["OutputViewport"]!!
+//        var cnt = AtomicInteger(0)
+//        (renderer as VulkanRenderer).persistentTextureRequests.add(opTexture to cnt)
+//
+//        thread {
+//            if(cnt.get() == 1) {
+//                val buf = opTexture.contents
+//                if (buf != null) {
+//                    SystemHelpers.dumpToFile(buf, "bruteforce.raw")
+//                }
+//            }
+//        }
+//        thread {
+//            Thread.sleep(2000)
+//            println("${cam.position}")
+//            println("${cam.rotation}")
+//        }
     }
 
     companion object {
