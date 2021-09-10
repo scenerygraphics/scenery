e06a75b1eada1e7df1b975853c7a4852a7dc3902
Justin <JustinBuerger@googlemail.com>
2021-09-10 16:39:25 +0200

    VRSelectionStarTree: dragbehaviuour for star trees

---
+++ src/main/kotlin/graphics/scenery/controls/behaviours/VRSelectionStarTree.kt
@@ -0,0 +1,134 @@
+package graphics.scenery.controls.behaviours
+
+import graphics.scenery.Scene
+import graphics.scenery.attribute.spatial.Spatial
+import graphics.scenery.controls.OpenVRHMD
+import graphics.scenery.controls.StarTree
+import graphics.scenery.controls.TrackedDeviceType
+import graphics.scenery.controls.TrackerRole
+import graphics.scenery.utils.LazyLogger
+import graphics.scenery.utils.Wiggler
+import graphics.scenery.utils.extensions.minus
+import org.joml.Quaternionf
+import org.joml.Vector3f
+import org.scijava.ui.behaviour.DragBehaviour
+
+/**
+ * A selection wheel to let the user choose between different actions.
+ *
+ * The list of selectable actions can be changed dynamically.
+ *
+ * @param actions List of named lambdas which can be selected by the user
+ * @param cutoff  after this distance between controller and targets no action will be selected if the button is released
+ */
+class VRSelectionStarTree(
+    val controller: Spatial,
+    val scene: Scene,
+    val getHmdPos: () -> Vector3f,
+    val starTree: StarTree,
+    val cutoff: Float = 0.1f
+) : DragBehaviour {
+    protected val logger by LazyLogger(System.getProperty("scenery.LogLevel", "info"))
+    private var activeWiggler: Wiggler? = null
+    private var currentStarTree: StarTree = starTree
+
+    override fun init(x: Int, y: Int) {
+        starTree.spatial {
+            position = controller.worldPosition()
+        }
+        starTree.update.add {
+            starTree.spatial {
+                val hmdp = getHmdPos()
+                val diff = (hmdp - position).normalize()
+                rotation = Quaternionf().rotationTo(Vector3f(0f, 0f, 1f), diff)
+
+            }
+        }
+        scene.addChild(starTree)
+    }
+
+    override fun drag(x: Int, y: Int) {
+
+        val closestStarTree = closestStarTree()
+
+        if (closestStarTree.distance > cutoff) {
+            activeWiggler?.deativate()
+            activeWiggler = null
+
+        } else if (activeWiggler?.target != closestStarTree.starTree.children.filter { it.name == "StarTreeSphere" }[0].ifSpatial {  }) {
+            activeWiggler?.deativate()
+            activeWiggler = Wiggler(closestStarTree.starTree.children.filter { it.name == "StarTreeSphere" }[0].ifSpatial {  }!!, 0.01f)
+            if(closestStarTree.isParent) {
+                currentStarTree.decrementRoot()
+            }
+            else {
+                currentStarTree.inkrementRoot()
+                closestStarTree.starTree.setRoot()
+            }
+            currentStarTree = closestStarTree.starTree
+        }
+
+    }
+
+    override fun end(x: Int, y: Int) {
+        val closestStarTree = closestStarTree()
+
+        if (closestStarTree.distance < cutoff) {
+            closestStarTree.starTree.action()
+        }
+
+        activeWiggler?.deativate()
+        activeWiggler = null
+
+        scene.removeChild(starTree)
+    }
+
+    /**
+     * @return (closest actionSphere) to (distance to controller)
+     */
+    private fun closestStarTree(): StarTreeDistance {
+        val distanceToParent = currentStarTree.parentStarTree?.spatial()?.worldPosition()?.distance(controller.worldPosition())
+        val distanceToChildren = currentStarTree.starTreeChildren.map {it to it.spatial().worldPosition().distance(controller.worldPosition()) }.sortedByDescending{it.second}[0]
+        return if(currentStarTree.parentStarTree != null && distanceToParent!! <= distanceToChildren.second) {
+            StarTreeDistance(currentStarTree.parentStarTree!!, distanceToParent, true)
+        } else {
+            StarTreeDistance(distanceToChildren.first, distanceToChildren.second, false)
+        }
+    }
+
+    companion object {
+        private data class StarTreeDistance(val starTree: StarTree, val distance: Float, val isParent: Boolean)
+        /**
+         * Convenience method for adding tool select behaviour
+         */
+        fun createAndSet(
+            scene: Scene,
+            hmd: OpenVRHMD,
+            getHmdPos: () -> Vector3f,
+            button: List<OpenVRHMD.OpenVRButton>,
+            controllerSide: List<TrackerRole>,
+            starTree: StarTree,
+        ) {
+            hmd.events.onDeviceConnect.add { _, device, _ ->
+                if (device.type == TrackedDeviceType.Controller) {
+                    device.model?.let { controller ->
+                        if (controllerSide.contains(device.role)) {
+                            val name = "VRDrag:${hmd.trackingSystemName}:${device.role}:$button"
+                            val vrToolSelector = VRSelectionStarTree(
+                                controller.children.first().spatialOrNull()
+                                    ?: throw IllegalArgumentException("The target controller needs a spatial."),
+                                scene,
+                                getHmdPos,
+                                starTree
+                            )
+                            hmd.addBehaviour(name, vrToolSelector)
+                            button.forEach {
+                                hmd.addKeyBinding(name, device.role, it)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
