456e1a9be93eeb41942ef216b4c12cc5657f3218
Justin <JustinBuerger@googlemail.com>
2021-11-09 18:16:55 +0100

    ProteinBuilder: added all the pictures for aminoacids according to their IUPAc abbreviation

--- src/main/kotlin/graphics/scenery/flythroughs/ProteinBuilder.kt
+++ src/main/kotlin/graphics/scenery/flythroughs/ProteinBuilder.kt
@@ -4,35 +4,30 @@ import graphics.scenery.Box
 import graphics.scenery.Camera
 import graphics.scenery.Scene
 
-import graphics.scenery.geometry.Curve
-import graphics.scenery.geometry.FrenetFrame
-import graphics.scenery.geometry.FrenetFramesCalc
-import graphics.scenery.geometry.UniformBSpline
-import graphics.scenery.proteins.Axis
-import graphics.scenery.proteins.Helix
 import graphics.scenery.proteins.RibbonDiagram
 import graphics.scenery.textures.Texture
 import graphics.scenery.utils.Image
-import graphics.scenery.utils.LazyLogger
 import org.joml.Quaternionf
-import org.joml.Vector2f
 import org.joml.Vector3f
 import org.scijava.ui.behaviour.ClickBehaviour
-import org.slf4j.Logger
-import java.text.DecimalFormat
-import kotlin.math.acos
 
 class ProteinBuilder(ribbonDiagram: RibbonDiagram, override val cam: ()-> Camera?,  val scene: Scene,
                      private val name: String): ProteinRollercoaster(ribbonDiagram, cam), ClickBehaviour {
-    val img = Image.fromResource("L-Glutamic-Acid.jpg", ProteinBuilder::class.java)
-    var k = 0
-    val residueCount = ribbonDiagram.children.flatMap { chains -> chains.children }.flatMap { residues -> residues.children }.size
-    val framesCount = listOfCameraFrames.size
+
+    // ribbon diagram to work with the residues later on
+    private val ribbonDiagram = ribbonDiagram
+    // hash map of all amino acid abbreviations as well as their full name and chemical category (acidic, basic, etc.)
+    private val abbreviations = IUPACAbbreviationsReader().abbrevations
+    // the number of points the camera wanders through is not clearly linked to the numbers of residues, hence, we'll divide it by the number of residues
     private val cameraFramesPerStepFloat = listOfCameraFrames.size.toFloat()/ribbonDiagram.children.flatMap { chains -> chains.children }.flatMap { residues -> residues.children }.size
     //toInt() cuts away all the decimal places after 0
     private val cameraFramesPerStep = cameraFramesPerStepFloat.toInt()
     //for more precision we will use the first decimal place after the comma as well
     private val remainder = ((cameraFramesPerStepFloat-cameraFramesPerStep)*10).toInt()
+    //counting variable to keep track of the current residue
+    var k = 0
+    // all images of amino acids
+    private val allImages = fillUpImageMap(abbreviations)
     override fun click(x: Int, y: Int) {
         if(k > 0) {
             val startingPoint = if(k%10 <= remainder) { 0 } else { 1 }
@@ -40,14 +35,19 @@ class ProteinBuilder(ribbonDiagram: RibbonDiagram, override val cam: ()-> Camera
                 flyToNextPoint()
             }
         }
+        //TODO the index gets too large!
+        print(ribbonDiagram.protein.structure.chains.flatMap { it.atomGroups }.filter { it.isAminoAcid }[k].pdbName)
+        val aaImage = allImages[ribbonDiagram.protein.structure.chains.flatMap { it.atomGroups }.filter { it.isAminoAcid }[k].pdbName]
         //remove old pic
         scene.removeChild("le box du win")
         //add the amino acid picture
         val box = Box(Vector3f(1.0f, 1.0f, 1.0f))
         box.name = "le box du win"
-        val height = img.height
-        val width = img.width
-        box.spatial().scale = Vector3f(width/height.toFloat(), 1f, 0f)
+        val height = aaImage?.height
+        val width = aaImage?.width
+        if (width != null && height != null) {
+                box.spatial().scale = Vector3f(width/height.toFloat(), 1f, 0f)
+        }
         box.spatial().rotation = Quaternionf(camera?.spatial()?.rotation).conjugate()
         box.spatial().position = Vector3f(camera?.spatial()?.position)
         val forwardTimesTwo = Vector3f()
@@ -58,7 +58,7 @@ class ProteinBuilder(ribbonDiagram: RibbonDiagram, override val cam: ()-> Camera
             box.spatial().position.add(camera?.forward?.mul(2f, forwardTimesTwo))
         }
         box.material {
-            textures["alphamask"] = Texture.fromImage(img)
+            if(aaImage != null) { textures["alphamask"] = Texture.fromImage(aaImage) }
             metallic = 0.3f
             roughness = 0.9f
         }
@@ -72,5 +72,15 @@ class ProteinBuilder(ribbonDiagram: RibbonDiagram, override val cam: ()-> Camera
             }
         }
         k += 1
+    }
+
+    companion object LoadImages {
+        fun fillUpImageMap(abbreviations: HashMap<String, IUPACAbbreviationsReader.IUPACAbbrevation>): HashMap<String, Image> {
+            val images = HashMap<String, Image>(20)
+            abbreviations.forEach { aminoAcid ->
+                images[aminoAcid.key] = Image.fromResource("${aminoAcid.value.chemicalCategory}/${aminoAcid.value.fullName}.jpg", ProteinBuilder::class.java)
+            }
+            return images
+        }
     }
 }
