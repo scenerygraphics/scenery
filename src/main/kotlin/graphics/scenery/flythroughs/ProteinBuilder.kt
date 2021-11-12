9e399a75ebf1ab222638bb41803356fe93807bb3
Justin <JustinBuerger@googlemail.com>
2021-11-12 18:41:57 +0100

    ProteinBuilder: introduced logical distinction between camera movement and amino acid proceedings

--- src/main/kotlin/graphics/scenery/flythroughs/ProteinBuilder.kt
+++ src/main/kotlin/graphics/scenery/flythroughs/ProteinBuilder.kt
@@ -17,7 +17,7 @@ class ProteinBuilder(ribbonDiagram: RibbonDiagram, override val cam: ()-> Camera
     // ribbon diagram to work with the residues later on
     private val ribbonDiagram = ribbonDiagram
     // hash map of all amino acid abbreviations as well as their full name and chemical category (acidic, basic, etc.)
-    private val abbreviations = IUPACAbbreviationsReader().abbrevations
+    val abbreviations = IUPACAbbreviationsReader().abbrevations
     // the number of points the camera wanders through is not clearly linked to the numbers of residues, hence, we'll divide it by the number of residues
     private val cameraFramesPerStepFloat = listOfCameraFrames.size.toFloat()/ribbonDiagram.children.flatMap { chains -> chains.children }.flatMap { residues -> residues.children }.size
     //toInt() cuts away all the decimal places after 0
@@ -26,6 +26,7 @@ class ProteinBuilder(ribbonDiagram: RibbonDiagram, override val cam: ()-> Camera
     private val remainder = ((cameraFramesPerStepFloat-cameraFramesPerStep)*10).toInt()
     //counting variable to keep track of the current residue
     var k = 0
+    var currentAminoCode: String? = ribbonDiagram.protein.structure.chains.flatMap { it.atomGroups }.filter { it.isAminoAcid }[k].pdbName
     // all images of amino acids
     private val allImages = fillUpImageMap(abbreviations)
     override fun click(x: Int, y: Int) {
@@ -35,40 +36,42 @@ class ProteinBuilder(ribbonDiagram: RibbonDiagram, override val cam: ()-> Camera
                 flyToNextPoint()
             }
         }
-        val aaImage = if(k <= (ribbonDiagram.protein.structure.chains.flatMap { it.atomGroups }.filter { it.isAminoAcid }.lastIndex)) {
-            allImages[ribbonDiagram.protein.structure.chains.flatMap { it.atomGroups }.filter { it.isAminoAcid }[k].pdbName]
-        } else { null }
-        //remove old pic
-        scene.removeChild("le box du win")
-        //add the amino acid picture
-        val box = Box(Vector3f(1.0f, 1.0f, 1.0f))
-        box.name = "le box du win"
-        val height = aaImage?.height
-        val width = aaImage?.width
-        if (width != null && height != null) {
-                box.spatial().scale = Vector3f(width/height.toFloat(), 1f, 0f)
-        }
-        box.spatial().rotation = Quaternionf(camera?.spatial()?.rotation).conjugate()
-        box.spatial().position = Vector3f(camera?.spatial()?.position)
-        val forwardTimesTwo = Vector3f()
-        if(camera?.targeted == true) {
-            box.spatial().position.add(camera.target.mul(2f, forwardTimesTwo))
-        }
-        else {
-            box.spatial().position.add(camera?.forward?.mul(2f, forwardTimesTwo))
-        }
-        box.material {
-            if(aaImage != null) {
-                textures["diffuse"] = Texture.fromImage(aaImage) }
+        if (k <= (ribbonDiagram.protein.structure.chains.flatMap { it.atomGroups }.filter { it.isAminoAcid }.lastIndex)) {
+            currentAminoCode =
+                ribbonDiagram.protein.structure.chains.flatMap { it.atomGroups }.filter { it.isAminoAcid }[k].pdbName
+            val aaImage = allImages[ribbonDiagram.protein.structure.chains.flatMap { it.atomGroups }
+                .filter { it.isAminoAcid }[k].pdbName]
+            //remove old pic
+            scene.removeChild("le box du win")
+            //add the amino acid picture
+            val box = Box(Vector3f(1.0f, 1.0f, 1.0f))
+            box.name = "le box du win"
+            val height = aaImage?.height
+            val width = aaImage?.width
+            if (width != null && height != null) {
+                box.spatial().scale = Vector3f(width / height.toFloat(), 1f, 0f)
+            }
+            box.spatial().rotation = Quaternionf(camera?.spatial()?.rotation).conjugate()
+            box.spatial().position = Vector3f(camera?.spatial()?.position)
+            val forwardTimesTwo = Vector3f()
+            if (camera?.targeted == true) {
+                box.spatial().position.add(camera.target.mul(2f, forwardTimesTwo))
+            } else {
+                box.spatial().position.add(camera?.forward?.mul(2f, forwardTimesTwo))
+            }
+            box.material {
+                if (aaImage != null) {
+                    textures["diffuse"] = Texture.fromImage(aaImage)
+                }
                 scene.addChild(box)
-        }
+            }
 
-
-        if(scene.children.filter { it.name == name }[0] is RibbonDiagram) {
-            if (k <= scene.children.filter { it.name == name }[0].children.flatMap { subProtein -> subProtein.children }
-                    .flatMap { curve -> curve.children }.lastIndex) {
-                scene.children.filter { it.name == name }[0].children.flatMap { subProtein -> subProtein.children }
-                    .flatMap { curve -> curve.children }[k].visible = true
+            if (scene.children.filter { it.name == name }[0] is RibbonDiagram) {
+                if (k <= scene.children.filter { it.name == name }[0].children.flatMap { subProtein -> subProtein.children }
+                        .flatMap { curve -> curve.children }.lastIndex) {
+                    scene.children.filter { it.name == name }[0].children.flatMap { subProtein -> subProtein.children }
+                        .flatMap { curve -> curve.children }[k].visible = true
+                }
             }
         }
         k += 1
