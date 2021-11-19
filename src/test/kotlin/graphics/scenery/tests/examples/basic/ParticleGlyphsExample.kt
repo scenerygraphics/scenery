e94ffc10a1e59583a457d3084ce70756d8a6fff0
PowerOfNames <PowerOfNames@web.de>
2021-11-19 15:56:39 +0100

    ParticleGlyphsExample: changed particle generation to repeat{} loop

--- src/test/kotlin/graphics/scenery/tests/examples/basic/ParticleGlyphsExample.kt
+++ src/test/kotlin/graphics/scenery/tests/examples/basic/ParticleGlyphsExample.kt
@@ -4,11 +4,8 @@ import org.joml.Vector3f
 import graphics.scenery.*
 import graphics.scenery.backends.Renderer
 import graphics.scenery.attribute.material.Material
-import kotlin.concurrent.thread
-
 
 import graphics.scenery.primitives.ParticleGlyphs
-
 
 import kotlin.random.Random
 
@@ -17,37 +14,53 @@ class ParticleGlyphsExample : SceneryBase("ParticleGlyphsExample") {
         renderer = hub.add(SceneryElement.Renderer,
             Renderer.createRenderer(hub, applicationName, scene, windowWidth, windowHeight))
 
-        val hull = Box(Vector3f(200.0f, 200.0f, 200.0f), insideNormals = true)
+        val hull = Box(Vector3f(250.0f, 250.0f, 250.0f), insideNormals = true)
         hull.material {
             diffuse = Vector3f(0.07f, 0.07f, 0.07f)
             cullingMode = Material.CullingMode.Front
         }
         scene.addChild(hull)
 
-        val light0 = PointLight(radius = 400.0f)
-        light0.spatial().position = Vector3f(0.0f, 5.0f, 3.0f)
-        light0.intensity = 5.0f
-        light0.emissionColor = Vector3f(1.0f, 1.0f, 1.0f)
-        scene.addChild(light0)
+        val light1 = PointLight(radius = 200.0f)
+        light1.spatial().position = Vector3f(0.0f, 0.0f, 0.0f)
+        light1.intensity = 5.0f
+        light1.emissionColor = Vector3f(1.0f, 1.0f, 1.0f)
+        scene.addChild(light1)
 
-        val cam: Camera = DetachedHeadCamera()
-        cam.spatial {
-            position = Vector3f(0.0f, 0.0f, 10.0f)
-        }
-        cam.perspectiveCamera(50.0f, windowWidth, windowHeight)
-        cam.target = Vector3f(150.0f, 0.0f, 150.0f)
-        scene.addChild(cam)
+        val light2 = PointLight(radius = 200.0f)
+        light2.spatial().position = Vector3f(100.0f, 100.0f, 100.0f)
+        light2.intensity = 5.0f
+        light2.emissionColor = Vector3f(1.0f, 1.0f, 1.0f)
+        scene.addChild(light2)
 
+        val light3 = PointLight(radius = 200.0f)
+        light3.spatial().position = Vector3f(0.0f, 0.0f, -100.0f)
+        light3.intensity = 5.0f
+        light3.emissionColor = Vector3f(1.0f, 1.0f, 1.0f)
+        scene.addChild(light3)
 
-        val particlePositions = mutableListOf<Vector3f>()
-        val particleProperties = mutableListOf<Vector3f>()
-        for(i in 0..1000000) {
-            particlePositions.add(graphics.scenery.numerics.Random.random3DVectorFromRange(-30.0f, 30.0f))
-            particleProperties.add(Vector3f(Random.nextDouble(0.0, 0.5).toFloat(), Random.nextDouble(0.0, 1.0).toFloat(), 0.0f))
+        val particleNumber = 500000
+        val particlePositions = ArrayList<Vector3f>(particleNumber)
+        val particleProperties = ArrayList<Vector3f>(particleNumber)
+        repeat(particleNumber) {
+            particlePositions.add(graphics.scenery.numerics.Random.random3DVectorFromRange(-50.0f, 50.0f))
+            particleProperties.add(Vector3f(Random.nextDouble(1.0, 5.0).toFloat(), Random.nextDouble(0.0, 1.0).toFloat(), 0.0f))
         }
         val particleGlyphs = ParticleGlyphs(particlePositions, particleProperties, false)
         particleGlyphs.name = "Particles?"
         scene.addChild(particleGlyphs)
+
+        val camStartDist = 10.0f
+        val camStart = Vector3f(0.0f, 0.0f, camStartDist)
+        val cam: Camera = DetachedHeadCamera()
+        cam.spatial {
+            position = camStart
+        }
+        cam.perspectiveCamera(60.0f, windowWidth, windowHeight)
+        cam.target = Vector3f(0.0f, 0.0f, 0.0f)
+        scene.addChild(cam)
+
+
     }
 
     companion object {
