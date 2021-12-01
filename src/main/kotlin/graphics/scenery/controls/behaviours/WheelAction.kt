542753dc87eee877f3145af2c6998edeea922cf8
Jan Tiemann <j.tiemann@hzdr.de>
2021-12-01 13:26:01 +0100

    WheelAction.kt: documentation

--- src/main/kotlin/graphics/scenery/controls/behaviours/WheelAction.kt
+++ src/main/kotlin/graphics/scenery/controls/behaviours/WheelAction.kt
@@ -1,6 +1,20 @@
 package graphics.scenery.controls.behaviours
 
-
+/**
+ * Data Class for [VRSelectionWheel] and [VRTreeSelectionWheel]
+ */
 sealed class WheelAction(val name: String)
-class Action(name: String, val action: () -> Unit): WheelAction(name)
-class SubWheel(name: String, val actions: List<WheelAction>): WheelAction(name)
+
+/**
+ * A wheel menu entry.
+ *
+ * @param name displayed name of the action
+ * @param action called when the user selects this entry
+ */
+class Action(name: String, val action: () -> Unit) : WheelAction(name)
+
+/**
+ * A sub menu of a wheel menu. It will be displayed like a regular menu entry and when selected
+ * it opens on-top of the previous menu. Deeper sub menus are supported.
+ */
+class SubWheel(name: String, val actions: List<WheelAction>) : WheelAction(name)
