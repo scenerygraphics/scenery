97b5683b4effa0555038f8a021645c3029ab2afc
Justin <JustinBuerger@googlemail.com>
2021-12-16 00:04:01 +0100

    AminoBondTree: skeleton for storage class of all aminoacid bonds.

---
+++ src/main/kotlin/graphics/scenery/proteins/chemistry/AminoAcidBondTree.kt
@@ -0,0 +1,37 @@
+package graphics.scenery.proteins.chemistry
+
+import graphics.scenery.proteins.AminoAcid
+import graphics.scenery.proteins.AminoList
+import graphics.scenery.utils.LazyLogger
+
+/**
+ * BondTree specific for amino acids as stored in the AminoList
+ */
+class AminoAcidBondTree(val aminoAcid: AminoAcid, val aminoParent:AminoAcidBondTree? = null,
+                        val aminoChildren: List<AminoAcidBondTree> = ArrayList()): BondTree(aminoAcid, aminoParent, aminoChildren){
+
+        //The bond tree is calculated here.
+        companion object AminoTreeCalculator {
+            private val logger by LazyLogger()
+            /**
+             * Convenience method to return the amino acid bond tree from an iupac abbreviation (three letters)
+             */
+            fun fromAbbreviation(abbreviation: String): AminoAcidBondTree {
+                val aminoList = AminoList()
+                aminoList.forEach {
+                    if (abbreviation == it.name) {
+                        return fromAminoAcid(it)
+                    }
+                }
+                logger.warn("Your amino acid abbreviation code was invalid, fallback to alanine")
+                return(fromAminoAcid(aminoList[1]))
+            }
+            /**
+             * Convenience method to return the amino acid bond tree from an AminoAcid.
+             */
+            fun fromAminoAcid(aminoAcid: AminoAcid): AminoAcidBondTree {
+
+            }
+        }
+
+}
