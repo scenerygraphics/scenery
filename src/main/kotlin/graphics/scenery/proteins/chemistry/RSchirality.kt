51bbb43ffd4f73c7a38ddc97a7376aa8ad4bca55
Justin <JustinBuerger@googlemail.com>
2021-12-15 22:34:35 +0100

    RSChirality: probably the simplest form of chirality, r and s.

---
+++ src/main/kotlin/graphics/scenery/proteins/chemistry/RSchirality.kt
@@ -0,0 +1,7 @@
+package graphics.scenery.proteins.chemistry
+
+class RSchirality(val bondTree: BondTree, val rs: String): Chirality {
+    override fun orderAtomPositions(bondtree: BondTree): BondTree {
+        TODO("Not yet implemented")
+    }
+}
