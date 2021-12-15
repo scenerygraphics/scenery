8a453e17d30b645129745bab3cf387a18df260a9
Justin <JustinBuerger@googlemail.com>
2021-12-16 00:32:30 +0100

    BondTree: implemented the algorithm for cycle detection and added docu.

--- src/main/kotlin/graphics/scenery/proteins/chemistry/BondTree.kt
+++ src/main/kotlin/graphics/scenery/proteins/chemistry/BondTree.kt
@@ -1,7 +1,26 @@
 package graphics.scenery.proteins.chemistry
 
-open class BondTree(val molecule: Molecule, val children: ArrayList<BondTree>, val parent: BondTree?) {
-    fun isPartOfACycle(): Boolean {
-        //TODO: implement
+/**
+ * Open class to store the configuration of a molecule.
+ * [molecule] molecule of which a BondTree is to be configured
+ * [parent] perant of the node
+ * [children] children of the node
+ * [bondOrder] order of the bond with which the node is connected to its parent, 0 if parent is null
+ */
+open class BondTree(val molecule: Molecule, val parent: BondTree?, val children: List<BondTree>, val bondOrder: Int = 0) {
+    var isPartOfACycle = false
+    init {
+        checkIfPartOfACycle(this)
+    }
+    /**
+     * It is necessary to know whether an atom is part of a cycle or not to calculate the positions correctly
+     */
+    fun checkIfPartOfACycle(root: BondTree) {
+        root.children.forEach {
+            if(it.children.contains(this)) {
+                isPartOfACycle = true
+            }
+            checkIfPartOfACycle(it)
+        }
     }
 }
