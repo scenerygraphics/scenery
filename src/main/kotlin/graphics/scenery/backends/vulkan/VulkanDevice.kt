aca3407d2bd710240af8026c7ed6c5b8e13b4143
Ulrik Günther <hello@ulrik.is>
2021-12-17 18:36:33 +0100

    VulkanDevice: Fix incorrect DSL caching

--- src/main/kotlin/graphics/scenery/backends/vulkan/VulkanDevice.kt
+++ src/main/kotlin/graphics/scenery/backends/vulkan/VulkanDevice.kt
@@ -534,10 +534,10 @@ open class VulkanDevice(
      * The shader stages to which the DSL should be visible can be set via [shaderStages].
      */
     fun createDescriptorSetLayout(types: List<Pair<Int, Int>>, binding: Int = 0, shaderStages: Int): Long {
-        val current = descriptorSetLayouts[DescriptorSetLayout(types, binding, shaderStages)]
-        if( current != null) {
-            return current
-        }
+//        val current = descriptorSetLayouts[DescriptorSetLayout(types, binding, shaderStages)]
+//        if( current != null) {
+//            return current
+//        }
 
         return stackPush().use { stack ->
             val layoutBinding = VkDescriptorSetLayoutBinding.calloc(types.size, stack)
