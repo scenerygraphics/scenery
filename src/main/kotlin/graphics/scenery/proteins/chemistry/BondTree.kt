7d025084b5720eef9b7980258eb72b6b7352569a
Justin <JustinBuerger@googlemail.com>
2021-12-18 01:49:09 +0100

    BondTree: minor changes

--- src/main/kotlin/graphics/scenery/proteins/chemistry/BondTree.kt
+++ src/main/kotlin/graphics/scenery/proteins/chemistry/BondTree.kt
@@ -2,13 +2,14 @@ package graphics.scenery.proteins.chemistry
 
 /**
  * Open class to store the configuration of a molecule.
- * [molecule] molecule of which a BondTree is to be configured
  * [parent] perant of the node
- * [children] children of the node
+ * [boundMolecules] children of the node
  * [bondOrder] order of the bond with which the node is connected to its parent, 0 if parent is null
  */
-open class BondTree(val molecule: Molecule, val parent: BondTree?, val children: List<BondTree>, val bondOrder: Int = 0) {
+open class BondTree(val element: String, val bondOrder: Int = 0, var parent: BondTree?) {
+    var boundMolecules = mutableListOf<BondTree>()
     var isPartOfACycle = false
+    val periodicTable = PeriodicTable()
     init {
         checkIfPartOfACycle(this)
     }
@@ -16,11 +17,36 @@ open class BondTree(val molecule: Molecule, val parent: BondTree?, val children:
      * It is necessary to know whether an atom is part of a cycle or not to calculate the positions correctly
      */
     fun checkIfPartOfACycle(root: BondTree) {
-        root.children.forEach {
-            if(it.children.contains(this)) {
+        root.boundMolecules.forEach {
+            if(it.boundMolecules.contains(this)) {
                 isPartOfACycle = true
             }
             checkIfPartOfACycle(it)
+        }
+    }
+
+    /**
+     * Get the data for the respective chemical element
+     */
+    fun getDataForElement(): ChemicalElement {
+        return periodicTable.findElementBySymbol(element)
+    }
+
+    /**
+     * add a child
+     */
+    fun addBoundMolecule(bondTree: BondTree) {
+        this.boundMolecules.add(bondTree)
+    }
+
+    /**
+     * add hydrogen
+     */
+    fun addhydrogen(numberOfHydrogens: Int) {
+        for (i in 0 until numberOfHydrogens) {
+            val hydrogen = BondTree("H", 1, this)
+            this.boundMolecules.add(hydrogen)
+
         }
     }
 }
