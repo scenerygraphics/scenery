d0c1249724a4022f8d2ccb51f59bafca6b6c4829
Justin <JustinBuerger@googlemail.com>
2022-01-05 21:14:03 +0100

    CircularMolecularStructure: on the way to compute the positions of the circle elements

--- src/main/kotlin/graphics/scenery/proteins/chemistry/CircularMolecularStructure.kt
+++ src/main/kotlin/graphics/scenery/proteins/chemistry/CircularMolecularStructure.kt
@@ -1,14 +1,61 @@
 package graphics.scenery.proteins.chemistry
 
+import graphics.scenery.Icosphere
 import graphics.scenery.Mesh
+import org.joml.Matrix3f
+import org.joml.Vector3f
 
 /**
  * Class to compute the 3D structure of a circular molecule. We are using the simplest model, i.e., the ring is kept
  * flat.
  */
-class CircularMolecularStructure(val root: BondTreeCycle, val initialAngle: Float): Mesh("CircularMolecularStructure") {
+class CircularMolecularStructure(val root: BondTreeCycle, val initialAngle: Float, private val basis: Matrix3f, val bondLength: Float): Mesh("CircularMolecularStructure") {
+
+    init {
 
 
+        val allCycles = root.cyclesAndChildren.filter{it.size > 1}
+        if(allCycles.isEmpty()) {
+            logger.warn("Cicle without children")
+        }
+        //more than one circle attached to the root
+        else if(allCycles.size > 1) {
+            //TODO
+        }
+        else {
+            val cycle = allCycles[0]
+            //angle around which to turn each new z axis
+            val theta = 2*kotlin.math.PI/cycle.size
+            val cosTheta = kotlin.math.cos(theta)
+            val sinTheta = kotlin.math.sin(theta)
+            var currentRoot = root
+            var currentBasis = basis
+            var i = 0
+            while(cycle.drop(i).size > 1) {
+                val currentConstituent = cycle[i]
+                val element = PeriodicTable().findElementBySymbol(currentConstituent.element)
+                val elementSphere = if(element == PeriodicTable().findElementByNumber(1)) {
+                    Icosphere(0.05f, 2) }
+                else { Icosphere(0.15f, 2) }
+                if (element.color != null) { elementSphere.ifMaterial { diffuse = element.color } }
+                val x = Vector3f()
+                currentBasis.getColumn(0, x)
+                val y = Vector3f()
+                currentBasis.getColumn(1, y)
+                val z = Vector3f()
+                currentBasis.getColumn(2, z)
+
+                i += 1
+            }
+        }
+    }
+
+    /**
+     * checks if there are more than one circles attached to the root.
+     */
+    fun moreThanOneCircle(): Boolean {
+        return root.cyclesAndChildren.filter{it.size > 1}.size > 1
+    }
     /**
      * A root can be part of two circles. This leaves us (at least*) two possibilities: either the root is a singular root
      * or there is another node which could also function as a root.
