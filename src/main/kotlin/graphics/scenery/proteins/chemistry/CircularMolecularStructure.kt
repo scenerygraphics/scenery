64a3e24c386636141f85753191c1a12e4d817cb9
Justin <JustinBuerger@googlemail.com>
2022-01-06 22:54:21 +0100

    CircularMolecularStructure: first working prototype for arbritrary initial angle and coordinate system

--- src/main/kotlin/graphics/scenery/proteins/chemistry/CircularMolecularStructure.kt
+++ src/main/kotlin/graphics/scenery/proteins/chemistry/CircularMolecularStructure.kt
@@ -10,11 +10,26 @@ import org.joml.Vector3f
  * Class to compute the 3D structure of a circular molecule. We are using the simplest model, i.e., the ring is kept
  * flat.
  */
-class CircularMolecularStructure(val root: BondTreeCycle, val initialAngle: Float, private val basis: Matrix3f, val bondLength: Float,
+class CircularMolecularStructure(val root: BondTreeCycle, initialAngle: Float, basis: Matrix3f, bondLength: Float,
                                  positionalVector: Vector3f): Mesh("CircularMolecularStructure") {
 
     init {
         this.spatial().position = positionalVector
+
+        //change the basis according to initial angle
+        val initialX = Vector3f()
+        basis.getColumn(0, initialX)
+        val initialY = Vector3f()
+        basis.getColumn(1, initialY)
+        val initialZ = Vector3f()
+        basis.getColumn(2, initialZ)
+
+        //turn the axes according to inital angle
+        val sinInitial = kotlin.math.sin(initialAngle)
+        val cosInitial = kotlin.math.cos(initialAngle)
+        val x = Vector3f(initialX).mul(cosInitial).add(Vector3f(initialZ).mul(sinInitial)).normalize()
+        val intermediateZ = Vector3f(Vector3f(x).cross(Vector3f(initialY))).normalize()
+
 
         val element = PeriodicTable().findElementBySymbol(root.element)
         val elementSphere = if(element == PeriodicTable().findElementByNumber(1)) {
@@ -33,26 +48,23 @@ class CircularMolecularStructure(val root: BondTreeCycle, val initialAngle: Floa
             //TODO
         }
         else {
-            val x = Vector3f()
-            var currentBasis = basis
-            currentBasis.getColumn(0, x)
-            val y = Vector3f()
-            currentBasis.getColumn(1, y)
-            val z = Vector3f()
-            currentBasis.getColumn(2, z)
             val cycle = allCycles[0]
             //angle around which to turn each new z axis
             val theta = 2*kotlin.math.PI/(cycle.size+1)
             val cosTheta = kotlin.math.cos(theta).toFloat()
             val sinTheta = kotlin.math.sin(theta).toFloat()
 
+            //intial z and y vector need to be turned such that the circle is symmetric to the original z axis
+            val alpha = (kotlin.math.PI-theta)/2f
+            val cosAlpha = kotlin.math.cos(alpha).toFloat()
+            val sinAlpha = kotlin.math.sin(alpha).toFloat()
+            val z = Vector3f(intermediateZ).mul(cosAlpha).add(Vector3f(initialY).mul(sinAlpha)).normalize()
+            val y = Vector3f(x).cross(Vector3f(z)).normalize()
+
             var currentPosition = positionalVector
             var i = 0
             while(cycle.drop(i).isNotEmpty()) {
-                val newZ = Vector3f(z).mul(cosTheta).add(Vector3f(y).mul(sinTheta)).normalize()
-                z.set(newZ)
-                val newY = Vector3f(x).cross(z).normalize()
-                y.set(newY)
+
                 val constituentPosition = Vector3f(currentPosition).add(Vector3f(z).mul(bondLength))
 
                 //add the sphere
@@ -70,6 +82,9 @@ class CircularMolecularStructure(val root: BondTreeCycle, val initialAngle: Floa
                 c.spatial().orientBetweenPoints(currentPosition, elementSphere.spatial().position, true, true)
                 this.addChild(c)
                 currentPosition = constituentPosition
+                val newZ = (Vector3f(z).mul(cosTheta).add(Vector3f(y).mul(-sinTheta))).normalize()
+                z.set(newZ)
+                y.set(Vector3f(x).cross(Vector3f(z))).normalize()
                 i += 1
             }
             //last cylinder
