4ffceec632cfdba998e6d0ef62d517088e97e5dc
Justin <JustinBuerger@googlemail.com>
2022-01-14 11:28:50 +0100

    CircularMolecularStructure: removed useless function to determine the geometry (structures I accounted for fall apart in a matter of milliseconds, there is not point in including them)

--- src/main/kotlin/graphics/scenery/proteins/chemistry/CircularMolecularStructure.kt
+++ src/main/kotlin/graphics/scenery/proteins/chemistry/CircularMolecularStructure.kt
@@ -178,34 +178,4 @@ class CircularMolecularStructure(val root: BondTreeCycle, initialAngle: Float, b
 
 
     }
-
-    /**
-     * checks if there are more than one circles attached to the root.
-     */
-    fun moreThanOneCircle(): Boolean {
-        return root.cyclesAndChildren.filter{it.size > 1}.size > 1
-    }
-    /**
-     * A root can be part of two circles. This leaves us (at least*) two possibilities: either the root is a singular root
-     * or there is another node which could also function as a root.
-     * one root:
-     *      \ /
-     *      / \
-     * two roots:
-     *      \ /
-     *       |
-     *      / \
-     *
-     * *we don't consider other possibilities than those described above as they are rare and not necessary at this stage
-     */
-    fun twoRoots(): Boolean {
-        //are there even two circles to consider?
-        return if(root.cyclesAndChildren.filter { it.size > 1 }.size > 1) {
-            //yes, well, then check if they have a common root other than the original one
-            val allChildren = root.cyclesAndChildren.filter { it.size > 1 }.flatMap{it}
-            allChildren.distinct().size != allChildren.size
-        } else {
-            false
-        }
-    }
 }
