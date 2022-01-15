6e8da2d6448f78079a243b8cbd2c043781b7e8ea
Justin <JustinBuerger@googlemail.com>
2022-01-15 16:23:48 +0100

    BondTreeCycle: added counter for non cyclic children

--- src/main/kotlin/graphics/scenery/proteins/chemistry/BondTreeCycle.kt
+++ src/main/kotlin/graphics/scenery/proteins/chemistry/BondTreeCycle.kt
@@ -3,4 +3,10 @@ package graphics.scenery.proteins.chemistry
 import java.lang.module.Configuration
 
 class BondTreeCycle(element: String, val cyclesAndChildren: List<List<BondTree>>, bondOrder: Int,  parent: BondTree?):
-    BondTree(element = element, bondOrder, parent)
+    BondTree(element = element, bondOrder, parent) {
+        init {
+            cyclesAndChildren.filter { it.size == 1 }.forEach {
+                this.boundMolecules.add(it[0])
+            }
+        }
+    }
