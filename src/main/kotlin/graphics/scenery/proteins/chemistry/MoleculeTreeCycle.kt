17719e91104c519f60dc24fbd6c67a68f54541c5
Justin <JustinBuerger@googlemail.com>
2022-01-19 16:15:30 +0100

    MoleculeTreeCycle: removed unnecessary clutter

--- src/main/kotlin/graphics/scenery/proteins/chemistry/MoleculeTreeCycle.kt
+++ src/main/kotlin/graphics/scenery/proteins/chemistry/MoleculeTreeCycle.kt
@@ -1,10 +1,25 @@
 package graphics.scenery.proteins.chemistry
 
-class MoleculeTreeCycle(element: String, val cyclesAndChildren: List<List<MoleculeTree>>, bondOrder: Int, id: String = ""):
+import java.util.concurrent.CopyOnWriteArrayList
+
+class MoleculeTreeCycle(element: String, var cyclesAndChildren: List<List<MoleculeTree>>, bondOrder: Int, id: String = ""):
     MoleculeTree(element = element, bondOrder, id) {
         init {
             cyclesAndChildren.filter { it.size == 1 }.forEach {
                 this.boundMolecules.add(it[0])
             }
+        }
+
+        fun renameAminoIdCycle(number: Int) {
+            val mutable = mutableListOf<MutableList<MoleculeTree>>()
+            this.cyclesAndChildren.forEach {
+                mutable.add(it as MutableList)
+            }
+            mutable.forEach { child ->
+                child.forEach {
+                    it.renameAminoAcidIds(number)
+                }
+            }
+            this.cyclesAndChildren = mutable
         }
     }
