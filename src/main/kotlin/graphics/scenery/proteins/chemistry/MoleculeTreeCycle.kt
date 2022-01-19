57bb18919caa83db56c4c3043d10a566c7273e79
Justin <JustinBuerger@googlemail.com>
2022-01-19 01:43:57 +0100

    BondTreeCycle: renamed to MoleculeTreeCycle

--- src/main/kotlin/graphics/scenery/proteins/chemistry/MoleculeTreeCycle.kt
+++ src/main/kotlin/graphics/scenery/proteins/chemistry/MoleculeTreeCycle.kt
@@ -1,7 +1,7 @@
 package graphics.scenery.proteins.chemistry
 
-class BondTreeCycle(element: String, val cyclesAndChildren: List<List<BondTree>>, bondOrder: Int, id: String = ""):
-    BondTree(element = element, bondOrder, id) {
+class MoleculeTreeCycle(element: String, val cyclesAndChildren: List<List<MoleculeTree>>, bondOrder: Int, id: String = ""):
+    MoleculeTree(element = element, bondOrder, id) {
         init {
             cyclesAndChildren.filter { it.size == 1 }.forEach {
                 this.boundMolecules.add(it[0])
