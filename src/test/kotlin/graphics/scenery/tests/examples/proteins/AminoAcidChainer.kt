d8e031e43f0975b3ed77700f71edc3c1f1a268ff
Justin <JustinBuerger@googlemail.com>
2022-01-19 23:41:13 +0100

    AminoAcidChainer: working prototype

--- src/test/kotlin/graphics/scenery/tests/examples/proteins/AminoAcidChainer.kt
+++ src/test/kotlin/graphics/scenery/tests/examples/proteins/AminoAcidChainer.kt
@@ -1,15 +1,17 @@
 package graphics.scenery.tests.examples.proteins
 
-
 import org.joml.*
 import graphics.scenery.*
 import graphics.scenery.backends.Renderer
 import graphics.scenery.numerics.Random
 import graphics.scenery.attribute.material.Material
 import graphics.scenery.controls.behaviours.SelectCommand
-import graphics.scenery.proteins.chemistry.AminoTreeList
-import graphics.scenery.proteins.chemistry.MoleculeTreeCycle
-import graphics.scenery.proteins.chemistry.MoleculeMesh
+import graphics.scenery.flythroughs.IUPACAbbreviationsReader
+import graphics.scenery.proteins.Protein
+import graphics.scenery.proteins.chemistry.*
+import graphics.scenery.utils.extensions.minus
+import graphics.scenery.utils.extensions.plus
+import kotlin.concurrent.thread
 
 /**
  *
@@ -18,12 +20,25 @@ import graphics.scenery.proteins.chemistry.MoleculeMesh
  */
 class AminoAcidChainer: SceneryBase("RainbowRibbon", windowWidth = 1280, windowHeight = 720) {
 
-    private val aminoAcidAbbreviations = listOf("ALA", "PRO", "ALA", "TYR", "ASP", "ARG", "THR")
+    private val protein = Protein.fromID("3nir")
+    private val structure = protein.structure
+    private val chains = structure.chains
+    val groups = chains.flatMap { it.atomGroups }.filter { it.isAminoAcid }
+    private val aminoAcidAbbreviations = ArrayList<String>(groups.size)
+    val abbreviations = IUPACAbbreviationsReader().abbrevations
     private var aminoacidNumbersStored = 0
-    private var rootAA = AminoTreeList().aminoMap[aminoAcidAbbreviations.first()]
+    lateinit var rootAA: MoleculeTree
     private var combineTwoAcids = false
     private var nextAaUP = false
     override fun init() {
+        groups.forEach { residue ->
+            abbreviations.forEach {
+                if(it.value.threeLetters == residue.pdbName) {
+                    aminoAcidAbbreviations.add(it.value.threeLetters)
+                }
+            }
+        }
+        rootAA = AminoTreeList().aminoMap[aminoAcidAbbreviations.first()]!!
         rootAA!!.renameAminoAcidIds(aminoacidNumbersStored)
         val root = MoleculeMesh(rootAA!!)
         root.name = "poly$aminoacidNumbersStored"
@@ -87,7 +102,6 @@ class AminoAcidChainer: SceneryBase("RainbowRibbon", windowWidth = 1280, windowH
         val action: (Scene.RaycastResult, Int, Int) -> Unit = { _, _, _ ->
             if(aminoacidNumbersStored < aminoAcidAbbreviations.size) {
                 if(!combineTwoAcids) {
-
                     val previous = scene.find("poly${aminoacidNumbersStored - 1}")
                     val cPrev = previous!!.findChildrenByNameRecursive("C" + "${aminoacidNumbersStored - 1}")?.spatialOrNull()!!.world.getColumn(3, Vector3f())
                     val ohPrev = previous.findChildrenByNameRecursive("OH" + "${aminoacidNumbersStored - 1}")?.spatialOrNull()!!.world.getColumn(3, Vector3f())
@@ -102,36 +116,122 @@ class AminoAcidChainer: SceneryBase("RainbowRibbon", windowWidth = 1280, windowH
                     combineTwoAcids = !combineTwoAcids
                 }
                 else {
-                    val nextAminoBondTree =
-                        AminoTreeList().aminoMap[aminoAcidAbbreviations[aminoacidNumbersStored]]
-                    nextAminoBondTree!!.renameAminoAcidIds(aminoacidNumbersStored)
-                    rootAA!!.removeByID("OH" + "${aminoacidNumbersStored-1}")
-                    nextAaUP = if(nextAaUP) {
-                        //would break in case its Proline
-                        if(nextAminoBondTree is MoleculeTreeCycle) {
-                            nextAminoBondTree.removeByID("HN$aminoacidNumbersStored")
-                        }
-                        else {
-                            nextAminoBondTree.removeByID("HNB$aminoacidNumbersStored")
-                        }
-                        !nextAaUP
-                    } else {
-                        nextAminoBondTree.removeByID("HN$aminoacidNumbersStored")
-                        !nextAaUP
-                    }
+                    thread {
+                        val nextAA = scene.find("aa$aminoacidNumbersStored")
+                        val prevPolyPep = scene.find("poly${aminoacidNumbersStored - 1}")
+                        nextAA?.findChildrenByNameRecursive("HN${aminoacidNumbersStored}Cyl")?.visible = false
+                        prevPolyPep?.findChildrenByNameRecursive("OH${aminoacidNumbersStored - 1}Cyl")?.visible = false
+                        prevPolyPep?.findChildrenByNameRecursive("HO${aminoacidNumbersStored - 1}Cyl")?.visible = false
+                        //add new nodes for the water, using the "bounded" result in false positions
+                        val hNode = nextAA?.findChildrenByNameRecursive("HN$aminoacidNumbersStored")
+                        val oNode = prevPolyPep?.findChildrenByNameRecursive("OH${aminoacidNumbersStored - 1}")
+                        val h2Node = prevPolyPep?.findChildrenByNameRecursive("HO${aminoacidNumbersStored - 1}")
 
-                    rootAA!!.addAtID("C${aminoacidNumbersStored-1}", nextAminoBondTree)
-                    val newRoot = rootAA!!
-                    val polypeptide = MoleculeMesh(newRoot)
-                    polypeptide.name = "poly${aminoacidNumbersStored}"
-                    if(aminoacidNumbersStored == 1) {
-                        scene.removeChild("aa" + "${aminoacidNumbersStored-1}")
+                        //first oxygen
+                        val oPos = Vector3f(oNode?.spatialOrNull()!!.world.getColumn(3, Vector3f()))
+                        val o = Icosphere(0.15f, 2)
+                        o.material { diffuse = PeriodicTable().findElementByNumber(8).color!! }
+                        o.spatial().position = oPos
+                        o.name = "O"
+                        scene.addChild(o)
+                        oNode.visible = false
+                        //then hydrogen number 1
+                        val hPos = Vector3f(hNode?.spatialOrNull()!!.world.getColumn(3, Vector3f()))
+                        val h = Icosphere(0.05f, 2)
+                        h.material { diffuse = PeriodicTable().findElementByNumber(1).color!! }
+                        h.spatial().position = hPos
+                        h.name = "HN"
+                        scene.addChild(h)
+                        hNode.visible = false
+                        // and hydrogen number 2
+                        val h2Pos = Vector3f(h2Node?.spatialOrNull()!!.world.getColumn(3, Vector3f()))
+                        val h2 = Icosphere(0.05f, 2)
+                        h2.material { diffuse = PeriodicTable().findElementByNumber(1).color!! }
+                        h2.spatial().position = h2Pos
+                        h2.name = "HO"
+                        scene.addChild(h2)
+                        h2Node.visible = false
+
+                        val waterDirection = Vector3f(hPos).sub(Vector3f(h2Pos)).normalize()
+
+                        val randomVector = Vector3f(waterDirection).mul(1 / 1610f)
+                        //let them wiggle around to show that they are going to be emitted
+                        for (i in 0 until 1000) {
+                            //Random.random3DVectorFromRange(-0.005f, 0.005f)
+                            h.spatial().position = h.spatial().worldPosition() - randomVector
+                            o.spatial().position = o.spatial().worldPosition() + randomVector
+                            h2.spatial().position = h2.spatial().worldPosition() + randomVector
+                            //Thread.sleep(2)
+                        }
+
+
+                        //water molecule is emitted
+                        val waterPosition =
+                            Vector3f(o.spatialOrNull()!!.position).add(Vector3f(waterDirection).mul(0.2f))
+                        o.spatialOrNull()!!.position = waterPosition
+                        val down = Vector3f(scene.findObserver()!!.up).mul(-1f).normalize()
+                        val side = Vector3f(down).cross(scene.findObserver()!!.forward).normalize()
+                        val sin60 = kotlin.math.sin(kotlin.math.PI.toFloat() / 3f)
+                        val cos60 = kotlin.math.cos(kotlin.math.PI.toFloat() / 3f)
+                        val newHPos = Vector3f(waterPosition).add(
+                            Vector3f(down).mul(cos60).add(Vector3f(side).mul(sin60)).normalize().mul(0.2f)
+                        )
+                        val newH2Pos = Vector3f(waterPosition).add(
+                            Vector3f(down).mul(cos60).add(Vector3f(side).mul(-sin60)).normalize().mul(0.2f)
+                        )
+                        h.spatialOrNull()!!.position = newHPos
+                        h2.spatialOrNull()!!.position = newH2Pos
+                        Thread.sleep(300)
+                        for (i in 0 until 1000) {
+                            val randomVector = Random.random3DVectorFromRange(-0.005f, 0.005f)
+                            h.spatialOrNull()!!.position = newHPos + randomVector
+                            o.spatialOrNull()!!.position = waterPosition + randomVector
+                            h2.spatialOrNull()!!.position = newH2Pos + randomVector
+                           //Thread.sleep(2)
+                        }
+                        scene.removeChild(h)
+                        scene.removeChild(o)
+                        scene.removeChild(h2)
+                        val n = nextAA.findChildrenByNameRecursive("N$aminoacidNumbersStored")
+                        val nPos = Vector3f(n?.spatialOrNull()!!.position)
+                        val c = prevPolyPep.findChildrenByNameRecursive("C${aminoacidNumbersStored - 1}")
+                        val cPos = Vector3f(c?.spatialOrNull()!!.position)
+                        for (i in 0 until 1000) {
+                            val randomVector = Random.random3DVectorFromRange(-0.005f, 0.005f)
+                            n.spatialOrNull()!!.position = nPos + randomVector
+                            c.spatialOrNull()!!.position = cPos + randomVector
+                            //Thread.sleep(2)
+                        }
+                        val nextAminoBondTree =
+                            AminoTreeList().aminoMap[aminoAcidAbbreviations[aminoacidNumbersStored]]
+                        nextAminoBondTree!!.renameAminoAcidIds(aminoacidNumbersStored)
+                        rootAA!!.removeByID("OH" + "${aminoacidNumbersStored - 1}")
+                        nextAaUP = if (nextAaUP) {
+                            //would break in case its Proline
+                            if (nextAminoBondTree is MoleculeTreeCycle) {
+                                nextAminoBondTree.removeByID("HN$aminoacidNumbersStored")
+                            } else {
+                                nextAminoBondTree.removeByID("HNB$aminoacidNumbersStored")
+                            }
+                            !nextAaUP
+                        } else {
+                            nextAminoBondTree.removeByID("HN$aminoacidNumbersStored")
+                            !nextAaUP
+                        }
+
+                        rootAA!!.addAtID("C${aminoacidNumbersStored - 1}", nextAminoBondTree)
+                        val newRoot = rootAA!!
+                        val polypeptide = MoleculeMesh(newRoot)
+                        polypeptide.name = "poly${aminoacidNumbersStored}"
+                        if (aminoacidNumbersStored == 1) {
+                            scene.removeChild("aa" + "${aminoacidNumbersStored - 1}")
+                        }
+                        scene.removeChild("aa$aminoacidNumbersStored")
+                        scene.removeChild("poly${aminoacidNumbersStored - 1}")
+                        scene.addChild(polypeptide)
+                        aminoacidNumbersStored += 1
+                        combineTwoAcids = !combineTwoAcids
                     }
-                    scene.removeChild("aa$aminoacidNumbersStored")
-                    scene.removeChild("poly${aminoacidNumbersStored - 1}")
-                    scene.addChild(polypeptide)
-                    aminoacidNumbersStored += 1
-                    combineTwoAcids = !combineTwoAcids
                 }
             }
             /*
