409c77e8966afe544fcaf8b7bfea64dce3f8876b
Jan T <moreApi@users.noreply.github.com>
2022-06-01 16:29:52 +0200

    Network*: Adding Kdocs

--- src/main/kotlin/graphics/scenery/net/NetworkEvent.kt
+++ src/main/kotlin/graphics/scenery/net/NetworkEvent.kt
@@ -1,6 +1,14 @@
 package graphics.scenery.net
 
+/**
+ * Events Client and Server send each other to facilitate syncing.
+ *
+ * @author Jan Tiemann <j.tiemann@hzdr.de>
+ */
 sealed class NetworkEvent {
+    /**
+     * An object has benn created or properties have been changed.
+     */
     data class Update(val obj: NetworkWrapper<*>,
                  val additionalData: Any? = obj.obj.getAdditionalUpdateData(),
                  val constructorParameters: Any? = obj.obj.getConstructorParameters()) : NetworkEvent() {
@@ -8,6 +16,13 @@ sealed class NetworkEvent {
                          this(obj,obj.obj.getAdditionalUpdateData(),obj.obj.getConstructorParameters())
     }
 
+    /**
+     * A scene graph parent-child relation has been changed.
+     */
     data class NewRelation(val parent: Int?, val child: Int) : NetworkEvent()
+
+    /**
+     * A client requests a full send of the scene.
+     */
     object RequestInitialization : NetworkEvent()
 }
