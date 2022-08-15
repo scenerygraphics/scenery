4980fba9c0d7ab693aa80059210f9bc63eff4c1a
Aryaman Gupta <argupta@mpi-cbg.de>
2022-08-15 16:38:49 +0200

    VDIData,VDIDataIO: adds classes for serialization of VDI metadata

---
+++ src/main/kotlin/graphics/scenery/volumes/vdi/VDIData.kt
@@ -0,0 +1,23 @@
+package graphics.scenery.volumes.vdi
+
+import org.joml.Matrix4f
+import org.joml.Vector2i
+import org.joml.Vector3f
+import java.nio.ByteBuffer
+
+data class VDIMetadata(
+    val version: Int = 1,
+    var projection: Matrix4f = Matrix4f(),
+    var view: Matrix4f = Matrix4f(),
+    val model: Matrix4f = Matrix4f(),
+    val volumeDimensions: Vector3f = Vector3f(),
+    val windowDimensions: Vector2i = Vector2i(),
+    val nw: Float = 0f
+)
+
+data class VDIData(
+//    val vdiDepth: ByteBuffer = ByteBuffer.allocate(1),
+//    val vdiColor: ByteBuffer = ByteBuffer.allocate(1),
+//    val gridCells: ByteBuffer = ByteBuffer.allocate(1),
+    val metadata: VDIMetadata = VDIMetadata()
+)
