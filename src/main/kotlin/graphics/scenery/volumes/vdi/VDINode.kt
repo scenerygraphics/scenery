5764898bb0bbdb69b2327382cec464b3f7cb13f0
Aryaman Gupta <argupta@mpi-cbg.de>
2022-10-19 11:04:08 +0200

    VDINode: creates node containing shader uniforms for rendering VDI

---
+++ src/main/kotlin/graphics/scenery/volumes/vdi/VDINode.kt
@@ -0,0 +1,42 @@
+package graphics.scenery.volumes.vdi
+
+import graphics.scenery.RichNode
+import graphics.scenery.ShaderProperty
+import org.joml.Matrix4f
+import org.joml.Vector3f
+
+class VDINode : RichNode() {
+
+    @ShaderProperty
+    var ProjectionOriginal = Matrix4f()
+
+    @ShaderProperty
+    var invProjectionOriginal = Matrix4f()
+
+    @ShaderProperty
+    var ViewOriginal = Matrix4f()
+
+    @ShaderProperty
+    var invViewOriginal = Matrix4f()
+
+    @ShaderProperty
+    var invModel = Matrix4f()
+
+    @ShaderProperty
+    var volumeDims = Vector3f()
+
+    @ShaderProperty
+    var nw = 0f
+
+    @ShaderProperty
+    var do_subsample = false
+
+    @ShaderProperty
+    var max_samples = 50
+
+    @ShaderProperty
+    var sampling_factor = 0.1f
+
+    @ShaderProperty
+    var stratified_downsampling = false
+}
