7d7bc3c744dd09b620c8fb17f7c670dff606afae
Aryaman Gupta <argupta@mpi-cbg.de>
2023-01-12 12:21:18 +0100

    VulkanTexture, VulkanNodeHelpers: changes log statements to debug

--- src/main/kotlin/graphics/scenery/backends/vulkan/VulkanNodeHelpers.kt
+++ src/main/kotlin/graphics/scenery/backends/vulkan/VulkanNodeHelpers.kt
@@ -303,7 +303,7 @@ object VulkanNodeHelpers {
                     }
 
                     texture.contents?.let { contents ->
-                        logger.info("Copying contents of size ${contents.remaining()/1024/1024}M")
+                        logger.debug("Copying contents of size ${contents.remaining()/1024/1024}M")
                         t.copyFrom(contents.duplicate())
                     }
 
