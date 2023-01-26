ebb276db4497d66c07d79f7845452cebf3245220
Ulrik Guenther <hello@ulrik.is>
2023-01-26 15:40:27 +0100

    Origin/Volume: Introduce support for custom origins and use in Volume

--- src/main/kotlin/graphics/scenery/Origin.kt
+++ src/main/kotlin/graphics/scenery/Origin.kt
@@ -1,11 +1,14 @@
 package graphics.scenery
 
+import org.joml.Vector3f
+
 /**
  * Enum class to store origin information, e.g. for use with [graphics.scenery.volumes.Volume]s
  *
  * @author Ulrik Guenther <hello@ulrik.is>
  */
-enum class Origin {
-    Center,
-    FrontBottomLeft
+sealed class Origin {
+    object Center: Origin()
+    object FrontBottomLeft: Origin()
+    class Custom(val origin: Vector3f): Origin()
 }
