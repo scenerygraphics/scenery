7b17cbf1ffe69f3b8d9ace6da8df454bdabdb4ef
PowerOfNames <PowerOfNames@web.de>
2023-02-01 16:49:35 +0100

    added SettingsEditorExample to ExampleRunnerBlacklist

--- src/test/kotlin/graphics/scenery/tests/examples/stresstests/ExampleRunner.kt
+++ src/test/kotlin/graphics/scenery/tests/examples/stresstests/ExampleRunner.kt
@@ -116,6 +116,8 @@ class ExampleRunner(
             // these examples don't work in headless mode
             "SwingTexturedCubeExample",
             "TexturedCubeJavaExample",
+
+            "SettingsEditorExample",
             // these examples need additional hardware
             "VRControllerExample",
             "VRSideChainsExample",
