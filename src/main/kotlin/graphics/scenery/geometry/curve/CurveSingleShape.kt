186f8b9efed9eb89375386dac2fe248c94d44875
Justin <JustinBuerger@googlemail.com>
2023-04-28 15:29:35 +0200

    CurveSingleShape: made a curve with a single baseShape to save memory

---
+++ src/main/kotlin/graphics/scenery/geometry/curve/CurveSingleShape.kt
@@ -0,0 +1,16 @@
+package graphics.scenery.geometry.curve
+
+import graphics.scenery.geometry.Spline
+import org.joml.*
+
+/**
+ * Curve implementing the baseShapes with only a single shape, allowing for more efficient storage
+ */
+
+class CurveSingleShape (spline: Spline, firstPerpendicularVector: Vector3f = Vector3f(0f, 0f, 0f),
+             partitionAlongControlpoints: Boolean = true, baseShape: List<Vector3f>):
+    AbstractCurve(spline, firstPerpendicularVector, partitionAlongControlpoints) {
+
+    //invokation of the baseShapes
+    override val baseShapes: List<List<Vector3f>> = listOf(baseShape)
+}
