ce3b58ff6d99d956497f2d5832851643d8d0ed79
wissal <wissalagueb@gmail.com>
2023-05-31 17:57:50 +0200

    vdi, VolumeCommons: adding files for the VDI Generation Example

---
+++ src/main/kotlin/graphics/scenery/volumes/vdi/VDIData.kt
@@ -0,0 +1,31 @@
+package graphics.scenery.volumes.vdi
+
+import org.joml.Matrix4f
+import org.joml.Vector2i
+import org.joml.Vector3f
+import java.nio.ByteBuffer
+
+data class VDIMetadata(
+    val version: Int = 1,
+    var index: Int = 0,
+    var projection: Matrix4f = Matrix4f(),
+    var view: Matrix4f = Matrix4f(),
+    val model: Matrix4f = Matrix4f(),
+    val volumeDimensions: Vector3f = Vector3f(),
+    val windowDimensions: Vector2i = Vector2i(),
+    val nw: Float = 0f
+)
+
+data class VDIBufferSizes(
+    var colorSize: Long = 0,
+    var depthSize: Long = 0,
+    var accelGridSize: Long = 0
+)
+
+data class VDIData(
+//    val vdiDepth: ByteBuffer = ByteBuffer.allocate(1),
+//    val vdiColor: ByteBuffer = ByteBuffer.allocate(1),
+//    val gridCells: ByteBuffer = ByteBuffer.allocate(1),
+    val bufferSizes: VDIBufferSizes = VDIBufferSizes(),
+    val metadata: VDIMetadata = VDIMetadata()
+)
