024758049042a9372f3bf36afe7412eb2aaffc69
wissal <wissalagueb@gmail.com>
2023-05-31 16:24:52 +0200

    DummyVolume: creating dummy a dummy volume class

---
+++ src/main/kotlin/graphics/scenery/volumes/DummyVolume.kt
@@ -0,0 +1,52 @@
+package graphics.scenery.volumes
+
+import bdv.tools.brightness.ConverterSetup
+import graphics.scenery.*
+import graphics.scenery.attribute.material.Material
+import graphics.scenery.net.Networkable
+import graphics.scenery.utils.extensions.minus
+import graphics.scenery.utils.extensions.times
+import org.joml.Vector3f
+import java.lang.IllegalArgumentException
+
+open class DummyVolume(val counterStart : Int = 0) : DefaultNode("DummyVolume"),HasTransferFunction {
+    override var transferFunction: TransferFunction = TransferFunction.flat(0.5f)
+        set(m) {
+            field = m
+            modifiedAt = System.nanoTime()
+        }
+    var counter = 0
+
+    val converterSetups = ArrayList<ConverterSetup>()
+    override var minDisplayRange: Float
+        get() = converterSetups.getOrNull(0)?.displayRangeMin?.toFloat() ?: throw IllegalStateException()
+        set(value) { setTransferFunctionRange(value, maxDisplayRange) }
+    override var maxDisplayRange: Float
+        get() = converterSetups.getOrNull(0)?.displayRangeMax?.toFloat() ?: throw IllegalStateException()
+        set(value) { setTransferFunctionRange(minDisplayRange, value) }
+
+    init {
+        name = "DummyVolume"
+        counter = counterStart
+    }
+
+    override fun update(fresh: Networkable, getNetworkable: (Int) -> Networkable, additionalData: Any?) {
+        if (fresh !is DummyVolume) throw IllegalArgumentException("Update called with object of foreign class")
+        super.update(fresh, getNetworkable, additionalData)
+        this.transferFunction = fresh.transferFunction
+        this.counter = fresh.counter
+    }
+
+    @JvmOverloads
+    open fun setTransferFunctionRange(min: Float, max: Float, forSetupId: Int = 0) {
+        converterSetups.getOrNull(forSetupId)?.setDisplayRange(min.toDouble(), max.toDouble())
+    }
+    override fun getConstructorParameters(): Any? {
+        return counterStart
+    }
+
+    override fun constructWithParameters(parameters: Any, hub: Hub): Networkable {
+        val counterStart = parameters as Int
+        return DummyVolume(counterStart)
+    }
+}
