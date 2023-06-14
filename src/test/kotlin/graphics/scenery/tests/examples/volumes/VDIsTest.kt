5a6eed1e9711ead2f0327614afca398563887eec
wissal <wissalagueb@gmail.com>
2023-06-14 15:12:01 +0200

    VDIsTest: created class to test VDI generation process

---
+++ src/test/kotlin/graphics/scenery/tests/examples/volumes/VDIsTest.kt
@@ -0,0 +1,44 @@
+package graphics.scenery.tests.examples.volumes
+
+import graphics.scenery.utils.lazyLogger
+import graphics.scenery.volumes.vdi.VDIDataIO
+import org.junit.Test
+import java.io.File
+import java.io.FileInputStream
+import kotlin.test.assertTrue
+
+class VDIsTest {
+
+     private val logger by lazyLogger()
+
+     /**
+      * Tests the generation of VDIs
+      */
+     @Test
+     fun testGeneration() {
+         //Step 1 : load the correct VDI files from desk
+         val path = "/home/argupta/Desktop/scenery/src/test/resources/graphics/scenery/tests/unit/volumes"
+         val testContent = File("${path}/Test_VDI_dump4").readBytes()
+         val testColor = File("${path}/Test_VDI_4_ndc_col").readBytes()
+         val testDepth = File("${path}/Test_VDI_4_ndc_depth").readBytes()
+         val testOct = File("${path}/Test_VDI_4_ndc_octree").readBytes()
+
+         //Step 2 : run VDI generation example and generate files
+         VDIGenerationExample.main(emptyArray())
+
+         //Step 3 : load just generated VDI files
+         val content = File("VDI_dump4").readBytes()
+         val color = File("VDI_4_ndc_col").readBytes()
+         val depth = File("VDI_4_ndc_depth").readBytes()
+         val oct = File("VDI_4_ndc_octree").readBytes()
+
+         //Step 4 : create assertions to compare the files
+         assertTrue(content.contentEquals(testContent))
+         assertTrue(color.contentEquals(testColor))
+         assertTrue(depth.contentEquals(testDepth))
+         assertTrue(oct.contentEquals(testOct))
+
+
+     }
+
+}
