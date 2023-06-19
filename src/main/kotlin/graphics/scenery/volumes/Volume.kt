b6bce50fba3f89448277eef4676db103204b7e76
Ulrik Guenther <hello@ulrik.is>
2023-06-19 22:40:11 +0200

    HasTransferFunction: Add range property

--- src/main/kotlin/graphics/scenery/volumes/Volume.kt
+++ src/main/kotlin/graphics/scenery/volumes/Volume.kt
@@ -128,6 +128,14 @@ open class Volume(
         get() = converterSetups.getOrNull(0)?.displayRangeMax?.toFloat() ?: throw IllegalStateException()
         set(value) { setTransferFunctionRange(minDisplayRange, value) }
 
+    override var range: Pair<Float, Float>
+        get() = when(dataSource) {
+            VolumeDataSource.NullSource -> 0.0f to 0.0f
+            is VolumeDataSource.RAISource<*> -> dataSource.type.toRange()
+            is SpimDataMinimalSource -> (dataSource.sources.first().spimSource.type as NumericType<*>).toRange()
+        }
+        set(value) { logger.warn("Cannot set data range, it is automatically determined.") }
+
     /** The color map for the volume. */
     var colormap: Colormap = Colormap.get("viridis")
         set(m) {
@@ -344,6 +352,18 @@ open class Volume(
             VolumeManager.regenerateVolumeManagerWithExtraVolume(this,hub)
         }
     }
+
+    private fun NumericType<*>.toRange(): Pair<Float, Float> {
+        return when(this) {
+            is UnsignedByteType -> 0.0f to 255.0f
+            is ByteType -> -127.0f to 128.0f
+            is UnsignedShortType -> 0.0f to 65535.0f
+            is ShortType -> -32768.0f to 32767.0f
+            is FloatType -> 0.0f to 1.0f
+            else -> 0.0f to 1.0f
+        }
+    }
+
 
     override fun update(fresh: Networkable, getNetworkable: (Int) -> Networkable, additionalData: Any?) {
         if (fresh !is Volume) throw IllegalArgumentException("Update called with object of foreign class")
