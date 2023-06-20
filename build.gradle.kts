e793ececd681ea702db4c55354a1040c2a3d8ec1
Samuel Pantze <83579186+smlpt@users.noreply.github.com>
2023-06-20 21:39:06 +0200

    OpenEXR support (#555)
    * adds insideNormals for Sphere and Icosphere
    * adds OpenEXR reader

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,20 @@ repositories {
 //    mavenLocal()
 }
 
+val lwjglArtifacts = listOf(
+        "lwjgl",
+        "lwjgl-glfw",
+        "lwjgl-jemalloc",
+        "lwjgl-vulkan",
+        "lwjgl-opengl",
+        "lwjgl-openvr",
+        "lwjgl-xxhash",
+        "lwjgl-remotery",
+        "lwjgl-spvc",
+        "lwjgl-shaderc",
+        "lwjgl-tinyexr"
+)
+
 dependencies {
     implementation(platform("org.scijava:pom-scijava:35.1.1"))
     annotationProcessor("org.scijava:scijava-common:2.94.1")
@@ -44,41 +58,31 @@ dependencies {
 
 
     val lwjglVersion = "3.3.1"
-    listOf("",
-        "-glfw",
-        "-jemalloc",
-        "-vulkan",
-        "-opengl",
-        "-openvr",
-        "-xxhash",
-        "-remotery",
-        "-spvc",
-        "-shaderc"
-    ).forEach { p ->
-        api("org.lwjgl:lwjgl$p:$lwjglVersion")
+    lwjglArtifacts.forEach { artifact ->
+        api("org.lwjgl:$artifact:$lwjglVersion")
 
         lwjglNatives.forEach { native ->
             when {
                 // Vulkan binaries are only necessary on macOS
-                p.endsWith("vulkan") -> {
+                artifact.endsWith("vulkan") -> {
                     if(native.contains("macos")) {
-                        logger.info("vulkan: org.lwjgl:lwjgl$p:$lwjglVersion:$native")
-                        runtimeOnly("org.lwjgl:lwjgl$p:$lwjglVersion:$native")
+                        logger.info("vulkan: org.lwjgl:$artifact:$lwjglVersion:$native")
+                        runtimeOnly("org.lwjgl:$artifact:$lwjglVersion:$native")
                     }
                 }
 
                 // OpenVR binaries are available on all scenery-supported platforms,
                 // apart from macOS/ARM64
-                p.endsWith("openvr") -> {
+                artifact.endsWith("openvr") -> {
                     if(!(native.contains("macos") && native.contains("arm64"))) {
-                        logger.info("openvr: org.lwjgl:lwjgl$p:$lwjglVersion:$native")
-                        runtimeOnly("org.lwjgl:lwjgl$p:$lwjglVersion:$native")
+                        logger.info("openvr: org.lwjgl:$artifact:$lwjglVersion:$native")
+                        runtimeOnly("org.lwjgl:$artifact:$lwjglVersion:$native")
                     }
                 }
 
                 else -> {
-                    logger.info("else: org.lwjgl:lwjgl$p:$lwjglVersion:$native")
-                    runtimeOnly("org.lwjgl:lwjgl$p:$lwjglVersion:$native")
+                    logger.info("else: org.lwjgl:$artifact:$lwjglVersion:$native")
+                    runtimeOnly("org.lwjgl:$artifact:$lwjglVersion:$native")
                 }
             }
         }
@@ -208,18 +212,7 @@ tasks {
 
             // lwjgl natives
             lwjglNatives.forEach { nativePlatform ->
-                listOf(
-                    "",
-                    "-glfw",
-                    "-jemalloc",
-                    "-opengl",
-                    "-openvr",
-                    "-xxhash",
-                    "-remotery",
-                    "-spvc",
-                    "-shaderc",
-                    "-vulkan",
-                ).forEach pkg@ { lwjglProject ->
+                lwjglArtifacts.forEach pkg@ { lwjglProject ->
                     // OpenVR does not have macOS binaries, Vulkan only has macOS binaries
                     if(lwjglProject.contains("vulkan")
                         && !nativePlatform.contains("mac")) {
@@ -261,7 +254,7 @@ tasks {
                 classifier = "natives-all",
                 scope = "runtime")
 
-            val versionedArtifacts = listOf(
+            val versionedArtifacts = mutableListOf(
                 "flatlaf",
                 "kotlin-stdlib-common",
                 "kotlin-stdlib",
@@ -289,17 +282,7 @@ tasks {
                 "jackson-module-kotlin",
                 "jackson-dataformat-yaml",
                 "kryo",
-                "lwjgl",
-                "lwjgl-glfw",
-                "lwjgl-jemalloc",
-                "lwjgl-vulkan",
-                "lwjgl-opengl",
-                "lwjgl-openvr",
-                "lwjgl-xxhash",
-                "lwjgl-remotery",
-                "lwjgl-spvc",
-                "lwjgl-shaderc",
-                "bigvolumeviewer")
+                ) + lwjglArtifacts
 
             val toSkip = listOf("pom-scijava")
 
