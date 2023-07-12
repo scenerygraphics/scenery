9be0832bf580180e825050456f769c371de9b002
wissal <wissalagueb@gmail.com>
2023-07-12 17:10:54 +0200

    EmptyNode: created a node to transfer user choise of view

---
+++ src/main/kotlin/graphics/scenery/volumes/EmptyNode.kt
@@ -0,0 +1,25 @@
+package graphics.scenery.volumes
+
+import graphics.scenery.DefaultNode
+import graphics.scenery.Light
+import graphics.scenery.net.Networkable
+
+class EmptyNode : DefaultNode("EmptyNode"),Networkable {
+
+    var value : Boolean = false
+        set(value){
+            field = value
+            modifiedAt = System.nanoTime()
+        }
+
+    init {
+        name = "EmptyNode"
+    }
+
+    override fun update(fresh: Networkable, getNetworkable: (Int) -> Networkable, additionalData: Any?) {
+        super.update(fresh, getNetworkable,additionalData)
+        if (fresh !is EmptyNode) throw IllegalArgumentException("Update called with object of foreign class")
+        this.value = fresh.value
+
+    }
+}
