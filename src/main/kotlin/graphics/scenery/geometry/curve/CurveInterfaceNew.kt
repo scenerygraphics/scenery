36de72e167e6ee989b77100b44449991412bc456
Justin <JustinBuerger@googlemail.com>
2023-09-08 20:10:06 +0200

    Added new Curve Interface

---
+++ src/main/kotlin/graphics/scenery/geometry/curve/CurveInterfaceNew.kt
@@ -0,0 +1,35 @@
+package graphics.scenery.geometry.curve
+
+import graphics.scenery.geometry.Spline
+import org.joml.Vector3f
+
+/**
+ * Interface providing the functionality of creating a geometry which evolves along a spline object.
+ *
+ * @author Justin Buerger
+ */
+interface CurveInterfaceNew {
+
+    /**
+     * Spline object along which the curve is rendered.
+     */
+    val spline: Spline
+
+
+    /**
+     * List of shapes that will be drawn along the spline. Shapes are simple Polygons that act like a sleeve pulled
+     * along the spline. The polygon points of a polygon and its predecessor are connected by the triangles.
+     * Consider a simple curve, consisting of two triangles as base shapes (triangle1: {00, 01, 02},
+     * triangle2: {10, 11, 12}. If we cut open resulting curve mesh (to view it in 2D) it would look like this:
+     *
+     *  00_______01_______02_______00
+     *  | \     | \       | \      |
+     *  |  \    |  \      |  \     |
+     *  |   \   |   \     |   \    |
+     *  |    \  |    \    |    \   |
+     *  |     \ |     \   |     \  |
+     *  10 _____11 ______12_______10
+     *
+     */
+    val baseShapes: ()-> List<List<Vector3f>>
+}
