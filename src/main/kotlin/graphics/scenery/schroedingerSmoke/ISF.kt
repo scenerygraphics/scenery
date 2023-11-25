f68d6a15d330ebb5268e747abb0e8c54d8ecf999
Justin <JustinBuerger@googlemail.com>
2023-11-25 02:01:19 +0100

    added schroedinger flow

--- src/main/kotlin/graphics/scenery/schroedingerSmoke/ISF.kt
+++ src/main/kotlin/graphics/scenery/schroedingerSmoke/ISF.kt
@@ -11,15 +11,12 @@ class ISF(val hBar: Float, val dt: Float, torusDEC: TorusDEC) {
     val sizex = torusDEC.sizex
     val sizey = torusDEC.sizey
     val sizez = torusDEC.sizez
-    val iix: Array<Array<Array<Double>>>
-    val iiy: Array<Array<Array<Double>>>
-    val iiz: Array<Array<Array<Double>>>
+    val iix: Array<Array<Array<Double>>> = Array(nx) { i -> Array(ny) { j -> Array(nz) { k -> i.toDouble() } } }
+    val iiy: Array<Array<Array<Double>>> = Array(nx) { i -> Array(ny) { j -> Array(nz) { k -> j.toDouble() } } }
+    val iiz: Array<Array<Array<Double>>> = Array(nx) { i -> Array(ny) { j -> Array(nz) { k -> k.toDouble() } } }
     lateinit var SchroedingerMask: Array<Array<Array<Complex>>>
 
     init {
-        iix = Array(nx) { i -> Array(ny) { j -> Array(nz) { k -> i.toDouble() } } }
-        iiy = Array(nx) { i -> Array(ny) { j -> Array(nz) { k -> j.toDouble() } } }
-        iiz = Array(nx) { i -> Array(ny) { j -> Array(nz) { k -> k.toDouble() } } }
         buildSchroedinger()
     }
 
@@ -44,4 +41,39 @@ class ISF(val hBar: Float, val dt: Float, torusDEC: TorusDEC) {
         // This is a placeholder implementation
         return Complex(Math.exp(c.re) * Math.cos(c.im), Math.exp(c.re) * Math.sin(c.im))
     }
+
+    fun schroedingerFlow(psi1: Array<Array<Array<Complex>>>, psi2: Array<Array<Array<Complex>>>): Pair<Array<Array<Array<Complex>>>, Array<Array<Array<Complex>>>> {
+        var psi1Fft = fftn(psi1)
+        var psi2Fft = fftn(psi2)
+
+        psi1Fft = elementWiseMultiply(psi1Fft, SchroedingerMask)
+        psi2Fft = elementWiseMultiply(psi2Fft, SchroedingerMask)
+
+        val psi1Result = ifftn(psi1Fft)
+        val psi2Result = ifftn(psi2Fft)
+
+        return Pair(psi1Result, psi2Result)
+    }
+
+    private fun fftn(data: Array<Array<Array<Complex>>>): Array<Array<Array<Complex>>> {
+        // Implement FFT or use a library function
+        return data // Placeholder
+    }
+
+    private fun ifftn(data: Array<Array<Array<Complex>>>): Array<Array<Array<Complex>>> {
+        // Implement IFFT or use a library function
+        return data // Placeholder
+    }
+
+    private fun elementWiseMultiply(a: Array<Array<Array<Complex>>>, b: Array<Array<Array<Complex>>>): Array<Array<Array<Complex>>> {
+        return Array(a.size) { i ->
+            Array(a[0].size) { j ->
+                Array(a[0][0].size) { k ->
+                    complexProd(a[i][j][k], b[i][j][k]) // Assuming Complex class has an operator overload for multiplication
+                }
+            }
+        }
+    }
+
+
 }
