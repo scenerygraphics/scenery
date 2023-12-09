03b120ea330274ba3141eea579babfc303b339bd
Justin <JustinBuerger@googlemail.com>
2023-12-09 03:12:42 +0100

    Particles: habing a chicken and egg problem atm, don't know which version would be correct. let's seee tomorrow

--- src/main/kotlin/graphics/scenery/schroedingerSmoke/Particles.kt
+++ src/main/kotlin/graphics/scenery/schroedingerSmoke/Particles.kt
@@ -18,35 +18,35 @@ class Particles {
     }
 
     // Advect particle positions using RK4 in a grid torus with staggered velocity vx, vy, vz, for dt period of time
-    fun staggeredAdvect(torus: TorusDEC, vx: ArrayRealVector, vy: ArrayRealVector, vz: ArrayRealVector, dt: Double) {
-        val (k1x, k1y, k1z) = staggeredVelocity(x, y, z, torus, vx, vy, vz)
-        val (k2x, k2y, k2z) = staggeredVelocity(x.add(k1x.mapMultiply(dt / 2)),
-            y.add(k1y.mapMultiply(dt / 2)),
-            z.add(k1z.mapMultiply(dt / 2)),
-            torus, vx, vy, vz)
-        val (k3x, k3y, k3z) = staggeredVelocity(x.add(k2x.mapMultiply(dt / 2)),
-            y.add(k2y.mapMultiply(dt / 2)),
-            z.add(k2z.mapMultiply(dt / 2)),
-            torus, vx, vy, vz)
-        val (k4x, k4y, k4z) = staggeredVelocity(x.add(k3x.mapMultiply(dt)),
-            y.add(k3y.mapMultiply(dt)),
-            z.add(k3z.mapMultiply(dt)),
-            torus, vx, vy, vz)
+    fun staggeredAdvect(
+        torus: TorusDEC,
+        vx: Array<Array<Array<Double>>>,
+        vy: Array<Array<Array<Double>>>,
+        vz: Array<Array<Array<Double>>>,
+        dt: Double
+    ) {
+        val new_x = ArrayRealVector(x.dimension)
+        val new_y = ArrayRealVector(y.dimension)
+        val new_z = ArrayRealVector(z.dimension)
 
-        x = x.add(k1x.mapMultiply(dt / 6))
-            .add(k2x.mapMultiply(dt / 3))
-            .add(k3x.mapMultiply(dt / 3))
-            .add(k4x.mapMultiply(dt / 6))
+        for (i in 0 until x.dimension) {
+            val particleX = x.getEntry(i)
+            val particleY = y.getEntry(i)
+            val particleZ = z.getEntry(i)
 
-        y = y.add(k1y.mapMultiply(dt / 6))
-            .add(k2y.mapMultiply(dt / 3))
-            .add(k3y.mapMultiply(dt / 3))
-            .add(k4y.mapMultiply(dt / 6))
+            val (k1x, k1y, k1z) = staggeredVelocity(particleX, particleY, particleZ, torus, vx, vy, vz)
+            val (k2x, k2y, k2z) = staggeredVelocity(particleX + k1x * dt / 2, particleY + k1y * dt / 2, particleZ + k1z * dt / 2, torus, vx, vy, vz)
+            val (k3x, k3y, k3z) = staggeredVelocity(particleX + k2x * dt / 2, particleY + k2y * dt / 2, particleZ + k2z * dt / 2, torus, vx, vy, vz)
+            val (k4x, k4y, k4z) = staggeredVelocity(particleX + k3x * dt, particleY + k3y * dt, particleZ + k3z * dt, torus, vx, vy, vz)
 
-        z = z.add(k1z.mapMultiply(dt / 6))
-            .add(k2z.mapMultiply(dt / 3))
-            .add(k3z.mapMultiply(dt / 3))
-            .add(k4z.mapMultiply(dt / 6))
+            new_x.setEntry(i, particleX + dt / 6 * (k1x + 2 * k2x + 2 * k3x + k4x))
+            new_y.setEntry(i, particleY + dt / 6 * (k1y + 2 * k2y + 2 * k3y + k4y))
+            new_z.setEntry(i, particleZ + dt / 6 * (k1z + 2 * k2z + 2 * k3z + k4z))
+        }
+
+        x = new_x
+        y = new_y
+        z = new_z
     }
 
     // For removing particles
