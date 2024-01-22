10714bdfaad2cc6b7ee5d2bf00f37c36336aa76d
Jan Tiemann <j.tiemann@hzdr.de>
2024-01-22 18:36:42 +0100

    TransferFunctionEditor, DisplayRangeEditor: move DisplayRangeEditor to own file

---
+++ src/main/kotlin/graphics/scenery/volumes/DisplayRangeEditor.kt
@@ -0,0 +1,83 @@
+package graphics.scenery.volumes
+
+import graphics.scenery.ui.RangeSlider
+import net.miginfocom.swing.MigLayout
+import javax.swing.*
+import kotlin.math.max
+import kotlin.math.roundToInt
+
+/**
+ * GUI for editing the display range of volumes.
+ * Part of TransferFunctionEditor
+ */
+class DisplayRangeEditor(private val tfContainer: HasTransferFunction): JPanel(){
+
+    //RangeEditor
+    private val minText: JTextField
+    private val maxText: JTextField
+    private val rangeSlider: RangeSlider
+    private val minValueLabel: JLabel
+    private val maxValueLabel: JLabel
+
+    init {
+        this.layout = MigLayout(
+            "fill",
+            "[left, 10%]5[right, 40%]5[left, 10%]5[right, 40%]"
+        )
+        val title = BorderFactory.createTitledBorder("Display Range")
+        this.border = title
+
+        // Range editor
+        val initMinValue = max(tfContainer.minDisplayRange.toInt(), 100)
+        minText = JTextField(initMinValue.toString())
+        minValueLabel = JLabel(String.format("%.1f", tfContainer.range.first))
+
+        val initMaxValue = max(tfContainer.maxDisplayRange.toInt(), 100)
+        maxText = JTextField(initMaxValue.toString())
+        maxText.horizontalAlignment = SwingConstants.RIGHT
+        maxValueLabel = JLabel(String.format("%.1f", tfContainer.range.second))
+
+        rangeSlider = RangeSlider()
+        rangeSlider.minimum = tfContainer.range.first.roundToInt()
+        rangeSlider.maximum = tfContainer.range.second.roundToInt()
+        rangeSlider.value = tfContainer.minDisplayRange.toInt()
+        rangeSlider.upperValue = tfContainer.maxDisplayRange.toInt()
+
+        minText.addActionListener { updateSliderRange() }
+        maxText.addActionListener { updateSliderRange() }
+        rangeSlider.addChangeListener {
+            updateConverter()
+        }
+
+        val rangeEditorPanel = this
+        rangeEditorPanel.add(JLabel("min:"), "shrinkx")
+        rangeEditorPanel.add(minText, "growx")
+        rangeEditorPanel.add(JLabel("max:"), "shrinkx")
+        rangeEditorPanel.add(maxText, "growx, wrap")
+        rangeEditorPanel.add(rangeSlider, "spanx, growx, wrap")
+        rangeEditorPanel.add(minValueLabel, "spanx 2, left")
+        rangeEditorPanel.add(maxValueLabel, "spanx 2, right")
+    }
+
+    private fun updateSliderRange() {
+        val min = minText.toInt()
+        val max = maxText.toInt()
+        if (min != null && max != null) {
+            rangeSlider.value = min
+            rangeSlider.upperValue = max
+        }
+        updateConverter()
+    }
+
+    private fun JTextField.toInt() = text.toIntOrNull()
+
+    private fun updateConverter() {
+        minText.text = rangeSlider.value.toString()
+        maxText.text = rangeSlider.upperValue.toString()
+        minValueLabel.text = String.format("%.1f", tfContainer.range.first)
+        maxValueLabel.text = String.format("%.1f", tfContainer.range.second)
+
+        tfContainer.minDisplayRange = rangeSlider.value.toFloat()
+        tfContainer.maxDisplayRange = rangeSlider.upperValue.toFloat()
+    }
+}
--- src/main/kotlin/graphics/scenery/volumes/TransferFunctionEditor.kt
+++ src/main/kotlin/graphics/scenery/volumes/TransferFunctionEditor.kt
@@ -1,6 +1,5 @@
 package graphics.scenery.volumes
 
-import graphics.scenery.ui.RangeSlider
 import net.miginfocom.swing.MigLayout
 import org.jfree.chart.ChartMouseEvent
 import org.jfree.chart.ChartMouseListener
@@ -31,7 +30,6 @@ import java.awt.event.MouseMotionListener
 import java.awt.image.BufferedImage
 import javax.swing.*
 import kotlin.math.abs
-import kotlin.math.max
 import kotlin.math.pow
 import kotlin.math.roundToInt
 
@@ -63,13 +61,6 @@ class TransferFunctionEditor constructor(
     //TFEditor and Histogram
     val mainChart: JPanel
 
-    //RangeEditor
-    private val rangeEditorPanel: JPanel
-    private val minText: JTextField
-    private val maxText: JTextField
-    private val rangeSlider: RangeSlider
-    private val minValueLabel: JLabel
-    private val maxValueLabel: JLabel
 
     //ColormapEditor
     private val colormapPanel : JPanel
@@ -212,7 +203,6 @@ class TransferFunctionEditor constructor(
             override fun mouseMoved(e: MouseEvent) {}
         })
 
-
         mainChart.addChartMouseListener(object : ChartMouseListener {
             override fun chartMouseClicked(e: ChartMouseEvent) {
                 if (e.entity is XYItemEntity) {
@@ -295,43 +285,9 @@ class TransferFunctionEditor constructor(
             }
         }
 
-        // Range editor
-        val initMinValue = max(tfContainer.minDisplayRange.toInt(), 100)
-        minText = JTextField(initMinValue.toString())
-        minValueLabel = JLabel(String.format("%.1f", tfContainer.range.first))
 
-        val initMaxValue = max(tfContainer.maxDisplayRange.toInt(), 100)
-        maxText = JTextField(initMaxValue.toString())
-        maxText.horizontalAlignment = SwingConstants.RIGHT
-        maxValueLabel = JLabel(String.format("%.1f", tfContainer.range.second))
-
-        rangeSlider = RangeSlider()
-        rangeSlider.minimum = tfContainer.range.first.roundToInt()
-        rangeSlider.maximum = tfContainer.range.second.roundToInt()
-        rangeSlider.value = tfContainer.minDisplayRange.toInt()
-        rangeSlider.upperValue = tfContainer.maxDisplayRange.toInt()
-
-        minText.addActionListener { updateSliderRange() }
-        maxText.addActionListener { updateSliderRange() }
-        rangeSlider.addChangeListener {
-            updateConverter()
-        }
-
-        rangeEditorPanel = JPanel()
-        rangeEditorPanel.layout = MigLayout("fill",
-            "[left, 10%]5[right, 40%]5[left, 10%]5[right, 40%]")
-        add(rangeEditorPanel, "grow")
-
-        rangeEditorPanel.add(JLabel("min:"), "shrinkx")
-        rangeEditorPanel.add(minText, "growx")
-        rangeEditorPanel.add(JLabel("max:"), "shrinkx")
-        rangeEditorPanel.add(maxText, "growx, wrap")
-        rangeEditorPanel.add(rangeSlider, "spanx, growx, wrap")
-        rangeEditorPanel.add(minValueLabel, "spanx 2, left")
-        rangeEditorPanel.add(maxValueLabel, "spanx 2, right")
-
+        add(DisplayRangeEditor(tfContainer), "grow")
         loadTransferFunction(tfContainer.transferFunction)
-//        updateSliderRange()
 
         //ColorMap manipulation
         colormapPanel = JPanel()
@@ -381,28 +337,6 @@ class TransferFunctionEditor constructor(
 
         return tfImage
     }
-
-    private fun updateSliderRange() {
-        val min = minText.toInt()
-        val max = maxText.toInt()
-        if (min != null && max != null) {
-            rangeSlider.value = min
-            rangeSlider.upperValue = max
-        }
-        updateConverter()
-    }
-
-    private fun updateConverter() {
-        minText.text = rangeSlider.value.toString()
-        maxText.text = rangeSlider.upperValue.toString()
-        minValueLabel.text = String.format("%.1f", tfContainer.range.first)
-        maxValueLabel.text = String.format("%.1f", tfContainer.range.second)
-
-        tfContainer.minDisplayRange = rangeSlider.value.toFloat()
-        tfContainer.maxDisplayRange = rangeSlider.upperValue.toFloat()
-    }
-
-    private fun JTextField.toInt() = text.toIntOrNull()
 
     private fun addControlpoint(targetCP: MouseDragTarget) {
         val chart = mainChart as ChartPanel
