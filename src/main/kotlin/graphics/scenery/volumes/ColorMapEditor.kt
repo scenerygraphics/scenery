507fd13bec8f3d30e6aae0fd5f383caf945def83
Jan Tiemann <j.tiemann@hzdr.de>
2024-01-24 15:16:11 +0100

    ColorMapEditor: move dots

--- src/main/kotlin/graphics/scenery/volumes/ColorMapEditor.kt
+++ src/main/kotlin/graphics/scenery/volumes/ColorMapEditor.kt
@@ -1,10 +1,14 @@
 package graphics.scenery.volumes
 
 import net.miginfocom.swing.MigLayout
-import org.joml.Vector3f
 import java.awt.*
+import java.awt.event.MouseEvent
+import java.awt.event.MouseListener
+import java.awt.event.MouseMotionListener
+import org.joml.Math.clamp
 import javax.swing.JFrame
 import javax.swing.JPanel
+import kotlin.math.absoluteValue
 
 
 class ColorMapEditor : JPanel() {
@@ -15,10 +19,46 @@ class ColorMapEditor : JPanel() {
         ColorPoint(1f, Color(0f, 0f, 0.9f))
     )
 
+    var hoveredOver:ColorPoint? = null
+    var dragging:ColorPoint? = null
+
     init {
         this.layout = MigLayout()
         this.preferredSize = Dimension(200, preferredSize.height)
+
+        this.addMouseListener(object : MouseListener{
+            override fun mouseClicked(e: MouseEvent) {}
+            override fun mousePressed(e: MouseEvent) {
+                val cp = pointAtMouse(e)
+                cp?.let {
+                    if (0f < cp.position && cp.position < 1.0f){
+                        // dont move first and last point
+                        dragging = cp
+                    }
+                }
+            }
+            override fun mouseReleased(e: MouseEvent?) {
+                dragging = null
+            }
+            override fun mouseEntered(e: MouseEvent?) {}
+            override fun mouseExited(e: MouseEvent?) {}
+        })
+
+        this.addMouseMotionListener(object: MouseMotionListener{
+            override fun mouseDragged(e: MouseEvent) {
+                dragging?.position = clamp(0.05f,0.95f, e.x/width.toFloat())
+                repaint()
+            }
+            override fun mouseMoved(e: MouseEvent) {
+                val temp = hoveredOver
+                hoveredOver = pointAtMouse(e) // height is the radius of the color point sphere
+                if (temp != hoveredOver) repaint()
+            }
+        })
     }
+
+    private fun pointAtMouse(e: MouseEvent) =
+        colorPoints.firstOrNull() { (e.x - (width * it.position)).absoluteValue < height / 2 }
 
     override fun paintComponent(g: Graphics) {
         super.paintComponent(g)
@@ -26,8 +66,9 @@ class ColorMapEditor : JPanel() {
         g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY)
         val w = width
         val h = height
-
         val pointList = colorPoints.sortedBy { it.position }
+
+        // background Gradient
         for (i in 0 until pointList.size - 1) {
             val p1 = pointList[i]
             val p2 = pointList[i + 1]
@@ -40,14 +81,14 @@ class ColorMapEditor : JPanel() {
 
         }
 
+        // color point markers
         pointList.forEach {
             val p1x = w * it.position
-            g2d.paint = Color.gray
+            g2d.paint = if (it == hoveredOver) Color.white else Color.BLACK
             g2d.fillOval(p1x.toInt() - h / 2, 0, h, h)
 
             val margin = 0.05f
             val innerSize = h * (1f - margin)
-
             g2d.paint = it.color
             g2d.fillOval(
                 (p1x - innerSize / 2).toInt(),
@@ -58,19 +99,7 @@ class ColorMapEditor : JPanel() {
         }
     }
 
-    private fun Vector3f.toColor(): Color {
-        return Color(this.x, this.y, this.z)
-    }
-
-    private fun Color.contrast(): Color {
-        return Color(
-            255 - this.red,
-            255 - this.green,
-            255 - this.blue
-        )
-    }
-
-    class ColorPoint(val position: Float, var color: Color)
+    class ColorPoint(var position: Float, var color: Color)
 
     companion object {
         @JvmStatic
