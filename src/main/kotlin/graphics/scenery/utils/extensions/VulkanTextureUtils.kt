df6a17ae160cd8f1657b5188e79991ffedfcc690
Aryaman Gupta <argupta@mpi-cbg.de>
2024-03-01 15:07:57 +0100

    VulkanTextureUtils: creates fetchTexture utility

---
+++ src/main/kotlin/graphics/scenery/utils/extensions/VulkanTextureUtils.kt
@@ -0,0 +1,23 @@
+package graphics.scenery.utils.extensions
+
+import graphics.scenery.backends.vulkan.VulkanTexture
+import graphics.scenery.textures.Texture
+import graphics.scenery.utils.lazyLogger
+
+fun VulkanTexture.fetchTexture(texture: Texture): Int {
+    val logger by lazyLogger()
+
+    val ref = VulkanTexture.getReference(texture)
+    val buffer = texture.contents ?: return -1
+
+    if(ref != null) {
+        val start = System.nanoTime()
+        texture.contents = ref.copyTo(buffer, true)
+        val end = System.nanoTime()
+        logger.info("The request textures of size ${texture.contents?.remaining()?.toFloat()?.div((1024f*1024f))} took: ${(end.toDouble()-start.toDouble())/1000000.0}")
+    } else {
+        logger.error("In fetchTexture: Texture not accessible")
+    }
+
+    return 0
+}
