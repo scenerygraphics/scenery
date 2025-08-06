5383f9c6d8b4eaa101675cbb07a1cd05873fe27f
Ulrik Guenther <hello@ulrik.is>
2025-08-06 16:31:28 +0200

    DemoReelExample.kt: bile is working

--- src/test/kotlin/graphics/scenery/tests/examples/cluster/DemoReelExample.kt
+++ src/test/kotlin/graphics/scenery/tests/examples/cluster/DemoReelExample.kt
@@ -185,39 +185,6 @@ class DemoReelExample: SceneryBase("Demo Reel") {
         //var client: AutofabClient? = null
         @JvmStatic
         fun main(args: Array<String>) {
-//            if(System.getProperty("scenery.master") != null) {
-//                thread {
-////                val clusterLaunch = Runtime.getRuntime().exec("C:\\Users\\mosaic\\Code\\scenery-base\\run-cluster.bat graphics.scenery.tests.examples.cluster.CaveClientExample",
-////                    null, Paths.get(".").absolute().parent.parent.toFile())
-//                    //val client = AutofabClient()
-//                    //val hosts = client.listAvailableHosts()
-//                    //logger.info("Available hosts are ${hosts.joinToString { it.hostAddress }}")
-////                val clusterLaunchCommand = """S:\\jdk\\temurin-21.0.3.9\\bin\\java.exe -cp "S:/scenery/build/libs/*;S:/scenery/build/dependencies/*" -ea -Xmx16g -Dscenery.VulkanRenderer.UseOpenGLSwapchain=false -Dscenery.Renderer.Framelock=true -Dscenery.RunFullscreen=false-Dscenery.Renderer.Config=DeferredShadingStereo.yml -Dscenery.vr.Active=true -Dscenery.ScreenConfig=CAVEExample.yml -Dscenery.TrackerAddress=DTrack:body-0@224.0.1.1:5001 -Dscenery.ScreenName=front graphics.scenery.tests.examples.basic.TexturedCubeExample"""
-//                    //val clusterLaunchCommand =
-//                    //    """S:\\jdk\\temurin-21.0.3.9\\bin\\java.exe -Dorg.lwjgl.system.stackSize=500 -cp "S:/scenery/build/libs/*;S:/scenery/build/dependencies/*" -ea -Xmx16g -Dscenery.VulkanRenderer.UseOpenGLSwapchain=false -Dscenery.Renderer.Framelock=true -Dscenery.RunFullscreen=false-Dscenery.Renderer.Config=DeferredShadingStereo.yml -Dscenery.vr.Active=true -Dscenery.ScreenConfig=CAVEExample.yml -Dscenery.TrackerAddress=DTrack:body-0@224.0.1.1:5001 -Dscenery.ScreenName=front graphics.scenery.tests.examples.basic.TexturedCubeExample"""
-//
-//                    //client.launchOnAvailableHosts(clusterLaunchCommand, register = false)
-//
-////                BufferedReader(InputStreamReader(clusterLaunch.inputStream)).use { input ->
-////                    var line: String?
-////                    while (input.readLine().also { line = it } != null) {
-////                        DemoReelExample.logger.info("Cluster: $line")
-////                    }
-////                }
-//                    client = AutofabClient()
-//                    while(client!!.listAvailableHosts().size == 0) {
-//                        Thread.sleep(1000)
-//                        logger.info("Waiting for available hosts...")
-//                    }
-//
-//                    val hosts = client!!.listAvailableHosts()
-//                    logger.info("Available hosts are ${hosts.joinToString { it.hostAddress }}")
-//                    //client.shutdownLaunchedProcesses()
-//                    val clusterLaunchCommand = """S:\\jdk\\temurin-21.0.3.9\\bin\\java.exe -cp "S:/scenery/build/libs/*;S:/scenery/build/dependencies/*" -ea -Xmx16g -Dorg.lwjgl.system.stackSize=300 -Dscenery.VulkanRenderer.UseOpenGLSwapchain=false -Dscenery.Renderer.Framelock=true -Dscenery.RunFullscreen=false -Dscenery.Renderer.Config=DeferredShadingStereo.yml -Dscenery.vr.Active=true -Dscenery.ScreenConfig=CAVEExample.yml -Dscenery.TrackerAddress=DTrack:body-0@224.0.1.1:5001 -Dscenery.DriveLetter=D """ + this::class.java.name.substringBefore("$")
-//
-//                    client!!.launchOnAvailableHosts(clusterLaunchCommand, register = false)
-//                }
-//            }
             DemoReelExample().main()
         }
     }
--- src/test/kotlin/graphics/scenery/tests/examples/cluster/DemoReelLaunchExample.kt
+++ src/test/kotlin/graphics/scenery/tests/examples/cluster/DemoReelLaunchExample.kt
@@ -33,40 +33,27 @@ class DemoReelLaunchExample: SceneryBase("Demo Reel") {
         var client: AutofabClient? = null
         @JvmStatic
         fun main(args: Array<String>) {
-            //if(System.getProperty("scenery.master") != null) {
-                thread {
-//                val clusterLaunch = Runtime.getRuntime().exec("C:\\Users\\mosaic\\Code\\scenery-base\\run-cluster.bat graphics.scenery.tests.examples.cluster.CaveClientExample",
-//                    null, Paths.get(".").absolute().parent.parent.toFile())
-                    //val client = AutofabClient()
-                    //val hosts = client.listAvailableHosts()
-                    //logger.info("Available hosts are ${hosts.joinToString { it.hostAddress }}")
-//                val clusterLaunchCommand = """S:\\jdk\\temurin-21.0.3.9\\bin\\java.exe -cp "S:/scenery/build/libs/*;S:/scenery/build/dependencies/*" -ea -Xmx16g -Dscenery.VulkanRenderer.UseOpenGLSwapchain=false -Dscenery.Renderer.Framelock=true -Dscenery.RunFullscreen=false-Dscenery.Renderer.Config=DeferredShadingStereo.yml -Dscenery.vr.Active=true -Dscenery.ScreenConfig=CAVEExample.yml -Dscenery.TrackerAddress=DTrack:body-0@224.0.1.1:5001 -Dscenery.ScreenName=front graphics.scenery.tests.examples.basic.TexturedCubeExample"""
-                    //val clusterLaunchCommand =
-                    //    """S:\\jdk\\temurin-21.0.3.9\\bin\\java.exe -Dorg.lwjgl.system.stackSize=500 -cp "S:/scenery/build/libs/*;S:/scenery/build/dependencies/*" -ea -Xmx16g -Dscenery.VulkanRenderer.UseOpenGLSwapchain=false -Dscenery.Renderer.Framelock=true -Dscenery.RunFullscreen=false-Dscenery.Renderer.Config=DeferredShadingStereo.yml -Dscenery.vr.Active=true -Dscenery.ScreenConfig=CAVEExample.yml -Dscenery.TrackerAddress=DTrack:body-0@224.0.1.1:5001 -Dscenery.ScreenName=front graphics.scenery.tests.examples.basic.TexturedCubeExample"""
+            val clusterScene = DemoReelExample()
+//            val clusterScene = CaveBaseScene()
 
-                    //client.launchOnAvailableHosts(clusterLaunchCommand, register = false)
+            client = AutofabClient()
+            while(client!!.listAvailableHosts().size == 0) {
+                Thread.sleep(1000)
+                logger.info("Waiting for available hosts...")
+            }
 
-//                BufferedReader(InputStreamReader(clusterLaunch.inputStream)).use { input ->
-//                    var line: String?
-//                    while (input.readLine().also { line = it } != null) {
-//                        DemoReelExample.logger.info("Cluster: $line")
-//                    }
-//                }
-                    client = AutofabClient()
-                    while(client!!.listAvailableHosts().size == 0) {
-                        Thread.sleep(1000)
-                        logger.info("Waiting for available hosts...")
-                    }
+            val hosts = client!!.listAvailableHosts()
+            logger.info("Available hosts are ${hosts.joinToString { it.hostAddress }}")
 
-                    val hosts = client!!.listAvailableHosts()
-                    logger.info("Available hosts are ${hosts.joinToString { it.hostAddress }}")
-                    //client!!.shutdownLaunchedProcesses()
-                    val clusterLaunchCommand = """S:\\jdk\\temurin-21.0.3.9\\bin\\java.exe -cp "S:/scenery/build/libs/*;S:/scenery/build/dependencies/*" -ea -Xmx16g -Dorg.lwjgl.system.stackSize=300 -Dscenery.VulkanRenderer.UseOpenGLSwapchain=true -Dscenery.Renderer.Framelock=true -Dscenery.RunFullscreen=true -Dscenery.Renderer.Config=DeferredShadingStereo.yml -Dscenery.vr.Active=true -Dscenery.ScreenConfig=CAVEExample.yml -Dscenery.TrackerAddress=DTrack:body-0@224.0.1.1:5000 -Dscenery.DriveLetter=D --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.lang.invoke=ALL-UNNAMED --add-opens=java.base/java.net=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED --add-opens=java.base/java.time=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED --add-opens=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.base/sun.util.calendar=ALL-UNNAMED -Dscenery.ServerAddress=tcp://10.1.2.201 """ + DemoReelExample::class.java.name//.substringBefore("$")
+            var clusterLaunchCommand = """S:\\jdk\\temurin-21.0.3.9\\bin\\java.exe -cp "S:/scenery/build/libs/*;S:/scenery/build/dependencies/*" -ea -Xmx16g -Dorg.lwjgl.system.stackSize=300 -Dscenery.TrackerAddress=DTrack:body-0@224.0.1.1:5000 -Dscenery.DriveLetter=E -Dscenery.RunFullscreen=true -Dscenery.Renderer.Config=DeferredShadingStereo.yml -Dscenery.VulkanRenderer.UseOpenGLSwapchain=true -Dscenery.Renderer.Framelock=true -Dscenery.vr.Active=true -Dscenery.ScreenConfig=CAVEExample.yml -Dscenery.ServerAddress=tcp://10.1.2.201"""
 
-                    client!!.launchOnAvailableHosts(clusterLaunchCommand, register = false)
-                }
-            //}
-            DemoReelExample().main()
+            clusterLaunchCommand += """ --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.lang.invoke=ALL-UNNAMED --add-opens=java.base/java.net=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED --add-opens=java.base/java.time=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED --add-opens=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.base/sun.util.calendar=ALL-UNNAMED """
+            clusterLaunchCommand +=  clusterScene::class.java.name
+
+            //client!!.shutdownLaunchedProcesses()
+            client!!.launchOnAvailableHosts(clusterLaunchCommand, register = false)
+
+            clusterScene.main()
         }
     }
 }
