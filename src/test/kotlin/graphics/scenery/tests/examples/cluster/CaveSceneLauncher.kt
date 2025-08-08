7253d137500a49bc16e2811d7730dd9865f7ce32
Ulrik Guenther <hello@ulrik.is>
2025-08-08 14:21:48 +0200

    CaveSceneLauncher.kt: add slim start option

--- src/test/kotlin/graphics/scenery/tests/examples/cluster/CaveSceneLauncher.kt
+++ src/test/kotlin/graphics/scenery/tests/examples/cluster/CaveSceneLauncher.kt
@@ -17,7 +17,8 @@ class CaveSceneLauncher: SceneryBase("Demo Reel") {
         @JvmStatic
         fun main(args: Array<String>) {
 //            val caveScene = CaveClientExample()
-            val caveScene = DemoReelExample()
+            val caveScene = MaybeWithSlicing()
+            val slim = true
 //            val caveScene = CaveBaseScene()
 
             client = AutofabClient()
@@ -32,8 +33,11 @@ class CaveSceneLauncher: SceneryBase("Demo Reel") {
             var clusterLaunchCommand = """S:\\jdk\\temurin-21.0.3.9\\bin\\java.exe -cp "S:/scenery/build/libs/*;S:/scenery/build/dependencies/*" -ea -Xmx16g -Dorg.lwjgl.system.stackSize=300 -Dscenery.TrackerAddress=DTrack:body-0@224.0.1.1:5000 -Dscenery.DriveLetter=E -Dscenery.RunFullscreen=true -Dscenery.Renderer.Config=DeferredShadingStereo.yml -Dscenery.VulkanRenderer.UseOpenGLSwapchain=true -Dscenery.Renderer.Framelock=true -Dscenery.vr.Active=true -Dscenery.ScreenConfig=CAVEExample.yml -Dscenery.ServerAddress=tcp://10.1.2.201"""
 
             clusterLaunchCommand += """ --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.lang.invoke=ALL-UNNAMED --add-opens=java.base/java.net=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED --add-opens=java.base/java.time=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED --add-opens=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.base/sun.util.calendar=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED """
-            clusterLaunchCommand +=  caveScene::class.java.name.substringBefore("$")
-
+            if (slim){
+                clusterLaunchCommand += CaveClientExample::class.java.name.substringBefore("$")
+            } else {
+                clusterLaunchCommand += caveScene::class.java.name.substringBefore("$")
+            }
             //client!!.shutdownLaunchedProcesses()
             client!!.launchOnAvailableHosts(clusterLaunchCommand, register = false)
 
