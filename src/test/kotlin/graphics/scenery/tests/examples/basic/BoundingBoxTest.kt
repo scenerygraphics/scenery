a07cdd911922be1f04c9954a9dfa5cff99326fa2
Curtis Rueden <ctrueden@wisc.edu>
2025-10-01 16:12:49 -0500

    Let BoundingBoxTest display the scene from main

--- src/test/kotlin/graphics/scenery/tests/examples/basic/BoundingBoxTest.kt
+++ src/test/kotlin/graphics/scenery/tests/examples/basic/BoundingBoxTest.kt
@@ -2,10 +2,14 @@ package graphics.scenery.tests.examples.basic
 
 import graphics.scenery.Box
 import graphics.scenery.Camera
-import graphics.scenery.Light
+import graphics.scenery.DetachedHeadCamera
 import graphics.scenery.Mesh
 import graphics.scenery.PointLight
 import graphics.scenery.Scene
+import graphics.scenery.SceneryBase
+import graphics.scenery.backends.Renderer
+import graphics.scenery.attribute.material.Material
+import graphics.scenery.numerics.Random
 import org.joml.Quaternionf
 import org.joml.Vector3f
 import kotlin.test.Test
@@ -17,24 +21,16 @@ import kotlin.test.assertTrue
  * @author Samuel Pantze
  *  */
 
-class BoundingBoxTest {
+class BoundingBoxTest : SceneryBase("BoundingBoxTest") {
 
-    /** Test whether two boxes overlap. Each box is a child of a transformed parent. */
-    @Test
-    fun testOverlap() {
+    data class Quad<out A, out B, out C, out D>(
+        val first: A,
+        val second: B,
+        val third: C,
+        val fourth: D
+    ) {}
 
-        val scene = Scene()
-        val cam = Camera()
-        scene.addChild(cam)
-
-        with(cam) {
-            spatial {
-                position = Vector3f(0f, 0f, 6.0f)
-            }
-            perspectiveCamera(70.0f, 1000, 1000, 1.0f, 1000.0f)
-            scene.addChild(this)
-        }
-
+    private fun createTestBoxes(scene: Scene): Quad<Mesh, Box, Mesh, Box> {
         // Box parent and box child are the objects to be tested against
         val boxParent = Mesh()
         val boxChild = Box(Vector3f(7f, 4f, 2f))
@@ -45,6 +41,9 @@ class BoundingBoxTest {
         }
         boxChild.spatial {
             position = Vector3f(-2.500E+0f, -2.500E+0f, -2.500E+0f)
+        }
+        boxChild.material {
+            diffuse = Vector3f(1.0f, 1.0f, 1.0f)
         }
         scene.addChild(boxParent)
 
@@ -62,15 +61,83 @@ class BoundingBoxTest {
             scale = Vector3f(3.0f, 3.0f, 3.0f)
             position = Vector3f(-7.0f, -2.0f, -2.0f)
         }
+        intersectionChild.material {
+            diffuse = Vector3f(0.2f, 1.0f, 0.3f)
+        }
         intersectionParent.addChild(intersectionChild)
 
-        // Since there are no renderes frames and thus no automatic world matrix updates, we do it here
-        intersectionParent.spatial().updateWorld(true, true)
+        return Quad(boxParent, boxChild, intersectionParent, intersectionChild)
+    }
+
+    /** Test whether two boxes overlap. Each box is a child of a transformed parent. */
+    @Test
+    fun testOverlap() {
+        val scene = Scene()
+        val (boxChild, boxParent, intersectionChild, intersectionParent) = createTestBoxes(scene)
+
+        // Since there are no rendered frames and thus no automatic world matrix updates, we do it here
         boxParent.spatial().updateWorld(true, true)
+        intersectionParent.spatial().updateWorld(true, true)
 
         val hit = intersectionChild.spatial().intersects(boxChild, true)
 
-        assertTrue(hit, "Boxes overlap.")
+        assertTrue(hit, "Overlapping boxes expected.")
+    }
+
+    override fun init() {
+        renderer = hub.add(Renderer.createRenderer(hub, applicationName, scene, 1280, 720))
+
+        val cam: Camera = DetachedHeadCamera()
+        with(cam) {
+            spatial {
+                position = Vector3f(0f, 0f, 15.0f)
+            }
+            perspectiveCamera(70.0f, windowWidth, windowHeight, 1.0f, 1000.0f)
+            scene.addChild(this)
+        }
+
+        createTestBoxes(scene)
+
+        val lights = (0..3).map {
+            PointLight(radius = 100.0f)
+        }.map {
+            it.spatial {
+                position = Random.random3DVectorFromRange(-50.0f, 50.0f)
+            }
+            it.emissionColor = Vector3f(1.0f, 1.0f, 1.0f)
+            it.intensity = Random.randomFromRange(0.5f, 1.5f)
+            it
+        }
+
+        lights.forEach { scene.addChild(it) }
+
+        val hullbox = Box(Vector3f(100.0f, 100.0f, 100.0f), insideNormals = true)
+        with(hullbox) {
+            spatial {
+                position = Vector3f(0.0f, 0.0f, 0.0f)
+            }
+
+            material {
+                ambient = Vector3f(0.6f, 0.6f, 0.6f)
+                diffuse = Vector3f(0.4f, 0.4f, 0.4f)
+                specular = Vector3f(0.0f, 0.0f, 0.0f)
+                cullingMode = Material.CullingMode.Front
+            }
+
+            scene.addChild(this)
+        }
+    }
+
+    override fun inputSetup() {
+        super.inputSetup()
+        setupCameraModeSwitching()
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            BoundingBoxTest().main()
+        }
     }
 
 }
