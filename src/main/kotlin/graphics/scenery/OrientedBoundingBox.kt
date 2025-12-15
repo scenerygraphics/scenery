eea7455b9557b0b0e3daf3b14c6aff713623be0c
Samuel Pantze <83579186+smlpt@users.noreply.github.com>
2025-12-15 13:31:04 +0100

    OrientedBoundingBox: copied and fixed JOMLs ObOb intersection (fix is in the rotation matrix calculation)

--- src/main/kotlin/graphics/scenery/OrientedBoundingBox.kt
+++ src/main/kotlin/graphics/scenery/OrientedBoundingBox.kt
@@ -68,105 +68,138 @@ open class OrientedBoundingBox(val n: Node, val min: Vector3f, val max: Vector3f
         return BoundingSphere(origin, radius)
     }
 
-    /** TODO This is a temporary fix for https://github.com/JOML-CI/JOML/issues/379 */
-    fun testObbIntersection(
-        b0c: Vector3f, b0uX: Vector3f, b0uY: Vector3f, b0uZ: Vector3f, b0hs: Vector3f,
-        b1c: Vector3f, b1uX: Vector3f, b1uY: Vector3f, b1uZ: Vector3f, b1hs: Vector3f
+    /** TODO This is a temporary fix for https://github.com/JOML-CI/JOML/issues/379.
+     * This code is a copy of JOMLs own testObOb code, but with the rotation matrices fixed (as described in the issue). */
+    fun testObOb(
+    b0c: Vector3f, b0uX: Vector3f, b0uY: Vector3f, b0uZ: Vector3f, b0hs: Vector3f,
+    b1c: Vector3f, b1uX: Vector3f, b1uY: Vector3f, b1uZ: Vector3f, b1hs: Vector3f): Boolean {
+        return testObOb(
+            b0c.x, b0c.y, b0c.z, b0uX.x, b0uX.y, b0uX.z, b0uY.x, b0uY.y, b0uY.z, b0uZ.x, b0uZ.y, b0uZ.z, b0hs.x, b0hs.y, b0hs.z,
+            b1c.x, b1c.y, b1c.z, b1uX.x, b1uX.y, b1uX.z, b1uY.x, b1uY.y, b1uY.z, b1uZ.x, b1uZ.y, b1uZ.z, b1hs.x, b1hs.y, b1hs.z);
+    }
+
+    fun testObOb(
+        b0cX: Float, b0cY: Float, b0cZ: Float,
+        b0uXx: Float, b0uXy: Float, b0uXz: Float,
+        b0uYx: Float, b0uYy: Float, b0uYz: Float,
+        b0uZx: Float, b0uZy: Float, b0uZz: Float,
+        b0hsX: Float, b0hsY: Float, b0hsZ: Float,
+        b1cX: Float, b1cY: Float, b1cZ: Float,
+        b1uXx: Float, b1uXy: Float, b1uXz: Float,
+        b1uYx: Float, b1uYy: Float, b1uYz: Float,
+        b1uZx: Float, b1uZy: Float, b1uZz: Float,
+        b1hsX: Float, b1hsY: Float, b1hsZ: Float
     ): Boolean {
-        // Vector from box 0 center to box 1 center
-        val t = Vector3f(b1c).sub(b0c)
+        var ra: Float
+        var rb: Float
 
-        // Rotation matrix expressing box 1 in box 0's coordinate frame
-        val r = Array(3) { FloatArray(3) }
-        val absR = Array(3) { FloatArray(3) }
+        // Compute rotation matrix expressing b in a's coordinate frame (column-major)
+        val rm00 = b0uXx * b1uXx + b0uXy * b1uXy + b0uXz * b1uXz;
+        val rm01 = b0uXx * b1uYx + b0uXy * b1uYy + b0uXz * b1uYz;
+        val rm02 = b0uXx * b1uZx + b0uXy * b1uZy + b0uXz * b1uZz;
+        val rm10 = b0uYx * b1uXx + b0uYy * b1uXy + b0uYz * b1uXz;
+        val rm11 = b0uYx * b1uYx + b0uYy * b1uYy + b0uYz * b1uYz;
+        val rm12 = b0uYx * b1uZx + b0uYy * b1uZy + b0uYz * b1uZz;
+        val rm20 = b0uZx * b1uXx + b0uZy * b1uXy + b0uZz * b1uXz;
+        val rm21 = b0uZx * b1uYx + b0uZy * b1uYy + b0uZz * b1uYz;
+        val rm22 = b0uZx * b1uZx + b0uZy * b1uZy + b0uZz * b1uZz;
 
-        val b0Axes = arrayOf(b0uX, b0uY, b0uZ)
-        val b1Axes = arrayOf(b1uX, b1uY, b1uZ)
+        // Compute common subexpressions. Add in an epsilon term to
+        // counteract arithmetic errors when two edges are parallel and
+        // their cross product is (near) null (see text for details)
+        val EPSILON = 1E-5f
+        val arm00 = abs(rm00) + EPSILON
+        val arm01 = abs(rm01) + EPSILON
+        val arm02 = abs(rm02) + EPSILON
+        val arm10 = abs(rm10) + EPSILON
+        val arm11 = abs(rm11) + EPSILON
+        val arm12 = abs(rm12) + EPSILON
+        val arm20 = abs(rm20) + EPSILON
+        val arm21 = abs(rm21) + EPSILON
+        val arm22 = abs(rm22) + EPSILON
 
-        // Compute rotation matrix and absolute rotation matrix
-        for (i in 0..2) {
-            for (j in 0..2) {
-                r[i][j] = b0Axes[i].dot(b1Axes[j])
-                absR[i][j] = abs(r[i][j]) + 1e-6f // Add epsilon to counteract arithmetic errors
-            }
-        }
+        // Compute translation vector t
+        val tx = b1cX - b0cX
+        val ty = b1cY - b0cY
+        val tz = b1cZ - b0cZ
 
-        val b0HalfSizes = floatArrayOf(b0hs.x, b0hs.y, b0hs.z)
-        val b1HalfSizes = floatArrayOf(b1hs.x, b1hs.y, b1hs.z)
+        // Bring translation into a's coordinate frame
+        val tax = tx * b0uXx + ty * b0uXy + tz * b0uXz
+        val tay = tx * b0uYx + ty * b0uYy + tz * b0uYz
+        val taz = tx * b0uZx + ty * b0uZy + tz * b0uZz
 
-        // Test axes L = A0, A1, A2 (box 0's axes)
-        for (i in 0..2) {
-            val tProj = t.dot(b0Axes[i])
-            val r0 = b0HalfSizes[i]
-            val r1 = b1HalfSizes[0] * absR[i][0] + b1HalfSizes[1] * absR[i][1] + b1HalfSizes[2] * absR[i][2]
-            if (abs(tProj) > r0 + r1) return false
-        }
+        // Test axes L = A0, L = A1, L = A2
+        ra = b0hsX
+        rb = b1hsX * arm00 + b1hsY * arm01 + b1hsZ * arm02
+        if (abs(tax) > ra + rb) return false
 
-        // Test axes L = B0, B1, B2 (box 1's axes)
-        for (i in 0..2) {
-            val tProj = t.dot(b1Axes[i])
-            val r0 = b0HalfSizes[0] * absR[0][i] + b0HalfSizes[1] * absR[1][i] + b0HalfSizes[2] * absR[2][i]
-            val r1 = b1HalfSizes[i]
-            if (abs(tProj) > r0 + r1) return false
-        }
+        ra = b0hsY
+        rb = b1hsX * arm10 + b1hsY * arm11 + b1hsZ * arm12
+        if (abs(tay) > ra + rb) return false
 
-        // Test 9 cross product axes (A x B)
+        ra = b0hsZ
+        rb = b1hsX * arm20 + b1hsY * arm21 + b1hsZ * arm22
+        if (abs(taz) > ra + rb) return false
 
-        // L = A0 x B0
-        var tProj = t.z * r[1][0] - t.y * r[2][0]
-        var r0 = b0HalfSizes[1] * absR[2][0] + b0HalfSizes[2] * absR[1][0]
-        var r1 = b1HalfSizes[1] * absR[0][2] + b1HalfSizes[2] * absR[0][1]
-        if (abs(tProj) > r0 + r1) return false
+        // Test axes L = B0, L = B1, L = B2
+        ra = b0hsX * arm00 + b0hsY * arm10 + b0hsZ * arm20
+        rb = b1hsX
+        if (abs(tax * rm00 + tay * rm10 + taz * rm20) > ra + rb) return false
 
-        // L = A0 x B1
-        tProj = t.z * r[1][1] - t.y * r[2][1]
-        r0 = b0HalfSizes[1] * absR[2][1] + b0HalfSizes[2] * absR[1][1]
-        r1 = b1HalfSizes[0] * absR[0][2] + b1HalfSizes[2] * absR[0][0]
-        if (abs(tProj) > r0 + r1) return false
+        ra = b0hsX * arm01 + b0hsY * arm11 + b0hsZ * arm21
+        rb = b1hsY
+        if (abs(tax * rm01 + tay * rm11 + taz * rm21) > ra + rb) return false
 
-        // L = A0 x B2
-        tProj = t.z * r[1][2] - t.y * r[2][2]
-        r0 = b0HalfSizes[1] * absR[2][2] + b0HalfSizes[2] * absR[1][2]
-        r1 = b1HalfSizes[0] * absR[0][1] + b1HalfSizes[1] * absR[0][0]
-        if (abs(tProj) > r0 + r1) return false
+        ra = b0hsX * arm02 + b0hsY * arm12 + b0hsZ * arm22
+        rb = b1hsZ
+        if (abs(tax * rm02 + tay * rm12 + taz * rm22) > ra + rb) return false
 
-        // L = A1 x B0
-        tProj = t.x * r[2][0] - t.z * r[0][0]
-        r0 = b0HalfSizes[0] * absR[2][0] + b0HalfSizes[2] * absR[0][0]
-        r1 = b1HalfSizes[1] * absR[1][2] + b1HalfSizes[2] * absR[1][1]
-        if (abs(tProj) > r0 + r1) return false
+        // Test axis L = A0 x B0
+        ra = b0hsY * arm20 + b0hsZ * arm10
+        rb = b1hsY * arm02 + b1hsZ * arm01
+        if (abs(taz * rm10 - tay * rm20) > ra + rb) return false
 
-        // L = A1 x B1
-        tProj = t.x * r[2][1] - t.z * r[0][1]
-        r0 = b0HalfSizes[0] * absR[2][1] + b0HalfSizes[2] * absR[0][1]
-        r1 = b1HalfSizes[0] * absR[1][2] + b1HalfSizes[2] * absR[1][0]
-        if (abs(tProj) > r0 + r1) return false
+        // Test axis L = A0 x B1
+        ra = b0hsY * arm21 + b0hsZ * arm11
+        rb = b1hsX * arm02 + b1hsZ * arm00
+        if (abs(taz * rm11 - tay * rm21) > ra + rb) return false
 
-        // L = A1 x B2
-        tProj = t.x * r[2][2] - t.z * r[0][2]
-        r0 = b0HalfSizes[0] * absR[2][2] + b0HalfSizes[2] * absR[0][2]
-        r1 = b1HalfSizes[0] * absR[1][1] + b1HalfSizes[1] * absR[1][0]
-        if (abs(tProj) > r0 + r1) return false
+        // Test axis L = A0 x B2
+        ra = b0hsY * arm22 + b0hsZ * arm12
+        rb = b1hsX * arm01 + b1hsY * arm00
+        if (abs(taz * rm12 - tay * rm22) > ra + rb) return false
 
-        // L = A2 x B0
-        tProj = t.y * r[0][0] - t.x * r[1][0]
-        r0 = b0HalfSizes[0] * absR[1][0] + b0HalfSizes[1] * absR[0][0]
-        r1 = b1HalfSizes[1] * absR[2][2] + b1HalfSizes[2] * absR[2][1]
-        if (abs(tProj) > r0 + r1) return false
+        // Test axis L = A1 x B0
+        ra = b0hsX * arm20 + b0hsZ * arm00
+        rb = b1hsY * arm12 + b1hsZ * arm11
+        if (abs(tax * rm20 - taz * rm00) > ra + rb) return false
 
-        // L = A2 x B1
-        tProj = t.y * r[0][1] - t.x * r[1][1]
-        r0 = b0HalfSizes[0] * absR[1][1] + b0HalfSizes[1] * absR[0][1]
-        r1 = b1HalfSizes[0] * absR[2][2] + b1HalfSizes[2] * absR[2][0]
-        if (abs(tProj) > r0 + r1) return false
+        // Test axis L = A1 x B1
+        ra = b0hsX * arm21 + b0hsZ * arm01
+        rb = b1hsX * arm12 + b1hsZ * arm10
+        if (abs(tax * rm21 - taz * rm01) > ra + rb) return false
 
-        // L = A2 x B2
-        tProj = t.y * r[0][2] - t.x * r[1][2]
-        r0 = b0HalfSizes[0] * absR[1][2] + b0HalfSizes[1] * absR[0][2]
-        r1 = b1HalfSizes[0] * absR[2][1] + b1HalfSizes[1] * absR[2][0]
-        if (abs(tProj) > r0 + r1) return false
+        // Test axis L = A1 x B2
+        ra = b0hsX * arm22 + b0hsZ * arm02
+        rb = b1hsX * arm11 + b1hsY * arm10
+        if (abs(tax * rm22 - taz * rm02) > ra + rb) return false
 
-        // No separating axis found - boxes intersect
+        // Test axis L = A2 x B0
+        ra = b0hsX * arm10 + b0hsY * arm00
+        rb = b1hsY * arm22 + b1hsZ * arm21
+        if (abs(tay * rm00 - tax * rm10) > ra + rb) return false
+
+        // Test axis L = A2 x B1
+        ra = b0hsX * arm11 + b0hsY * arm01
+        rb = b1hsX * arm22 + b1hsZ * arm20
+        if (abs(tay * rm01 - tax * rm11) > ra + rb) return false
+
+        // Test axis L = A2 x B2
+        ra = b0hsX * arm12 + b0hsY * arm02
+        rb = b1hsX * arm21 + b1hsY * arm20
+        if (abs(tay * rm02 - tax * rm12) > ra + rb) return false
+
+        // Since no separating axis is found, the OBBs must be intersecting
         return true
     }
 
@@ -182,7 +215,7 @@ open class OrientedBoundingBox(val n: Node, val min: Vector3f, val max: Vector3f
         val approxResult =
             other.getBoundingSphere().radius + getBoundingSphere().radius > (other.getBoundingSphere().origin - getBoundingSphere().origin).length()
         return if(precise && approxResult) {
-            testObbIntersection(
+            testObOb(
                 this.center,
                 this.n.spatialOrNull()!!.localX,
                 this.n.spatialOrNull()!!.localY,
