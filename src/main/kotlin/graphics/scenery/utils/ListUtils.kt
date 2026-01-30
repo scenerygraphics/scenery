142f9bf7739764eb88602469010a33af82383ba7
Samuel Pantze <83579186+smlpt@users.noreply.github.com>
2026-01-30 14:13:19 +0100

    Extract HedgehogAnalysis.gaussSmoothing to ListUtils

--- src/main/kotlin/graphics/scenery/utils/ListUtils.kt
+++ src/main/kotlin/graphics/scenery/utils/ListUtils.kt
@@ -5,7 +5,7 @@ import kotlin.math.sqrt
 
 /**
  * Convert a 3-element [DoubleArray] to a [Vector3f].
- * */
+ */
 fun DoubleArray.toVector3f(): Vector3f {
     require(size == 3) { "DoubleArray must have exactly 3 elements" }
     return Vector3f(this[0].toFloat(), this[1].toFloat(), this[2].toFloat())
@@ -32,8 +32,31 @@ fun localMaxima(list: List<Float>): List<Pair<Int, Float>> {
 
 /**
  * Returns the standard deviation of the elements in a [Float] collection.
- * */
+ */
 fun Iterable<Float>.stdDev() : Float {
     val average = this.average()
     return sqrt((this.sumOf { (it - average) * (it - average) } / this.count())).toFloat()
+}
+
+/**
+ * Perform a Gauss smoothing operation by sliding a kernel with (0.25, 0.5, 0.25) for [iterations] times over the data.
+ */
+fun gaussSmoothing(samples: List<Float>, iterations: Int): List<Float> {
+    var smoothed = samples.toList()
+    val kernel = listOf(0.25f, 0.5f, 0.25f)
+    for (i in 0 until iterations) {
+        val newSmoothed = ArrayList<Float>(smoothed.size)
+        // Handle the first element
+        newSmoothed.add(smoothed[0] * 0.75f + smoothed[1] * 0.25f)
+        // Apply smoothing to the middle elements
+        for (j in 1 until smoothed.size - 1) {
+            val value = kernel[0] * smoothed[j-1] + kernel[1] * smoothed[j] + kernel[2] * smoothed[j+1]
+            newSmoothed.add(value)
+        }
+        // Handle the last element
+        newSmoothed.add(smoothed[smoothed.size - 2] * 0.25f + smoothed[smoothed.size - 1] * 0.75f)
+
+        smoothed = newSmoothed
+    }
+    return smoothed
 }
